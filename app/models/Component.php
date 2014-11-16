779f8b70e724d1c41281e980dcaba37617420ad2
James Brooks <jbrooksuk@me.com>
2014-11-16 23:29:48 +0000

    Component now has a humanStatus attribute

--- app/models/Component.php
+++ app/models/Component.php
@@ -1,5 +1,12 @@
 <?php
 
 	class Component extends Eloquent {
-
+		public function getHumanStatus() {
+			switch ($this->status) {
+				case 1: return 'Operational';
+				case 2: return 'Performance Issues';
+				case 3: return 'Partial Outage';
+				case 4: return 'Major Outage';
+			}
+		}
 	}
