e58f1996f5779c6117e251546b49a9ec0f38c038
James Brooks <jbrooksuk@me.com>
2014-11-16 23:44:18 +0000

    Include Bootstrap within the views, make it a bit nicer

--- app/views/layout/master.blade.php
+++ app/views/layout/master.blade.php
@@ -1,10 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Cachet</title>
+	<!-- Set the viewport width to device width for mobile -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="author" content="http://james-brooks.uk">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<link href='http://fonts.googleapis.com/css?family=Raleway:400,700,300,600' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 </head>
 <body>
-	@section('content')
-	@show
+	<div class='container'>
+	@yield('content')
+	</div>
 </body>
 </html>
