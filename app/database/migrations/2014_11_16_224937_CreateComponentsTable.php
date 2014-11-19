5b0929eb548e5c50e2c30784577a9441d5b9fb6d
James Brooks <james@bluebaytravel.co.uk>
2014-11-19 16:51:14 +0000

    Don't set status limit, we may have more in the future

--- app/database/migrations/2014_11_16_224937_CreateComponentsTable.php
+++ app/database/migrations/2014_11_16_224937_CreateComponentsTable.php
@@ -17,7 +17,7 @@ class CreateComponentsTable extends Migration {
 			$table->increments('id');
 			$table->string('name');
 			$table->string('description')->nullable()->default(null);
-			$table->tinyInteger('status', 1)->default(1);
+			$table->tinyInteger('status')->default(1);
 			$table->timestamps();
 
 			$table->index('status');
