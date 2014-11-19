f3c20600887239817930b8fbfc9755cbec75a39d
James Brooks <james@bluebaytravel.co.uk>
2014-11-19 12:31:47 +0000

    Getting Elixir to work again

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
-    mix.sass("application.scss").version('css/application.css');
+    mix.sass("application.scss")
+       .version('css/application.css');
 });
