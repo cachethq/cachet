2176e63ad95c1e0682e8188f6fb00833b7342993
manavo <manavo@gmail.com>
2014-11-24 17:21:50 +0000

    Login page

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -5,7 +5,15 @@
 	 */
 	class AuthController extends Controller {
 		public function showLogin() {
-			return 'Coming soon...';
+			return View::make('auth.login');
+		}
+
+		public function postLogin() {
+			if (Auth::attempt(Input::only(['email', 'password']))) {
+				return Redirect::intended('dashboard');
+			} else {
+				return Redirect::back()->withInput(Input::except('password'))->with('error', 'Invalid email or password');
+			}
 		}
 
 		public function logoutAction() {
--- app/routes/app.php
+++ app/routes/app.php
@@ -11,6 +11,8 @@
 	});
 
 	Route::get('/auth/login', 'AuthController@showLogin')->before('guest');
+	Route::post('/auth/login', 'AuthController@postLogin')->before('guest|csrf');
+
 	Route::group(['before' => 'auth'], function() {
 		// Dashboard/Management Panel etc.
 		Route::get('/dashboard', 'DashboardController@showDashboard');
