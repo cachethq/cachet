782d05368ea8ba78f3e67e7aa97df94b1959706d
James Brooks <james@bluebaytravel.co.uk>
2014-11-24 17:48:26 +0000

    Move sections of HTML into imports directory.

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -7,27 +7,11 @@
 	<p>You're logged in. This will be a link to the Dashboard.</p>
 	@endif
 
-	@if($components->count() > 0)
-	<div class='page-header'>
-		<ul class='list-group components'>
-			@foreach($components as $component)
-			<li class='list-group-item component '>
-				<h4>{{ $component->name }} <small class='{{ $component->color }}'>{{ $component->humanStatus }}</small></h4>
-				<p>{{ $component->description }}</p>
-			</li>
-			@endforeach
-		</ul>
-	</div>
-	@endif
+	@include('imports.components')
 
 	@for($i=0; $i <= 7; $i++)
-	@include('incident', array('i', $i))
+	@include('imports.incident', array('i', $i))
 	@endfor
 
-	@if(Setting::get('show_support'))
-	<hr />
-	<div class='footer'>
-		<p>{{ Setting::get('app_name') }} Status Page is powered by <a href='https://github.com/jbrooksuk/Cachet' target="_blank">Cachet</a>.</p>
-	</div>
-	@endif
+	@include('imports.support-link')
 @stop
