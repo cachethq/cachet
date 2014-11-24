72314e40b5162df7bb14492dabcb5cf7a57f7ede
James Brooks <jbrooksuk@me.com>
2014-11-24 18:56:41 +0000

    Indent classes one level

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -1,18 +1,18 @@
 <?php
 
-class ApiController extends Dingo\Api\Routing\Controller{
+	class ApiController extends Dingo\Api\Routing\Controller{
 
-	public function getComponents() {
-		return Component::all();
-	}
-
-	public function getComponent($id) {
-		$component = Component::find($id);
-		if ($component) {
-			return $component;
-		} else {
-			App::abort(404, 'Component not found');
+		public function getComponents() {
+			return Component::all();
 		}
-	}
 
-}
+		public function getComponent($id) {
+			$component = Component::find($id);
+			if ($component) {
+				return $component;
+			} else {
+				App::abort(404, 'Component not found');
+			}
+		}
+
+	}
