7634b93852624b58a8916488eafed2e42431e634
James Brooks <james@bluebaytravel.co.uk>
2014-11-24 17:09:32 +0000

    Added some missing route models

--- app/routes.php
+++ app/routes.php
@@ -1,8 +1,11 @@
 <?php
 
-	Route::model('incident', 'Incident');
 	Route::model('component', 'Component');
+	Route::model('incident', 'Incident');
+	Route::model('incident_template', 'IncidentTemplate');
+	Route::model('setting', 'Setting');
 	Route::model('webhook', 'WebHook');
+	Route::model('user', 'User');
 
 	routesInDirectory();
 
