f71639f22642620a3760fe0673850bd4b5c7bcdc
James Brooks <jbrooksuk@me.com>
2014-11-25 20:37:37 +0000

    Use fieldsets and legends instead of headers

--- app/views/setup.blade.php
+++ app/views/setup.blade.php
@@ -13,50 +13,54 @@
 				<div class='panel-heading'>Service Details</div>
 				<div class='panel-body'>
 					{{ Form::open(['name' => 'SetupForm', 'class' => 'form-vertical', 'role' => 'form']) }}
-						<h3>Status Page Details</h3>
-						<div class='form-group'>
-							<label class='sr-only'>Site Name</label>
-							<input type='text' name='settings[app_name]' class='form-control' placeholder='Site Name' value='{{ Input::old('settings.app_name', '') }}' required />
-							@if($errors->has('settings.app_name'))
-							<span class='text-danger'>{{ $errors->first('settings.app_name') }}</span>
-							@endif
-						</div>
-						<div class='form-group'>
-							<label class='sr-only'>Site Domain</label>
-							<input type='text' name='settings[app_domain]' class='form-control' placeholder='Site Domain' value='{{ Input::old('settings.app_domain', '') }}' required />
-							@if($errors->has('settings.app_domain'))
-							<span class='text-danger'>{{ $errors->first('settings.app_domain') }}</span>
-							@endif
-						</div>
-						<div class='form-group'>
-							<label>
-								<input type='checkbox' name='settings[show_support]' value='1' checked />
-								Show support for Cachet?
-							</label>
-						</div>
+						<fieldset>
+							<legend>Status Page Setup</legend>
+							<div class='form-group'>
+								<label class='sr-only'>Site Name</label>
+								<input type='text' name='settings[app_name]' class='form-control' placeholder='Site Name' value='{{ Input::old('settings.app_name', '') }}' required />
+								@if($errors->has('settings.app_name'))
+								<span class='text-danger'>{{ $errors->first('settings.app_name') }}</span>
+								@endif
+							</div>
+							<div class='form-group'>
+								<label class='sr-only'>Site Domain</label>
+								<input type='text' name='settings[app_domain]' class='form-control' placeholder='Site Domain' value='{{ Input::old('settings.app_domain', '') }}' required />
+								@if($errors->has('settings.app_domain'))
+								<span class='text-danger'>{{ $errors->first('settings.app_domain') }}</span>
+								@endif
+							</div>
+							<div class='form-group'>
+								<label>
+									<input type='checkbox' name='settings[show_support]' value='1' checked />
+									Show support for Cachet?
+								</label>
+							</div>
+						</fieldset>
 
-						<h3>Administrator Account</h3>
-						<div class='form-group'>
-							<label class='sr-only'>Username</label>
-							<input type='text' name='user[name]' class='form-control' placeholder='Username' value='{{ Input::old('user.name', '') }}' required />
-							@if($errors->has('user.name'))
-							<span class='text-danger'>{{ $errors->first('user.name') }}</span>
-							@endif
-						</div>
-						<div class='form-group'>
-							<label class='sr-only'>Email</label>
-							<input type='email' name='user[email]' class='form-control' placeholder='Email' value='{{ Input::old('user.email', '') }}' required />
-							@if($errors->has('user.email'))
-							<span class='text-danger'>{{ $errors->first('user.email') }}</span>
-							@endif
-						</div>
-						<div class='form-group'>
-							<label class='sr-only'>Password</label>
-							<input type='password' name='user[password]' class='form-control' placeholder='Password' value='{{ Input::old('user.password', '') }}' required />
-							@if($errors->has('user.password'))
-							<span class='text-danger'>{{ $errors->first('user.password') }}</span>
-							@endif
-						</div>
+						<fieldset>
+							<legend>Administrator Account</legend>
+							<div class='form-group'>
+								<label class='sr-only'>Username</label>
+								<input type='text' name='user[name]' class='form-control' placeholder='Username' value='{{ Input::old('user.name', '') }}' required />
+								@if($errors->has('user.name'))
+								<span class='text-danger'>{{ $errors->first('user.name') }}</span>
+								@endif
+							</div>
+							<div class='form-group'>
+								<label class='sr-only'>Email</label>
+								<input type='email' name='user[email]' class='form-control' placeholder='Email' value='{{ Input::old('user.email', '') }}' required />
+								@if($errors->has('user.email'))
+								<span class='text-danger'>{{ $errors->first('user.email') }}</span>
+								@endif
+							</div>
+							<div class='form-group'>
+								<label class='sr-only'>Password</label>
+								<input type='password' name='user[password]' class='form-control' placeholder='Password' value='{{ Input::old('user.password', '') }}' required />
+								@if($errors->has('user.password'))
+								<span class='text-danger'>{{ $errors->first('user.password') }}</span>
+								@endif
+							</div>
+						</fieldset>
 
 						<hr />
 						<div class='form-group'>
