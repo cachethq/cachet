5576fc2ac6c9693cf49b359a8741b78ec1f47c35
James Brooks <james@bluebaytravel.co.uk>
2014-11-26 10:40:37 +0000

    Add metrics and user tabs

--- app/views/dashboard/index.blade.php
+++ app/views/dashboard/index.blade.php
@@ -12,14 +12,18 @@
 		<li role='presentation' class='active'><a href='#dash' role='tab' data-toggle='tab'>Home</a></li>
 		<li role='presentation'><a href='#components' role='tab' data-toggle='tab'>Components</a></li>
 		<li role='presentation'><a href='#incidents' role='tab' data-toggle='tab'>Incidents</a></li>
+		<li role='presentation'><a href='#metrics' role='tab' data-toggle='tab'>Metrics</a></li>
+		<li role='presentation'><a href='#users' role='tab' data-toggle='tab'>Users</a></li>
 	</ul>
 
 	<div class='tab-content'>
 		<div role='tabpanel' class='tab-pane active' id='dash'>
 			<p>Coming soon... <a href="/">Back to Status Page.</a></p>
 		</div>
-		<div role='tabpanel' class='tab-pane' id='components'>Components?</div>
-		<div role='tabpanel' class='tab-pane' id='incidents'>Incidents?</div>
+		<div role='tabpanel' class='tab-pane' id='components'>Components</div>
+		<div role='tabpanel' class='tab-pane' id='incidents'>Incidents</div>
+		<div role='tabpanel' class='tab-pane' id='metrics'>Metrics</div>
+		<div role='tabpanel' class='tab-pane' id='users'>Users</div>
 	</div>
 </div>
 @stop
