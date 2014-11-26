baee247363dd5aa1967c50d1254891a5b23efae1
James Brooks <james@bluebaytravel.co.uk>
2014-11-26 16:42:41 +0000

    Created the services table

---
+++ app/database/migrations/2014_11_26_164111_CreateServicesTable.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateServicesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('services', function(Blueprint $table)
+		{
+			$table->increments('id');
+
+			$table->string('type')->nullable(false);
+			$table->boolean('active')->default(0); // Inactive by default
+			$table->text('properties')->nullable(true);
+
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('services');
+	}
+
+}
