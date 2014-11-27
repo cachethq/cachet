cac71572b0fd4ac1c9697e171c6aad5c8908b417
James Brooks <james@bluebaytravel.co.uk>
2014-11-27 11:30:07 +0000

    Fixing up the DingoServiceProvider not being found

--- app/config/app.php
+++ app/config/app.php
@@ -122,7 +122,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 
-		'Dingo\Api\ApiServiceProvider',
+		'Dingo\Api\Provider\ApiServiceProvider',
 
 		'CachetHQ\Cachet\Support\ServiceProviders\RepositoryServiceProvider',
 
