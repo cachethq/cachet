4a30770d36d2596e6ef10a47a5f3786965a95c05
Thomas Coleman <tom@ilikeprograms.com>
2014-11-27 16:36:24 +0000

    [Coding Standards] Applied new coding standards to CachetHQ\Cachet folder

--- app/CachetHq/Cachet/Controllers/Api/MetricController.php
+++ app/CachetHq/Cachet/Controllers/Api/MetricController.php
@@ -1,76 +1,76 @@
 <?php
 
-	namespace CachetHQ\Cachet\Controllers\Api;
+namespace CachetHQ\Cachet\Controllers\Api;
 
-	use Input, Metric;
-	use Dingo\Api\Routing\Controller as DingoController;
+use Input, Metric;
+use Dingo\Api\Routing\Controller as DingoController;
 
-	class MetricController extends DingoController {
+class MetricController extends DingoController {
 
-		/**
-		 * Get all metrics
-		 *
-		 * @return \Illuminate\Database\Eloquent\Collection
-		 */
-		public function getMetrics() {
-			return Metric::all();
-		}
+    /**
+     * Get all metrics
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function getMetrics() {
+        return Metric::all();
+    }
 
-		/**
-		 * Get a single metric
-		 *
-		 * @param int $id
-		 *
-		 * @return Metric
-		 */
-		public function getMetric($id) {
-			if ($metric = Metric::find($id)) {
-				return $metric;
-			} else {
-				App::abort(404, 'Metric not found');
-			}
-		}
+    /**
+     * Get a single metric
+     *
+     * @param int $id
+     *
+     * @return Metric
+     */
+    public function getMetric($id) {
+        if ($metric = Metric::find($id)) {
+            return $metric;
+        } else {
+            App::abort(404, 'Metric not found');
+        }
+    }
 
-		/**
-		 * Create a new metric
-		 *
-		 * @return Metric
-		 */
-		public function postMetrics() {
-			$metric = new Metric(Input::all());
-			return $this->_saveMetric($metric);
-		}
+    /**
+     * Create a new metric
+     *
+     * @return Metric
+     */
+    public function postMetrics() {
+        $metric = new Metric(Input::all());
+        return $this->_saveMetric($metric);
+    }
 
-		/**
-		 * Update an existing metric
-		 *
-		 * @param int $id
-		 *
-		 * @return Metric
-		 */
-		public function putMetric($id) {
-			$metric = $this->getMetric($id);
-			$metric->fill(Input::all());
-			return $this->_saveMetric($metric);
-		}
+    /**
+     * Update an existing metric
+     *
+     * @param int $id
+     *
+     * @return Metric
+     */
+    public function putMetric($id) {
+        $metric = $this->getMetric($id);
+        $metric->fill(Input::all());
+        return $this->_saveMetric($metric);
+    }
 
-		/**
-		 * Function for saving a metric, and returning appropriate error codes
-		 *
-		 * @param Metric $metric
-		 *
-		 * @return Metric
-		 */
-		private function _saveMetric($metric) {
-			if ($metric->isValid()) {
-				try {
-					$metric->saveOrFail();
-					return $metric;
-				} catch (Exception $e) {
-					App::abort(500, $e->getMessage());
-				}
-			} else {
-				App::abort(404, $metric->getErrors()->first());
-			}
-		}
-	}
+    /**
+     * Function for saving a metric, and returning appropriate error codes
+     *
+     * @param Metric $metric
+     *
+     * @return Metric
+     */
+    private function _saveMetric($metric) {
+        if ($metric->isValid()) {
+            try {
+                $metric->saveOrFail();
+                return $metric;
+            } catch (Exception $e) {
+                App::abort(500, $e->getMessage());
+            }
+        } else {
+            App::abort(404, $metric->getErrors()->first());
+        }
+    }
+}
--- app/transformers/MetricTransformer.php
+++ app/transformers/MetricTransformer.php
@@ -1,15 +1,15 @@
 <?php
 
-	class MetricTransformer extends \League\Fractal\TransformerAbstract {
-		public function transform(Metric $metric) {
-			return [
-				'id' => (int) $component->id,
-				'name' => $component->name,
-				'description' => $component->description,
-				'suffix' => $component->suffix,
-				'display' => $component->shouldDisplay,
-				'created_at' => $component->created_at->timestamp,
-				'updated_at' => $component->updated_at->timestamp,
-			];
-		}
-	}
+class MetricTransformer extends \League\Fractal\TransformerAbstract {
+    public function transform(Metric $metric) {
+        return [
+            'id'            => (int) $component->id,
+            'name'          => $component->name,
+            'description'   => $component->description,
+            'suffix'        => $component->suffix,
+            'display'       => $component->shouldDisplay,
+            'created_at'    => $component->created_at->timestamp,
+            'updated_at'    => $component->updated_at->timestamp,
+        ];
+    }
+}
