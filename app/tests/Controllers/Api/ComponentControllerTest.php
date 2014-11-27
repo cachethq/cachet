1144df4023a6a59d6ff44b99945a140e307c703c
Elliot Hesp <elliot.hesp@gmail.com>
2014-11-27 20:42:12 +0000

    Added basic controller unit tests

---
+++ app/tests/Controllers/Api/ComponentControllerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+class ComponentControllerTest extends TestCase {
+
+	public function setUp() {
+		$this->repo = Mockery::mock('CachetHQ\Cachet\Repositories\Component\ComponentRepository');
+		$this->dingo = Mockery::mock('Dingo\Api\Auth\Shield');
+	}
+
+	public function tearDown() {
+		Mockery::close();
+	}
+
+	public function test_get_components_method()
+	{
+		$this->repo->shouldReceive('all')->once()->andReturn('foo');
+
+		$controller = new CachetHQ\Cachet\Controllers\Api\ComponentController($this->dingo, $this->repo);
+		$response = $controller->getComponents();
+		
+		$this->assertEquals('foo', $response);
+	}
+
+	public function test_get_component_method()
+	{
+		$this->repo->shouldReceive('findOrFail')->with(1)->once()->andReturn('foo');
+
+		$controller = new CachetHQ\Cachet\Controllers\Api\ComponentController($this->dingo, $this->repo);
+		$response = $controller->getComponent(1);
+		
+		$this->assertEquals('foo', $response);
+	}
+}
