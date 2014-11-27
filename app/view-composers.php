0b3f4f546888ed2134996767852b8b2dd4eed083
James Brooks <jbrooksuk@me.com>
2014-11-27 22:14:56 +0000

    Use tabs not spaces

--- app/view-composers.php
+++ app/view-composers.php
@@ -1,24 +1,24 @@
 <?php
 
 View::composer('index', function($view) {
-    $date = date('Y-m-d');
+	$date = date('Y-m-d');
 
-    $incidents = Incident::whereRaw('DATE(created_at) = "' . $date . '"')
-                    ->groupBy('status')
-                    ->orderBy('status', 'desc');
+	$incidents = Incident::whereRaw('DATE(created_at) = "' . $date . '"')
+					->groupBy('status')
+					->orderBy('status', 'desc');
 
-    $incidentCount = $incidents->count();
+	$incidentCount = $incidents->count();
 
-    if ($incidentCount <= 1 || ($incidentCount > 1 && (int) $incidents->first()->status === 4)) {
-        $status  = 'success';
-        $message = Lang::get('overview.good');
-    } else {
-        $status  = 'danger';
-        $message = Lang::get('overview.bad');
-    }
+	if ($incidentCount <= 1 || ($incidentCount > 1 && (int) $incidents->first()->status === 4)) {
+		$status  = 'success';
+		$message = Lang::get('overview.good');
+	} else {
+		$status  = 'danger';
+		$message = Lang::get('overview.bad');
+	}
 
-    $view->with([
-        'systemStatus'  => $status,
-        'systemMessage' => $message
-    ]);
+	$view->with([
+		'systemStatus'  => $status,
+		'systemMessage' => $message
+	]);
 });
