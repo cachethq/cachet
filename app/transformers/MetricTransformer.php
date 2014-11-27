a819b5f1cc569117d81928db87fcf3acd082155f
manavo <manavo@gmail.com>
2014-11-27 17:07:14 +0000

    Fix transformer

--- app/transformers/MetricTransformer.php
+++ app/transformers/MetricTransformer.php
@@ -3,13 +3,13 @@
 class MetricTransformer extends \League\Fractal\TransformerAbstract {
     public function transform(Metric $metric) {
         return [
-            'id'            => (int) $component->id,
-            'name'          => $component->name,
-            'description'   => $component->description,
-            'suffix'        => $component->suffix,
-            'display'       => $component->shouldDisplay,
-            'created_at'    => $component->created_at->timestamp,
-            'updated_at'    => $component->updated_at->timestamp,
+            'id'            => (int) $metric->id,
+            'name'          => $metric->name,
+            'description'   => $metric->description,
+            'suffix'        => $metric->suffix,
+            'display'       => $metric->shouldDisplay,
+            'created_at'    => $metric->created_at->timestamp,
+            'updated_at'    => $metric->updated_at->timestamp,
         ];
     }
 }
