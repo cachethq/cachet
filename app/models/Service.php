043078daaafe991724d4ae75ab1701afb61b1d2f
James Brooks <james@bluebaytravel.co.uk>
2014-11-27 14:09:47 +0000

    Add Validation to the Service model

--- app/models/Service.php
+++ app/models/Service.php
@@ -1,6 +1,16 @@
 <?php
 
+	use Watson\Validating\ValidatingTrait;
+
 	class Service extends Eloquent {
+		use ValidatingTrait;
+
+		protected $rules = [
+			'type'       => 'alpha_dash|required',
+			'active'     => 'required|in:0,1',
+			'properties' => ''
+		];
+
 		public function getPropertiesAttribute($properties) {
 			return json_decode($properties);
 		}
