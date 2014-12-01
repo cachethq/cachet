9f435ff5d054f66d744c965f35e10b41e5f7885a
Elliot Hesp <elliot.hesp@gmail.com>
2014-12-01 19:33:15 +0000

    Elixir now versions updated bower components

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
-    mix.sass("application.scss")
-       .version('css/application.css');
+    mix.scripts([
+    		'bower_components/jquery/dist/jquery.min.js',
+    		'bower_components/chartjs/Chart.min.js',
+    		'bower_components/rivets/dist/riverts.bundle.js',
+    		'js/**/*.js',
+    	], 'app/assets')
+		.styles([
+  			'bower_components/cachet-ui/dist/cachet.min.css',
+  		], 'app/assets')
+  		.version(['public/css/all.css', 'public/js/all.js'], 'public');
 });
