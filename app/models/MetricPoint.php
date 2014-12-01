0ae06acac2c10db197fa259463568e829b4ad9f8
James Brooks <james@bluebaytravel.co.uk>
2014-12-01 08:53:32 +0000

    MetricPoint belongsTo a Metric

--- app/models/MetricPoint.php
+++ app/models/MetricPoint.php
@@ -1,5 +1,11 @@
 <?php
 
 class MetricPoint extends Eloquent {
-
+    /**
+     * A metric point belongs to a metric unit.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function metric() {
+        return $this->belongsTo('Metric', 'id', 'metric_id');
+    }
 }
