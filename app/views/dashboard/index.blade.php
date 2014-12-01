2219fc82412c30100d703cc30f305e050f307541
James Brooks <james@bluebaytravel.co.uk>
2014-12-01 08:47:28 +0000

    Added subscribers tab

--- app/views/dashboard/index.blade.php
+++ app/views/dashboard/index.blade.php
@@ -13,17 +13,29 @@
         <li role='presentation'><a href='#components' role='tab' data-toggle='tab'>Components</a></li>
         <li role='presentation'><a href='#incidents' role='tab' data-toggle='tab'>Incidents</a></li>
         <li role='presentation'><a href='#metrics' role='tab' data-toggle='tab'>Metrics</a></li>
-        <li role='presentation'><a href='#users' role='tab' data-toggle='tab'>Users</a></li>
+        <li role='presentation'><a href='#subscribers' role='tab' data-toggle='tab'>Subscribers</a></li>
     </ul>
 
     <div class='tab-content'>
         <div role='tabpanel' class='tab-pane active' id='dash'>
             <p>Coming soon... <a href="/">Back to Status Page.</a></p>
+            <p>Once we're ready this tab will be showing you a mini status page.</p>
         </div>
-        <div role='tabpanel' class='tab-pane' id='components'>Components</div>
-        <div role='tabpanel' class='tab-pane' id='incidents'>Incidents</div>
-        <div role='tabpanel' class='tab-pane' id='metrics'>Metrics</div>
-        <div role='tabpanel' class='tab-pane' id='users'>Users</div>
+        <div role='tabpanel' class='tab-pane' id='components'>
+            <p>Add, edit and delete components here.</p>
+        </div>
+        <div role='tabpanel' class='tab-pane' id='incidents'>
+            <p>Add, edit and delete incidents here.</p>
+        </div>
+        <div role='tabpanel' class='tab-pane' id='metrics'>
+            <p>Add, edit and delete metrics (and metric points) here.</p>
+        </div>
+        <div role='tabpanel' class='tab-pane' id='users'>
+            <p>Add, edit and delete users here.</p>
+        </div>
+        <div role='tabpanel' class='tab-pane' id='subscribers'>
+            <p>Show all subscribers here.</p>
+        </div>
     </div>
 </div>
 @stop
