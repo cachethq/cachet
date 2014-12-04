f2a131314cf3d041d1790a5e433572b2953b2106
Elliot Hesp <elliot.hesp@gmail.com>
2014-12-04 10:29:55 +0000

    Fixed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,21 @@
 var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
-  mix.scripts([
-    'bower_components/jquery/dist/jquery.min.js',
-    'bower_components/chartjs/Chart.min.js',
-    'bower_components/rivets/dist/riverts.bundle.js',
-    'js/**/*.js',
-    'js/start.js',
-  ], 'app/assets')
-  .styles([
-     'bower_components/cachet-ui/dist/cachet.min.css',
-  ], 'app/assets')
-  .version(['public/css/all.css', 'public/js/all.js'], 'public');
+  mix.sass('main.scss')
+     .styles([
+        'app/assets/bower_components/bootstrap/dist/css/bootstrap.min.css',
+        'app/assets/bower_components/fontawesome/css/font-awesome.min.css',
+        'public/css/main.css',
+     ], './')
+     .scripts([
+       'bower_components/jquery/dist/jquery.min.js',
+       'bower_components/bootstrap/dist/js/bootstrap.min.js',
+       'bower_components/chartjs/Chart.min.js',
+       'bower_components/rivets/dist/riverts.bundle.js',
+       'js/app.js',
+       'js/**/*.js',
+     ], './app/assets/')
+     .version(['public/css/all.css', 'public/js/all.js'], 'public')
+     .publish('bootstrap/dist/fonts', 'public/build/fonts')
+     .publish('fontawesome/fonts', 'public/build/fonts');
 });
