70e95f612343413c761dbea187fab1236eccdf3a
Elliot Hesp <elliot.hesp@gmail.com>
2014-12-04 21:56:54 +0000

    Started on dashboard content area

--- app/views/dashboard/index.blade.php
+++ app/views/dashboard/index.blade.php
@@ -1,8 +1,24 @@
 @extends('layout.dashboard')
 
 @section('content')
-<div style="margin: 20px;">
-    Dashboard
-</div>
-
+	<div class="header">
+		<i class="fa fa-dashboard"></i> Dashboard
+		<input type="text" class="form-control input-sm pull-right" placeholder="Search...">
+	</div>
+	<div class="row">
+		<div class="col-sm-12">
+			<ul class="tabs">
+				<li><a href="#tab-1">Tab 1</a></li>
+				<li><a href="#tab-2">Tab 2</a></li>
+				<li><a href="#tab-3">Tab 3</a></li>
+				<li><a href="#tab-4">Tab 4</a></li>
+			</ul>
+			<div class="tab-content">
+				<div id="tab-1">Tab 1</div>
+				<div id="tab-2">Tab 2</div>
+				<div id="tab-3">Tab 3</div>
+				<div id="tab-4">Tab 4</div>
+			</div>
+		</div>
+	</div>
 @stop
