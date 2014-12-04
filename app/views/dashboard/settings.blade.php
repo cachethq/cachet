3f520632d1981d714ba485967dc3a9ab54e7d61e
James Brooks <jbrooksuk@me.com>
2014-12-04 22:51:49 +0000

    Use tab-* setup

--- app/views/dashboard/settings.blade.php
+++ app/views/dashboard/settings.blade.php
@@ -8,14 +8,14 @@
 	<div class="row">
 		<div class="col-sm-12">
 			<ul class="tabs">
-				<li class='active'><a href="#app">App</a></li>
-				<li><a href="#server">Server</a></li>
-				<li><a href="#mail">Mail</a></li>
+				<li class='active'><a href="#tab-1">App</a></li>
+				<li><a href="#tab-2">Server</a></li>
+				<li><a href="#tab-3">Mail</a></li>
 			</ul>
 			<div class="tab-content">
-				<div id="app" class='active'>General App settings</div>
-				<div id="server">Server settings</div>
-				<div id="mail">Mail setitngs, host, SMTP, user and password.</div>
+				<div id="tab-1">General App settings</div>
+				<div id="tab-2">Server settings</div>
+				<div id="tab-3">Mail setitngs, host, SMTP, user and password.</div>
 			</div>
 		</div>
 	</div>
