345401d4d6d7ba9db97d462e74b515cf1ee449ec
James Brooks <jbrooksuk@me.com>
2014-12-06 10:21:27 +0000

    Update metric panel

--- app/views/dashboard/metrics.blade.php
+++ app/views/dashboard/metrics.blade.php
@@ -9,14 +9,12 @@
 		<div class="col-sm-12">
 			<div role='tabpanel'>
 				<ul class="nav nav-tabs" role='tablist'>
-					<li role='presentation' class='active'><a data-toggle='tab' role='tab' href="#tab-1">Tab 1</a></li>
-					<li role='presentation'><a data-toggle='tab' role='tab' href="#tab-2">Tab 2</a></li>
-					<li role='presentation'><a data-toggle='tab' role='tab' href="#tab-3">Tab 3</a></li>
+					<li role='presentation' class='active'><a data-toggle='tab' role='tab' href="#metrics">Metrics</a></li>
+					<li role='presentation'><a data-toggle='tab' role='tab' href="#add-point">Add Data Point</a></li>
 				</ul>
 				<div class="tab-content">
-					<div role='tabpanel' class='tab-pane active' id="tab-1">Foo</div>
-					<div role='tabpanel' class='tab-pane' id="tab-2">Bar</div>
-					<div role='tabpanel' class='tab-pane' id="tab-3">Baz</div>
+					<div role='tabpanel' class='tab-pane active' id="metrics">Metrics</div>
+					<div role='tabpanel' class='tab-pane' id="add-point">Add a data point here</div>
 				</div>
 			</div>
 		</div>
