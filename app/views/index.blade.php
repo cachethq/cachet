30a8a87cee6434050da5ba921b067193738d970c
James Brooks <jbrooksuk@me.com>
2014-12-18 22:51:41 +0000

    Make the dashboard/logout link better

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -4,19 +4,10 @@
     <div class='alert alert-{{ $systemStatus }}'>{{ $systemMessage }}</div>
 
     @if(Auth::check())
-    <div class='row'>
-        <div class='col-md-12'>
-            <div class='row'>
-                <div class='col-md-6'>
-                    <p>{{ Lang::get('cachet.logged_in') }}</p>
-                </div>
-                <div class='col-md-6'>
-                    <div class='btn-group pull-right'>
-                        <a class="btn btn-default" href="{{ URL::route('dashboard') }}">{{ Lang::get('cachet.dashboard.dashboard') }}</a>
-                        <a class="btn btn-danger" href="{{ URL::route('logout') }}">{{ Lang::get('cachet.logout') }}</a>
-                    </div>
-                </div>
-            </div>
+    <div class='page-header text-right'>
+        <div class='btn-group'>
+            <a class="btn btn-default" href="{{ URL::route('dashboard') }}">{{ Lang::get('cachet.dashboard.dashboard') }}</a>
+            <a class="btn btn-danger" href="{{ URL::route('logout') }}">{{ Lang::get('cachet.logout') }}</a>
         </div>
     </div>
     @endif
