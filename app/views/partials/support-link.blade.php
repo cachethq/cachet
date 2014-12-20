933e24b42141f9f47c03e95ca09fd31188f9577d
James Brooks <jbrooksuk@me.com>
2014-12-20 17:29:18 +0000

    Show RSS link in the footer

--- app/views/partials/support-link.blade.php
+++ app/views/partials/support-link.blade.php
@@ -1,5 +1,6 @@
 @if(Setting::get('show_support'))
 <footer class='footer'>
     <p>{{ Lang::get('cachet.powered_by', array('app' => Setting::get('app_name'))) }}</p>
+    <p><a href='/rss'><i class='fa fa-rss'></i> RSS Feed</a></p>
 </footer>
 @endif
