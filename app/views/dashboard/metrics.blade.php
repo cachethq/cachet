950004a269f77164b355f9fbbe00a1f3a613fee0
James Brooks <jbrooksuk@me.com>
2014-12-20 20:34:43 +0000

    Add a bit of info text

--- app/views/dashboard/metrics.blade.php
+++ app/views/dashboard/metrics.blade.php
@@ -7,6 +7,7 @@
 	<div class="row">
 		<div class="col-sm-12">
 			<h3>Metrics</h3>
+			<p class='lead'>Eventually this page will show all of the graphs that make up your metrics.</p>
 		</div>
 	</div>
 @stop
