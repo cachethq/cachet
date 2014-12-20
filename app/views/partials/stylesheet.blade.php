d04da0ba78acecf1f358f745e2ad13f009120714
James Brooks <jbrooksuk@me.com>
2014-12-20 18:34:34 +0000

    Since we no longer ask for # in the setting, let's prefix

--- app/views/partials/stylesheet.blade.php
+++ app/views/partials/stylesheet.blade.php
@@ -1,10 +1,10 @@
 <style type='text/css'>
 body.status-page {
 	@if($styleBackgroundColor = Setting::get('style_background_color'))
-	background-color: {{ $styleBackgroundColor }};
+	background-color: #{{ $styleBackgroundColor }};
 	@endif
 	@if($styleTextColor = Setting::get('style_text_color'))
-	color: {{ $styleTextColor }};
+	color: #{{ $styleTextColor }};
 	@endif
 }
 </style>
