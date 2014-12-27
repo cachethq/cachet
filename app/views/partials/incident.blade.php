f8595973369a5276adea840bde85f0beaa0335e7
James Brooks <jbrooksuk@me.com>
2014-12-27 23:59:28 +0000

    Playing around with a new timeline view, may want restyling.

--- app/views/partials/incident.blade.php
+++ app/views/partials/incident.blade.php
@@ -1,23 +1,29 @@
-<ul class='list-group'>
-    <?php
-        $incidentDate = Carbon::now()->subDays($i);
-        $incidents = Incident::whereRaw('DATE(created_at) = "' . $incidentDate->format('Y-m-d') . '"')->orderBy('created_at', 'desc')->get();
-    ?>
-    <li class='list-group-item active'>
-        <h3 class='list-group-item-heading'>{{ $incidentDate->format('jS M Y') }}</h3>
-    </li>
-
-    @unless($incidents->count() > 0)
-    <li class='list-group-item'>{{ Lang::get('cachet.no_incidents') }}</li>
-    @endunless
-
-    @foreach($incidents as $incidentID => $incident)
-    <li class='list-group-item'>
-        <span class='badge badge-incident-{{ $incident->status }}'><i class='glyphicon {{ $incident->icon }}'></i></span>
-        <h4>{{ $incident->name }} <small><time>{{ $incident->created_at->format('H:i:s A') }}</time></small></h4>
-        {{ $incident->message }}
-        @if($incidentID < ($incident->count() - 1))
-    </li>
-    @endif
-    @endforeach
-</ul>
+<?php
+    $incidentDate = Carbon::now()->subDays($i);
+    $incidents = Incident::whereRaw('DATE(created_at) = "' . $incidentDate->format('Y-m-d') . '"')->orderBy('created_at', 'desc')->get();
+?>
+<h3>{{ $incidentDate->format('jS M Y') }}</h3>
+<div class='timeline'>
+    <div class='content-wrapper'>
+        @foreach($incidents as $incidentID => $incident)
+        <div class='moment {{ $incidentID === 0 ? "first" : null }}'>
+            <div class="row event clearfix">
+                <div class="col-sm-1">
+                    <div class="icon status-{{ $incident->status }}">
+                        <i class="{{ $incident->icon }}"></i>
+                    </div>
+                </div>
+                <div class="col-sm-11 message">
+                    <div class="content">
+                        <strong>{{ $incident->name }}</strong>
+                        <p>
+                            <span class='date'>{{ $incident->created_at->diffForHumans() }}</span>
+                            {{ $incident->message }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
