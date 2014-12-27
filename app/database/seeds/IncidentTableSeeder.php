e684826309c24a670c31cd3e8e88773eb3ccbf61
James Brooks <jbrooksuk@me.com>
2014-12-27 20:39:59 +0000

    component_id no longer needs to be set

--- app/database/seeds/IncidentTableSeeder.php
+++ app/database/seeds/IncidentTableSeeder.php
@@ -16,28 +16,28 @@ class IncidentTableSeeder extends Seeder
             [
                 "name"         => "Test Incident",
                 "message"      => "Something went wrong, oh noes.",
-                "component_id" => 1,
+                "component_id" => 0,
                 "user_id"      => 1,
             ],
             [
                 "name"         => "Update",
                 "message"      => "We've found the problem, so we're looking at it.",
                 "status"       => 2,
-                "component_id" => 1,
+                "component_id" => 0,
                 "user_id"      => 1,
             ],
             [
                 "name"         => "Monitoring the fix",
                 "message"      => "We're checking that our fix will first work.",
                 "status"       => 3,
-                "component_id" => 1,
+                "component_id" => 0,
                 "user_id"      => 1,
             ],
             [
                 "name"         => "Awesome",
                 "message"      => "We totally nailed the fix.",
                 "status"       => 4,
-                "component_id" => 2,
+                "component_id" => 0,
                 "user_id"      => 1,
             ],
         ];
