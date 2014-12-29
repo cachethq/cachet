141a7a8cd433780ddb0de4abbbb790340fd7494a
James Brooks <jbrooksuk@me.com>
2014-12-29 20:07:51 +0000

    Re-layout the settings page for inline theme settings

--- app/views/dashboard/settings.blade.php
+++ app/views/dashboard/settings.blade.php
@@ -1,24 +1,32 @@
 @extends('layout.dashboard')
 
 @section('content')
-	<div class="header">
+    <div class="header">
         <span class="uppercase">
-		    <i class="icon ion-gear-a"></i> {{ Lang::get('cachet.dashboard.settings') }}
+            <i class="icon ion-gear-a"></i> {{ Lang::get('cachet.dashboard.settings') }}
         </span>
-	</div>
+    </div>
     <div class="content-wrapper">
         <div class="row">
             <div class="col-sm-12">
                 <form name='SettingsForm' class='form-vertical' role='form' action='/dashboard/settings' method='POST'>
                     <h4 class="sub-header">Cachet Settings</h4>
                     <fieldset>
-                        <div class='form-group'>
-                            <label>Site Name</label>
-                            <input type='text' class='form-control' name='app_name' value='{{ Setting::get("app_name") }}' required />
+                        <div class='row'>
+                            <div class='col-xs-12'>
+                                <div class='form-group'>
+                                    <label>Site Name</label>
+                                    <input type='text' class='form-control' name='app_name' value='{{ Setting::get("app_name") }}' required />
+                                </div>
+                            </div>
                         </div>
-                        <div class='form-group'>
-                            <label>Site URL</label>
-                            <input type='text' class='form-control' name='app_domain' value='{{ Setting::get("app_domain") }}' required />
+                        <div class='row'>
+                            <div class='col-xs-12'>
+                                <div class='form-group'>
+                                    <label>Site URL</label>
+                                    <input type='text' class='form-control' name='app_domain' value='{{ Setting::get("app_domain") }}' required />
+                                </div>
+                            </div>
                         </div>
                     </fieldset>
 
@@ -27,11 +35,15 @@
 
                     <h4 class="sub-header">Security</h4>
                     <fieldset>
-                        <div class='form-group'>
-                            <label>Allowed Domains <em>Comma Seperated</em></label>
-                            <textarea class='form-control' name='allowed_domains' rows='5' placeholder='http://cachet.io, http://cachet.herokuapp.com'>{{ Setting::get('allowed_domains') }}</textarea>
-                            <div class='help-block'>
-                                The domain set above is automatically allowed by default.
+                        <div class='row'>
+                            <div class='col-xs-12'>
+                                <div class='form-group'>
+                                    <label>Allowed Domains <em>Comma Seperated</em></label>
+                                    <textarea class='form-control' name='allowed_domains' rows='5' placeholder='http://cachet.io, http://cachet.herokuapp.com'>{{ Setting::get('allowed_domains') }}</textarea>
+                                    <div class='help-block'>
+                                        The domain set above is automatically allowed by default.
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </fieldset>
@@ -41,33 +53,51 @@
 
                     <h4 class="sub-header">Theme</h4>
                     <fieldset>
-                        <div class='form-group'>
-                            <label>Background Color</label>
-                            <input type='text' class='form-control color-code' name='style.background_color' value='{{ Setting::get("style_background_color") }}' />
+                        <div class='row'>
+                            <div class='col-xs-6'>
+                                <div class='form-group'>
+                                    <label>Background Color</label>
+                                    <input type='text' class='form-control color-code' name='style.background_color' value='{{ Setting::get("style_background_color") }}' />
+                                </div>
                             </div>
-                        <div class='form-group'>
-                            <label>Text Color</label>
-                            <input type='text' class='form-control color-code' name='style.text_color' value='{{ Setting::get("style_text_color") }}' />
+                            <div class='col-xs-6'>
+                                <div class='form-group'>
+                                    <label>Text Color</label>
+                                    <input type='text' class='form-control color-code' name='style.text_color' value='{{ Setting::get("style_text_color") }}' />
+                                </div>
                             </div>
-                        <div class='form-group'>
-                            <label>Success Warning Color</label>
-                            <input type='text' class='form-control color-code' name='style.success_warning_color' value='{{ Setting::get("style_success_warning_color") }}' />
                         </div>
-                        <div class='form-group'>
-                            <label>Error Warning Color</label>
-                            <input type='text' class='form-control color-code' name='style.error_warning_color' value='{{ Setting::get("style_error_warning_color") }}' />
-                        </div>
-                        <div class='form-group'>
-                            <label>Info Warning Color</label>
-                            <input type='text' class='form-control color-code' name='style.style_info_warning_color' value='{{ Setting::get("style_info_warning_color") }}' />
+                        <div class='row'>
+                            <div class='col-xs-4'>
+                                <div class='form-group'>
+                                    <label>Success Warning Color</label>
+                                    <input type='text' class='form-control color-code' name='style.success_warning_color' value='{{ Setting::get("style_success_warning_color") }}' />
+                                </div>
+                            </div>
+                            <div class='col-xs-4'>
+                                <div class='form-group'>
+                                    <label>Error Warning Color</label>
+                                    <input type='text' class='form-control color-code' name='style.error_warning_color' value='{{ Setting::get("style_error_warning_color") }}' />
+                                </div>
+                            </div>
+                            <div class='col-xs-4'>
+                                <div class='form-group'>
+                                    <label>Info Warning Color</label>
+                                    <input type='text' class='form-control color-code' name='style.style_info_warning_color' value='{{ Setting::get("style_info_warning_color") }}' />
+                                </div>
+                            </div>
                         </div>
                     </fieldset>
 
                     <h4 class="sub-header">Stylesheet</h4>
                     <fieldset>
-                        <div class='form-group'>
-                            <label>Custom Stylesheet</label>
-                            <textarea class='form-control' name='stylesheet' rows='10'>{{ Setting::get('stylesheet') }}</textarea>
+                        <div class='row'>
+                            <div class='col-xs-12'>
+                                <div class='form-group'>
+                                    <label>Custom Stylesheet</label>
+                                    <textarea class='form-control' name='stylesheet' rows='10'>{{ Setting::get('stylesheet') }}</textarea>
+                                </div>
+                            </div>
                         </div>
                     </fieldset>
 
