9941727d44399e52c2b7aff754bce401ab5c2216
James Brooks <james@bluebaytravel.co.uk>
2014-12-30 14:18:24 +0000

    Remove my author link

--- app/views/partials/dashboard/head.blade.php
+++ app/views/partials/dashboard/head.blade.php
@@ -2,7 +2,6 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="author" content="http://james-brooks.uk">
     <meta name="apple-mobile-web-app-capable" content="yes">
 
     <title>{{ isset($pageTitle) ? $pageTitle : Setting::get('app_name') }} | Cachet</title>
