3c9478cdc422aefa13bdc2c6e7d11da930069569
James Brooks <james@bluebaytravel.co.uk>
2014-12-31 12:02:04 +0000

    Settings should hold LONGTEXT for app banners

---
+++ app/database/migrations/2014_12_31_115936_AlterTableSettingsValueLongText.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTableSettingsValueLongText extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+	    DB::statement("ALTER TABLE settings CHANGE `value` `value` LONGTEXT;");
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		DB::statement("ALTER TABLE settings CHANGE `value` `value` TEXT;");
+	}
+
+}
