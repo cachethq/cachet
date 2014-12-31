e5a432517718dd6c831e7c0101f6906ea8f64f23
Graham Campbell <graham@mineuk.com>
2014-12-31 12:08:43 +0000

    CS fix

--- app/database/migrations/2014_12_31_115936_AlterTableSettingsValueLongText.php
+++ app/database/migrations/2014_12_31_115936_AlterTableSettingsValueLongText.php
@@ -1,28 +1,26 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterTableSettingsValueLongText extends Migration {
+class AlterTableSettingsValueLongText extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("ALTER TABLE settings CHANGE `value` `value` LONGTEXT;");
+    }
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-	    DB::statement("ALTER TABLE settings CHANGE `value` `value` LONGTEXT;");
-	}
-
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		DB::statement("ALTER TABLE settings CHANGE `value` `value` TEXT;");
-	}
-
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement("ALTER TABLE settings CHANGE `value` `value` TEXT;");
+    }
 }
