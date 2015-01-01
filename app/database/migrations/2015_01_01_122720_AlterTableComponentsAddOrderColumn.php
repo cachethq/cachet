46d5b74d2e01da071200344bacde3151ae8cf89f
Graham Campbell <graham@mineuk.com>
2015-01-01 13:42:27 +0000

    CS fixes

--- app/database/migrations/2015_01_01_122720_AlterTableComponentsAddOrderColumn.php
+++ app/database/migrations/2015_01_01_122720_AlterTableComponentsAddOrderColumn.php
@@ -1,33 +1,31 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
 class AlterTableComponentsAddOrderColumn extends Migration
 {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('components', function (Blueprint $table) {
+            $table->tinyInteger('order')->default(0)->after('status');
+        });
+    }
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::table('components', function(Blueprint $table) {
-			$table->tinyInteger('order')->default(0)->after('status');
-		});
-	}
-
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::table('components', function(Blueprint $table) {
-			$table->dropColumn('order');
-		});
-	}
-
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('components', function (Blueprint $table) {
+            $table->dropColumn('order');
+        });
+    }
 }
