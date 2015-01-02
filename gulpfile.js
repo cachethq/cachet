152dda8b46058dc8a7a84d04d3394826aee80326
James Brooks <james@bluebaytravel.co.uk>
2015-01-02 14:29:07 +0000

    Finally fixed the Gulp build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,39 @@
 var elixir = require('laravel-elixir');
-var gulp = require('gulp');
-var changed = require('gulp-changed');
-var jshint = require('gulp-jshint');
-var stylish = require('jshint-stylish');
+var gulp   = require('gulp');
+var jshint = require('laravel-elixir-jshint');
+var rimraf = require('gulp-rimraf');
 
-/**
- * Custom Elixir job for copying images, but only if they're newer.
- */
-elixir.extend('images', function() {
-    gulp.task('copy-images', function() {
-        gulp.src('app/assets/bower_components/jquery/dist/jquery.min.map')
-            .pipe(changed('public/build/js/jquery.min.map', {
-                hasChanged: changed.compareSha1Digest
-            }))
-            .pipe(gulp.dest('public/build/js'));
-
-        gulp.src('app/assets/bower_components/ionicons/fonts/**')
-            .pipe(changed('public/build/fonts/ionicons', {
-                hasChanged: changed.compareSha1Digest
-            }))
-            .pipe(gulp.dest('public/build/fonts'));
+elixir.extend('rm', function(source) {
+    gulp.task('rm', function() {
+        gulp.src(source, { read: false })
+            .pipe(rimraf());
     });
 
-    return this.queueTask('copy-images');
+    return this.queueTask('rm');
 });
 
-/**
- * Run JSHint on our JavaScript sources.
- */
-elixir.extend('jshint', function() {
-    gulp.task('jshint', function() {
-        gulp.src('app/assets/js/**')
-            .pipe(jshint())
-            .pipe(jshint.reporter(stylish))
-            .pipe(jshint.reporter('fail'));
+gulp.task('default', function() {
+    elixir(function (mix) {
+        mix.sass('app/assets/sass/main.scss')
+            .rm('public/build/fonts')
+            .jshint('app/assets/js/*.js')
+            .styles([
+                'app/assets/bower_components/ionicons/css/ionicons.min.css',
+                'app/assets/bower_components/jquery-minicolors/jquery.minicolors.css',
+                'public/css/main.css',
+            ], './')
+            .scripts([
+                'bower_components/jquery/dist/jquery.js',
+                'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
+                'bower_components/lodash/dist/lodash.js',
+                'bower_components/messenger/build/js/messenger.js',
+                'bower_components/Sortable/Sortable.js',
+                'bower_components/jquery-minicolors/jquery.minicolors.js',
+                'bower_components/jquery-serialize-object/jquery.serialize-object.js',
+                'js/app.js',
+                'js/**/*.js',
+            ], './app/assets/')
+            .version(['public/css/all.css', 'public/js/all.js'])
+            .copy('app/assets/bower_components/ionicons/fonts', 'public/build/fonts');
     });
-
-    return this.queueTask('jshint');
-});
-
-elixir(function (mix) {
-    mix.sass('app/assets/sass/main.scss')
-        .styles([
-            'app/assets/bower_components/ionicons/css/ionicons.min.css',
-            'app/assets/bower_components/jquery-minicolors/jquery.minicolors.css',
-            'public/css/main.css',
-        ], './')
-        .scripts([
-            'bower_components/jquery/dist/jquery.min.js',
-            'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
-            'bower_components/lodash/dist/lodash.js',
-            'bower_components/messenger/build/js/messenger.js',
-            // 'bower_components/chartjs/Chart.min.js',
-            'bower_components/Sortable/Sortable.js',
-            'bower_components/jquery-minicolors/jquery.minicolors.js',
-            'bower_components/jquery-serialize-object/jquery.serialize-object.js',
-            'js/app.js',
-            'js/**/*.js',
-        ], './app/assets/')
-        .jshint()
-        .images()
-        .version(['public/css/all.css', 'public/js/all.js']);
 });
