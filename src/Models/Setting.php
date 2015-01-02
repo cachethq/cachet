05d0fcadffa036d84615bb6b300d897911dbe01f
James Brooks <james@bluebaytravel.co.uk>
2015-01-02 08:42:19 +0000

    Makes scrutinizer happy

--- src/Models/Setting.php
+++ src/Models/Setting.php
@@ -5,6 +5,10 @@ namespace CachetHQ\Cachet\Models;
 use ErrorException;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string $name
+ * @property string $value
+ */
 class Setting extends Model
 {
     /**
