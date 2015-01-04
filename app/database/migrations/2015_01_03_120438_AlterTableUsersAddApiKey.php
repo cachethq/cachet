f00ba2efda7f47bda73fa7c8affcbcfce8425841
James Brooks <jbrooksuk@me.com>
2015-01-04 21:04:24 +0000

    Nullable, default null api_key

--- app/database/migrations/2015_01_03_120438_AlterTableUsersAddApiKey.php
+++ app/database/migrations/2015_01_03_120438_AlterTableUsersAddApiKey.php
@@ -14,7 +14,7 @@ class AlterTableUsersAddApiKey extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('api_key')->after('email');
+            $table->string('api_key')->nullable()->default(null)->after('email');
         });
     }
 
