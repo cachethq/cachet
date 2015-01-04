c2c78caa3fd08e837f8711f7449a8fc6ca90c360
James Brooks <jbrooksuk@me.com>
2015-01-04 19:27:01 +0000

    Closes #265

--- app/views/dashboard/index.blade.php
+++ app/views/dashboard/index.blade.php
@@ -14,12 +14,12 @@
                     <div class='list-group'>
                         @forelse($components as $component)
                         <div class='list-group-item'>
-                            {{ Form::open(['class' => 'component-inline', 'data-messenger' => 'Component updated.']) }}
+                            {{ Form::open(['class' => 'component-inline form-horizontal', 'data-messenger' => 'Component updated.']) }}
                             <div class='row striped-list-item'>
-                                <div class='col-lg-2 col-xs-12'>
+                                <div class='col-lg-4 col-sm-12'>
                                     <strong>{{ $component->name }}</strong>
                                 </div>
-                                <div class='col-lg-10 col-xs-12 text-right'>
+                                <div class='col-lg-8 col-sm-12 radio-items'>
                                     @foreach(trans('cachet.component.status') as $statusID => $status)
                                     <div class='radio-inline'>
                                         <label>
