d212aa8a4d7659a9aec7d2544d461589914ee34a
Valentin PRUGNAUD <valentin@whatdafox.com>
2015-01-05 01:06:04 +0000

    Move all static text into the language file
    Signed-off-by: Graham Campbell <graham@mineuk.com>

---
+++ app/lang/en/setup.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title'             => 'Setup Cachet',
+    'service_details'   => 'Service Details',
+    'status_page_setup' => 'Status Page Setup',
+    'show_support'      => 'Show support for Cachet?',
+    'admin_account'     => 'Administrator Account',
+    'complete_setup'    => 'Complete Setup'
+];
