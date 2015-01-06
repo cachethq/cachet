695c0ca2ca2066048499bd17e09148a44cfb306d
James Brooks <jbrooksuk@me.com>
2015-01-06 19:31:22 +0000

    Update French translation

--- app/lang/fr/setup.php
+++ app/lang/fr/setup.php
@@ -5,7 +5,7 @@ return [
     'title'             => 'Installer Cachet',
     'service_details'   => 'Details du service',
     'status_page_setup' => 'Installation de la page de statut',
-    'show_support'      => 'Montrer votre support pour Cachet ?',
+    'show_support'      => 'Show support for Cachet? <small>Places a link in the footer linking to Cachet.</small>',
     'admin_account'     => 'Compte administrateur',
     'complete_setup'    => 'Terminer l\'installation'
 ];
