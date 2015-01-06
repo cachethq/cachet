b29018dfad30bb3b079ea36c95256e079d468796
William Durand <will+git@drnd.me>
2015-01-06 22:51:03 +0100

    [French] Wording

--- app/lang/fr/setup.php
+++ app/lang/fr/setup.php
@@ -5,7 +5,7 @@ return [
     'title'             => 'Installer Cachet',
     'service_details'   => 'Details du service',
     'status_page_setup' => 'Installation de la page de statut',
-    'show_support'      => 'Montrer votre support pour Cachet? <small>Affiche un lien vers Cachet dans le pied de page.</small>',
+    'show_support'      => 'Montrer votre soutien envers Cachet ? <small>Affiche un lien vers Cachet en bas de page.</small>',
     'admin_account'     => 'Compte administrateur',
     'complete_setup'    => 'Terminer l\'installation',
     'completed'         => 'Cachet a été configuré avec succès !',
