a6cdfaf63f6d20f989be311d76e386cb8f41c421
James Brooks <james@bluebaytravel.co.uk>
2015-01-06 15:41:47 +0000

    Prefix all dashboard routes for better grouping

--- app/routes/dashboard.php
+++ app/routes/dashboard.php
@@ -8,98 +8,112 @@ Route::group(['before' => 'auth', 'prefix' => 'dashboard', 'namespace' => 'Cache
     ]);
 
     // Components
-    Route::get('components', [
-        'as'   => 'dashboard.components',
-        'uses' => 'DashComponentController@showComponents',
-    ]);
-    Route::get('components/add', [
-        'as'   => 'dashboard.components.add',
-        'uses' => 'DashComponentController@showAddComponent',
-    ]);
-    Route::post('components/add', 'DashComponentController@createComponentAction');
-    Route::get('components/groups', [
-        'as'   => 'dashboard.components.groups',
-        'uses' => 'DashComponentController@showComponentGroups',
-    ]);
-    Route::get('components/groups/add', [
-        'as'   => 'dashboard.components.groups.add',
-        'uses' => 'DashComponentController@showAddComponentGroup',
-    ]);
-    Route::post('components/groups/add', 'DashComponentController@postAddComponentGroup');
-    Route::get('components/{component}/delete', 'DashComponentController@deleteComponentAction');
-    Route::get('components/{component}/edit', 'DashComponentController@showEditComponent');
-    Route::post('components/{component}/edit', 'DashComponentController@updateComponentAction');
+    Route::group(['prefix' => 'components'], function() {
+        Route::get('/', [
+            'as'   => 'dashboard.components',
+            'uses' => 'DashComponentController@showComponents',
+        ]);
+        Route::get('add', [
+            'as'   => 'dashboard.components.add',
+            'uses' => 'DashComponentController@showAddComponent',
+        ]);
+        Route::post('add', 'DashComponentController@createComponentAction');
+        Route::get('groups', [
+            'as'   => 'dashboard.components.groups',
+            'uses' => 'DashComponentController@showComponentGroups',
+        ]);
+        Route::get('groups/add', [
+            'as'   => 'dashboard.components.groups.add',
+            'uses' => 'DashComponentController@showAddComponentGroup',
+        ]);
+        Route::post('groups/add', 'DashComponentController@postAddComponentGroup');
+        Route::get('{component}/delete', 'DashComponentController@deleteComponentAction');
+        Route::get('{component}/edit', 'DashComponentController@showEditComponent');
+        Route::post('{component}/edit', 'DashComponentController@updateComponentAction');
+    });
 
     // Incidents
-    Route::get('incidents', [
-        'as'   => 'dashboard.incidents',
-        'uses' => 'DashIncidentController@showIncidents',
-    ]);
-    Route::get('incidents/add', [
-        'as'   => 'dashboard.incidents.add',
-        'uses' => 'DashIncidentController@showAddIncident',
-    ]);
-    Route::post('incidents/add', 'DashIncidentController@createIncidentAction');
-    Route::get('incidents/{incident}/delete', 'DashIncidentController@deleteIncidentAction');
-    Route::get('incidents/{incident}/edit', 'DashIncidentController@showEditIncidentAction');
-    Route::post('incidents/{incident}/edit', 'DashIncidentController@editIncidentAction');
-    Route::get('incidents/template', [
-        'as'   => 'dashboard.incidents.template',
-        'uses' => 'DashIncidentController@showAddIncidentTemplate',
-    ]);
-    Route::post('incidents/template', 'DashIncidentController@createIncidentTemplateAction');
+    Route::group(['prefix' => 'incidents'], function() {
+        Route::get('/', [
+            'as'   => 'dashboard.incidents',
+            'uses' => 'DashIncidentController@showIncidents',
+        ]);
+        Route::get('add', [
+            'as'   => 'dashboard.incidents.add',
+            'uses' => 'DashIncidentController@showAddIncident',
+        ]);
+        Route::post('add', 'DashIncidentController@createIncidentAction');
+        Route::get('{incident}/delete', 'DashIncidentController@deleteIncidentAction');
+        Route::get('{incident}/edit', 'DashIncidentController@showEditIncidentAction');
+        Route::post('{incident}/edit', 'DashIncidentController@editIncidentAction');
+        Route::get('template', [
+            'as'   => 'dashboard.incidents.template',
+            'uses' => 'DashIncidentController@showAddIncidentTemplate',
+        ]);
+        Route::post('template', 'DashIncidentController@createIncidentTemplateAction');
+    });
 
     // Metrics
-    Route::get('metrics', [
-        'as'   => 'dashboard.metrics',
-        'uses' => 'DashboardController@showMetrics',
-    ]);
+    Route::group(['prefix' => 'metrics'], function() {
+        Route::get('/', [
+            'as'   => 'dashboard.metrics',
+            'uses' => 'DashboardController@showMetrics',
+        ]);
+    });
 
     // Notifications
-    Route::get('notifications', [
-        'as'   => 'dashboard.notifications',
-        'uses' => 'DashboardController@showNotifications',
-    ]);
+    Route::group(['prefix' => 'notifications'], function() {
+        Route::get('/', [
+            'as'   => 'dashboard.notifications',
+            'uses' => 'DashboardController@showNotifications',
+        ]);
+    });
 
     // Team Members
-    Route::get('team', [
-        'as'   => 'dashboard.team',
-        'uses' => 'DashboardController@showTeamView',
-    ]);
-    Route::get('team/add', [
-        'as'   => 'dashboard.team.add',
-        'uses' => 'DashboardController@showAddTeamMemberView'
-    ]);
-    Route::get('team/{user}', 'DashboardController@showTeamMemberView');
-    Route::post('team/add', 'DashboardController@postAddUser');
-    Route::post('team/{user}', 'DashboardController@postUpdateUser');
+    Route::group(['prefix' => 'team'], function() {
+        Route::get('/', [
+            'as'   => 'dashboard.team',
+            'uses' => 'DashboardController@showTeamView',
+        ]);
+        Route::get('add', [
+            'as'   => 'dashboard.team.add',
+            'uses' => 'DashboardController@showAddTeamMemberView'
+        ]);
+        Route::get('{user}', 'DashboardController@showTeamMemberView');
+        Route::post('add', 'DashboardController@postAddUser');
+        Route::post('{user}', 'DashboardController@postUpdateUser');
+    });
 
     // Settings
-    Route::get('settings/setup', [
-        'as'   => 'dashboard.settings.setup',
-        'uses' => 'DashSettingsController@showSetupView',
-    ]);
-    Route::get('settings/security', [
-        'as'   => 'dashboard.settings.security',
-        'uses' => 'DashSettingsController@showSecurityView',
-    ]);
-    Route::get('settings/theme', [
-        'as'   => 'dashboard.settings.theme',
-        'uses' => 'DashSettingsController@showThemeView',
-    ]);
-    Route::get('settings/stylesheet', [
-        'as'   => 'dashboard.settings.stylesheet',
-        'uses' => 'DashSettingsController@showStylesheetView',
-    ]);
-    Route::post('settings', 'DashSettingsController@postSettings');
+    Route::group(['prefix' => 'settings'], function() {
+        Route::get('setup', [
+            'as'   => 'dashboard.settings.setup',
+            'uses' => 'DashSettingsController@showSetupView',
+        ]);
+        Route::get('security', [
+            'as'   => 'dashboard.settings.security',
+            'uses' => 'DashSettingsController@showSecurityView',
+        ]);
+        Route::get('theme', [
+            'as'   => 'dashboard.settings.theme',
+            'uses' => 'DashSettingsController@showThemeView',
+        ]);
+        Route::get('stylesheet', [
+            'as'   => 'dashboard.settings.stylesheet',
+            'uses' => 'DashSettingsController@showStylesheetView',
+        ]);
+        Route::post('/', 'DashSettingsController@postSettings');
+    });
 
     // User Settings
-    Route::get('user', [
-        'as'   => 'dashboard.user',
-        'uses' => 'DashUserController@showUser',
-    ]);
-    Route::get('user/{user}/api/regen', 'DashUserController@regenerateApiKey');
-    Route::post('user', 'DashUserController@postUser');
+    Route::group(['prefix' => 'user'], function() {
+        Route::get('/', [
+            'as'   => 'dashboard.user',
+            'uses' => 'DashUserController@showUser',
+        ]);
+        Route::post('/', 'DashUserController@postUser');
+        Route::get('{user}/api/regen', 'DashUserController@regenerateApiKey');
+    });
 
     // Internal API.
     // This should only be used for making requests within the dashboard.
