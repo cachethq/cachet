0c3e13480e9cb0e73e676ce64ad2f92091229b1f
Joseph Cohen <joseph.cohen@dinkbit.com>
2015-01-09 00:59:46 -0600

    Added steps to configure

--- app/lang/de/dashboard.php
+++ app/lang/de/dashboard.php
@@ -128,4 +128,21 @@ return [
         'whoops'        => 'Oops.',
     ],
 
+        // Configuration
+    'configure' => [
+        'configure' => 'Your Status Page is almost ready!',
+        'steps'     => [
+            'setup'             => 'Setup your Cachet Status Page',
+            'setup-button'      => 'Setup Cachet',
+            'login'             => 'Login to your dashboard',
+            'login-button'      => 'Login dashboard',
+            'component'         => 'Create and configure components for your Status Page',
+            'component-button'  => 'Add component',
+            'incident'          => 'Create your first incident',
+            'incident-button'   => 'Add incident',
+            'customize'         => 'Customize your Cachet Status Page with theme settings.',
+            'customize-button'  => 'Customize',
+        ],
+    ],
+
 ];
