600445dbeb5943b240a281b3c6216f11f6268871
James Brooks <jbrooksuk@me.com>
2015-01-17 19:44:39 +0000

    Add FixPermissionsCommand. Closes #397

--- src/Providers/ConsoleServiceProvider.php
+++ src/Providers/ConsoleServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace CachetHQ\Cachet\Providers;
 
+use CachetHQ\Cachet\Console\Commands\FixPermissionsCommand;
 use CachetHQ\Cachet\Console\Commands\OneClickDeployCommand;
 use Illuminate\Support\ServiceProvider;
 
@@ -14,6 +15,7 @@ class ConsoleServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->commands('CachetHQ\Cachet\Console\Commands\FixPermissionsCommand');
         $this->commands('CachetHQ\Cachet\Console\Commands\OneClickDeployCommand');
     }
 
@@ -24,6 +26,20 @@ class ConsoleServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->app->singleton('CachetHQ\Cachet\Console\Commands\FixPermissionsCommand', function ($app) {
+            $storageDirectory = $app->path['storage'];
+            $databaseDirectory = $app->path['database'];
+            $databasePath = $app->config->get('database.connections.sqlite.database');
+            $databaseDefault = $app->config->get('database.default');
+
+            return new FixPermissionsCommand(
+                $storageDirectory,
+                $databaseDirectory,
+                $databasePath,
+                $databaseDefault
+            );
+        });
+
         $this->app->singleton('CachetHQ\Cachet\Console\Commands\OneClickDeployCommand', function ($app) {
             return new OneClickDeployCommand($app->environment('heroku'));
         });
