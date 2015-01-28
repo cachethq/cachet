3405ef5eb75f1aa19403971bd0f178c056e46d74
Joseph Cohen <joseph.cohen@dinkbit.com>
2015-01-28 03:05:22 -0600

    Remove unnecessary form incidents day

--- src/Http/Controllers/HomeController.php
+++ src/Http/Controllers/HomeController.php
@@ -56,7 +56,7 @@ class HomeController extends Controller
             ])->orderBy('created_at', 'desc')->get();
 
             $allIncidents[] = [
-                'date'      => (new Date($date->toDateTimeString()))->format($dateFormat),
+                'date'      => (new Date($date->toDateString()))->format($dateFormat),
                 'incidents' => $incidents,
             ];
         }
