36081434898bb1cce08dfc4e3de857f8764d4954
James Brooks <jbrooksuk@me.com>
2015-02-28 22:35:05 +0000

    Fix some CS issus

--- app/database/migrations/2015_02_28_214642_UpdateIncidentsAddScheduledAt.php
+++ app/database/migrations/2015_02_28_214642_UpdateIncidentsAddScheduledAt.php
@@ -1,36 +1,32 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 class UpdateIncidentsAddScheduledAt extends Migration
 {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('incidents', function (Blueprint $table) {
+            $table->timestamp('scheduled_at')->after('user_id')->nullable()->default(null);
+        });
+    }
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::table('incidents', function(Blueprint $table)
-		{
-			$table->timestamp('scheduled_at')->after('user_id')->nullable()->default(null);
-		});
-	}
-
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::table('incidents', function(Blueprint $table)
-		{
-			$table->dropColumn('scheduled_at');
-		});
-	}
-
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('incidents', function (Blueprint $table) {
+            $table->dropColumn('scheduled_at');
+        });
+    }
 }
