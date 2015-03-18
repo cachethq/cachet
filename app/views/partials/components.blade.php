39f2bc1021140e1607cece7295a4b1b2542c47d8
James Brooks <jbrooksuk@me.com>
2015-03-18 21:51:56 +0000

    Restyle the component list slightly

--- app/views/partials/components.blade.php
+++ app/views/partials/components.blade.php
@@ -2,23 +2,19 @@
 <ul class="list-group components">
     @foreach($components as $component)
     <li class="list-group-item component">
-        <div class="row">
-            <div class="col-xs-10">
-                <h4>
-                    {{ $component->name }}
-                    @if($component->description)
-                    <i class="ion-ios-help-outline help-icon" data-toggle="tooltip" data-title="{{ $component->description }}"></i>
-                    @endif
-                    <small class="text-component-{{ $component->status }}">{{ $component->humanStatus }}</small>
-                </h4>
-            </div>
-            <div class="col-xs-2 text-right">
-                <h4>
-                    @if($component->link)
-                    <a href="{{ $component->link }}" target="_blank"><i class="ion ion-link"></i></a>
-                    @endif
-                </h4>
-            </div>
+        <strong>
+            @if($component->link)
+            <a href="{{ $component->link }}" target="_blank">{{ $component->name }}</a>
+            @else
+            {{ $component->name }}
+            @endif
+
+            @if($component->description)
+            <i class="ion-ios-help-outline help-icon" data-toggle="tooltip" data-title="{{ $component->description }}"></i>
+            @endif
+        </strong>
+        <div class="pull-right">
+            <small class="text-component-{{ $component->status }}">{{ $component->humanStatus }}</small>
         </div>
     </li>
     @endforeach
