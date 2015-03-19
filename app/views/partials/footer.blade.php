7cfa158e689e8de8554e9b875e520248a2f86f8c
James Brooks <jbrooksuk@me.com>
2015-03-19 23:53:59 +0000

    Always show a link to the dashboard. Closes #526.

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -3,11 +3,13 @@
     <p>{{ trans('cachet.powered_by', ['app' => Setting::get('app_name')]) }}</p>
     @endif
     <p><a href="/rss"><i class="ion-social-rss"></i> {{ trans('cachet.rss-feed') }}</a>&nbsp;-&nbsp;<a href="/atom"><i class="ion-social-rss"></i> {{ trans('cachet.atom-feed') }}</a></p>
-    @if($loggedUser)
     <p>
-        <a href="{{ route('dashboard') }}">{{ trans('dashboard.dashboard') }}</a> &ndash; <a href="{{ route('logout') }}">{{ trans('dashboard.logout') }}</a>
+        <a href="{{ route('dashboard') }}">{{ trans('dashboard.dashboard') }}</a>
+        @if($loggedUser)
+        &ndash;
+        <a href="{{ route('logout') }}">{{ trans('dashboard.logout') }}</a>
+        @endif
     </p>
-    @endif
 </footer>
 
 @include("partials.analytics")
