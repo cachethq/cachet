5e907ba04f2aa596c861c9fb5e55812010ee68a0
James Brooks <james@bluebaytravel.co.uk>
2015-03-24 08:52:28 +0000

    Component groups are now properly removed. Closes #535

--- app/database/migrations/2015_01_05_201324_CreateComponentGroupsTable.php
+++ app/database/migrations/2015_01_05_201324_CreateComponentGroupsTable.php
@@ -17,7 +17,6 @@ class CreateComponentGroupsTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
