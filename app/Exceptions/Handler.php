0f44fab68cbd8121d55aa453572a8230d0300dd0
Graham Campbell <graham@mineuk.com>
2015-03-30 21:48:57 +0100

    More specific docblock

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -11,7 +11,7 @@ class Handler extends ExceptionHandler
     /**
      * A list of the exception types that should not be reported.
      *
-     * @var array
+     * @var string[]
      */
     protected $dontReport = [
         'Symfony\Component\HttpKernel\Exception\HttpException',
