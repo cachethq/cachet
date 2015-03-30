2934f9be67086314d9749c98b3e7cbe4d9ced2b2
Paweł Pierścionek <ppierscionek@gmail.com>
2015-03-30 23:46:06 +0000

    PL Translation

---
+++ app/lang/pl/reminders.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Hasła muszą mieć przynajmniej 6 znaków i być pasować do potwierdzenia.',
+    'user'     => 'Nie możemy znaleźć użytkownia o tym adresie email.',
+    'token'    => 'Token resetu hasła jest nieprawidłowyw.',
+    'sent'     => 'Wysłano przypomnienie hasła!',
+    'reset'    => 'Hasło zostało zresetowane!',
+
+];
---
+++ app/lang/pl/setup.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'setup'             => 'Konfiguracja',
+    'title'             => 'Skonfiguruj Cachet',
+    'service_details'   => 'Szczegóły serwisu',
+    'status_page_setup' => 'Konfiguracja strony statusowej',
+    'show_support'      => 'Okaż wsparcie dla Cachet? <small>Umieść link w stopce wskazujący na Cachet.</small>',
+    'admin_account'     => 'Konto administratora',
+    'complete_setup'    => 'Konfiguracja zakończona',
+    'completed'         => 'Cachet został skonfigurowany pomyślnie!',
+    'finish_setup'      => 'Przejdź do panelu',
+    'allow_tracking'    => 'Zezwolić na anonimowe śledzenie wykorzystania?',
+];
