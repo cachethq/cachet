e61cae2dddf273b45a90c8d6c2aaff2e625828f1
James Brooks <jbrooksuk@me.com>
2015-03-31 21:29:27 +0100

    Fix output of HTML from support Cachet link

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer">
     @if(Setting::get('show_support'))
-    <p>{{ trans('cachet.powered_by', ['app' => Setting::get('app_name')]) }}</p>
+    <p>{!! trans('cachet.powered_by', ['app' => Setting::get('app_name')]) !!}</p>
     @endif
     <p><a href="/rss"><i class="ion-social-rss"></i> {{ trans('cachet.rss-feed') }}</a>&nbsp;-&nbsp;<a href="/atom"><i class="ion-social-rss"></i> {{ trans('cachet.atom-feed') }}</a></p>
     <p>
