51ae02992e2495faef75ff265ad71611bfad7b40
James Brooks <jbrooksuk@me.com>
2015-04-19 09:18:16 +0100

    Fix copyright info

--- config/htmlmin.php
+++ config/htmlmin.php
@@ -1,10 +1,9 @@
 <?php
 
 /*
- * This file is part of StyleCI.
+ * This file is part of Cachet.
  *
- * (c) Graham Campbell <graham@mineuk.com>
- * (c) Joseph Cohen <joseph.cohen@dinkbit.com>
+ * (c) James Brooks <james@cachethq.io>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
