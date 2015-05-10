0569142f84b4948d70cdc5a27a5c8e211cf388ed
James Brooks <jbrooksuk@me.com>
2015-05-10 19:41:33 +0100

    Update the Incident model attributes

--- app/Models/Incident.php
+++ app/Models/Incident.php
@@ -20,14 +20,15 @@ use Watson\Validating\ValidatingTrait;
 /**
  * @property int            $id
  * @property int            $component_id
- * @property int            $user_id
  * @property string         $name
  * @property int            $status
  * @property string         $message
- * @property string         $humanStatus
+ * @property int            $user_id
+ * @property \Carbon\Carbon $scheduled_at
  * @property \Carbon\Carbon $created_at
  * @property \Carbon\Carbon $updated_at
  * @property \Carbon\Carbon $deleted_at
+ * @property string         $humanStatus
  */
 class Incident extends Model implements HasPresenter
 {
