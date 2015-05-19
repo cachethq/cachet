c04ac4a5e3157697d3d085bcdcb34ff17cf1816f
James Brooks <jbrooksuk@me.com>
2015-05-19 19:30:16 +0100

    Fix RedirectIfAuthenticate middleware redirecting to wrong location

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -47,7 +47,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next)
     {
         if ($this->auth->check()) {
-            return new RedirectResponse(url('/home'));
+            return new RedirectResponse(route('dashboard'));
         }
 
         return $next($request);
