0928251673e94f617028b4abf0f46641f2ba910b
James Brooks <james@bluebaytravel.co.uk>
2015-05-19 10:45:38 +0100

    Add Joseph Cohem and Graham Campbell to the copyright headers.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,8 @@
  * This file is part of Cachet.
  *
  * (c) James Brooks <james@cachethq.io>
+ * (c) Joseph Cohen <joseph.cohen@dinkbit.com>
+ * (c) Graham Campbell <graham@mineuk.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
