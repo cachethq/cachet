91e4dafca9690b238a665bd6d2a2178311c7c336
Graham Campbell <graham@cachethq.io>
2015-05-23 14:48:33 +0100

    Updated my email address

--- resources/lang/pt-BR/cachet.php
+++ resources/lang/pt-BR/cachet.php
@@ -5,7 +5,7 @@
  *
  * (c) James Brooks <james@cachethq.io>
  * (c) Joseph Cohen <joseph.cohen@dinkbit.com>
- * (c) Graham Campbell <graham@mineuk.com>
+ * (c) Graham Campbell <graham@cachethq.io>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
