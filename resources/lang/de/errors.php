b06167f5e9fec6bc49b172f0393e210bc60c970e
Graham Campbell <graham@cachethq.io>
2015-05-25 17:53:06 +0100

    Updated joe's email address

--- resources/lang/de/errors.php
+++ resources/lang/de/errors.php
@@ -4,7 +4,7 @@
  * This file is part of Cachet.
  *
  * (c) James Brooks <james@cachethq.io>
- * (c) Joseph Cohen <joseph.cohen@dinkbit.com>
+ * (c) Joseph Cohen <joe@cachethq.io>
  * (c) Graham Campbell <graham@cachethq.io>
  *
  * For the full copyright and license information, please view the LICENSE
