3988753b9926bbecf71fae90d8881f3bc4d5d216
Graham Campbell <graham@cachethq.io>
2015-05-25 17:59:08 +0100

    Updated copyright headers

--- app/Repositories/Metric/MetricRepository.php
+++ app/Repositories/Metric/MetricRepository.php
@@ -3,9 +3,7 @@
 /*
  * This file is part of Cachet.
  *
- * (c) James Brooks <james@cachethq.io>
- * (c) Joseph Cohen <joe@cachethq.io>
- * (c) Graham Campbell <graham@cachethq.io>
+ * (c) Cachet HQ <support@cachethq.io>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
