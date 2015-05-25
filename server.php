5709995b136a91a4ebd3f6d40b27781e20f59220
Graham Campbell <graham@cachethq.io>
2015-05-25 18:31:51 +0100

    Fixed server.php

--- server.php
+++ server.php
@@ -9,18 +9,8 @@
  * file that was distributed with this source code.
  */
 
-/**
- * Laravel - A PHP Framework For Web Artisans.
- *
- * @author   Taylor Otwell <taylorotwell@gmail.com>
- */
-$uri = urldecode(
-    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
-);
+$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
-// This file allows us to emulate Apache's "mod_rewrite" functionality from the
-// built-in PHP web server. This provides a convenient way to test a Laravel
-// application without having installed a "real" web server software here.
 if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
     return false;
 }
