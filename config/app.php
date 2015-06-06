6b98832f3b4db5a4ec18c5a7f9baa115774d0e38
James Brooks <jbrooksuk@me.com>
2015-06-06 21:48:52 +0100

    Use the right cipher

--- config/app.php
+++ config/app.php
@@ -89,7 +89,7 @@ return [
 
     'key' => env('APP_KEY', 'SomeRandomString'),
 
-    'cipher' => 'AES-128-CBC',
+    'cipher' => 'AES-256-CBC',
 
     /*
     |--------------------------------------------------------------------------
