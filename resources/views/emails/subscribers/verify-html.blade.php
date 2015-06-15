f867d22b964e8d9f7f4d060ec7b682bff8fac90b
James Brooks <jbrooksuk@me.com>
2015-06-15 20:22:01 +0100

    Pass through app_name to the preheader locale

--- resources/views/emails/subscribers/verify-html.blade.php
+++ resources/views/emails/subscribers/verify-html.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.emails')
 
 @section('preheader')
-{!! trans('cachet.subscriber.email.verify.html-preheader') !!}
+{!! trans('cachet.subscriber.email.verify.html-preheader', ['app_name' => Setting::get('app_name')]) !!}
 @stop
 
 @section('content')
