16cb1bd5452f32e14dbf11aec0de50b5042c3047
James Brooks <james@bluebaytravel.co.uk>
2015-06-15 16:55:02 +0100

    Improve scheduled maintenance design

--- resources/views/partials/schedule.blade.php
+++ resources/views/partials/schedule.blade.php
@@ -1,14 +1,16 @@
-<h1>{{ trans('cachet.incidents.scheduled') }}</h1>
 
 <div class="timeline schedule">
-    @foreach($scheduledMaintenance as $schedule)
     <div class="panel panel-info">
         <div class="panel-heading">
-            <strong>{{ $schedule->name }}</strong>, <small class="date"><abbr class="timeago" data-toggle="tooltip" data-placement="right" title="{{ $schedule->scheduled_at_formatted }}" data-timeago="{{ $schedule->scheduled_at_iso }}"></abbr></small>
+            <strong>{{ trans('cachet.incidents.scheduled') }}</strong>
         </div>
-        <div class="panel-body">
-            {!! $schedule->formattedMessage !!}
+        <div class="list-group">
+            @foreach($scheduledMaintenance as $schedule)
+            <div class="list-group-item">
+                <strong>{{ $schedule->name }}</strong> <small class="date"><abbr class="timeago" data-toggle="tooltip" data-placement="right" title="{{ $schedule->scheduled_at_formatted }}" data-timeago="{{ $schedule->scheduled_at_iso }}"></abbr></small>
+                {!! $schedule->formattedMessage !!}
+            </div>
+            @endforeach
         </div>
     </div>
-    @endforeach
 </div>
