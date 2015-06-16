f0768fff6ddd8669857909622f87505b6176f842
amond <amond@farmers.io>
2015-06-16 23:18:21 +0900

    add Korean translation

---
+++ resources/lang/ko/forms.php
@@ -0,0 +1,147 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Cachet HQ <support@cachethq.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+    // Setup form fields
+    'setup' => [
+        'email'            => 'Email',
+        'username'         => 'Username',
+        'password'         => 'Password',
+        'site_name'        => 'Site Name',
+        'site_domain'      => 'Site Domain',
+        'site_timezone'    => 'Select your timezone',
+        'site_locale'      => 'Select your language',
+        'enable_google2fa' => 'Enable Google Two Factor Authentication',
+        'cache_driver'     => 'Cache Driver',
+        'session_driver'   => 'Session Driver',
+    ],
+
+    // Login form fields
+    'login' => [
+        'email'         => 'Email',
+        'password'      => 'Password',
+        '2fauth'        => 'Authentication Code',
+        'invalid'       => 'Invalid email or password',
+        'invalid-token' => 'Invalid token',
+        'cookies'       => 'You must enable cookies to login.',
+    ],
+
+    // Incidents form fields
+    'incidents' => [
+        'name'               => 'Name',
+        'status'             => 'Status',
+        'component'          => 'Component',
+        'message'            => 'Message',
+        'message-help'       => 'You may also use Markdown.',
+        'scheduled_at'       => 'When to schedule the maintenance for?',
+        'incident_time'      => 'When did this incident occur?',
+        'notify_subscribers' => 'Notify subscribers',
+        'visibility'         => 'Incident Visibility',
+        'public'             => 'Viewable by public',
+        'logged_in_only'     => 'Only visible logged in users',
+        'templates'          => [
+            'name'     => 'Name',
+            'template' => 'Template',
+        ],
+    ],
+
+    // Components form fields
+    'components' => [
+        'name'        => 'Name',
+        'status'      => 'Status',
+        'group'       => 'Group',
+        'description' => 'Description',
+        'link'        => 'Link',
+        'tags'        => 'Tags',
+        'tags-help'   => 'Comma separated.',
+
+        'groups' => [
+            'name' => 'Name',
+        ],
+    ],
+
+    // Metric form fields
+    'metrics' => [
+        'name'             => 'Name',
+        'suffix'           => 'Suffix',
+        'description'      => 'Description',
+        'description-help' => 'You may also use Markdown.',
+        'display-chart'    => 'Display chart on status page?',
+        'default-value'    => 'Default value',
+        'calc_type'        => 'Calculation of metrics',
+        'type_sum'         => 'Sum',
+        'type_avg'         => 'Average',
+
+        'points' => [
+            'value' => 'Value',
+        ],
+    ],
+
+    // Settings
+    'settings' => [
+        /// Application setup
+        'app-setup' => [
+            'site-name'              => 'Site Name',
+            'site-url'               => 'Site URL',
+            'site-timezone'          => 'Site Timezone',
+            'site-locale'            => 'Site Language',
+            'date-format'            => 'Date Format',
+            'incident-date-format'   => 'Incident Timestamp Format',
+            'display-graphs'         => 'Display graphs on status page?',
+            'about-this-page'        => 'About this page',
+            'days-of-incidents'      => 'How many days of incidents to show?',
+            'banner'                 => 'Banner Image',
+            'banner-help'            => "It's recommended that you upload files no bigger than 930px wide .",
+            'analytics_google'       => 'Google Analytics code',
+            'analytics_gosquared'    => 'GoSquared Analytics code',
+            'analytics_piwik_url'    => 'URL of your Piwik instance (without http(s)://)',
+            'analytics_piwik_siteid' => 'Piwik\'s site id',
+            'subscribers'            => 'Allow people to signup to email notifications?',
+        ],
+        'security' => [
+            'allowed-domains'      => 'Allowed domains',
+            'allowed-domains-help' => 'Comma separated. The domain set above is automatically allowed by default.',
+        ],
+        'stylesheet' => [
+            'custom-css' => 'Custom Stylesheet',
+        ],
+        'theme' => [
+            'background-color' => 'Background Color',
+            'text-color'       => 'Text Color',
+        ],
+    ],
+
+    'user' => [
+        'username'       => 'Username',
+        'email'          => 'Email',
+        'password'       => 'Password',
+        'api-token'      => 'API Token',
+        'api-token-help' => 'Regenerating your API token will prevent existing applications from accessing Cachet.',
+        '2fa'            => [
+            'help' => 'Enabling two factor authentication increases security of your account. You will need to download <a href="https://support.google.com/accounts/answer/1066447?hl=en">Google Authenticator</a> or a similar app on to your mobile device. When you login you will be asked to provide a token generated by the app.',
+        ],
+    ],
+
+    // Buttons
+    'add'    => 'Add',
+    'save'   => 'Save',
+    'update' => 'Update',
+    'create' => 'Create',
+    'edit'   => 'Edit',
+    'delete' => 'Delete',
+    'submit' => 'Submit',
+    'cancel' => 'Cancel',
+    'remove' => 'Remove',
+
+    // Other
+    'optional' => '* Optional',
+];
---
+++ resources/lang/ko/setup.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Cachet HQ <support@cachethq.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'setup'             => 'Setup',
+    'title'             => 'Setup Cachet',
+    'service_details'   => 'Service Details',
+    'env_setup'         => 'Environment Setup',
+    'status_page_setup' => 'Status Page Setup',
+    'show_support'      => 'Show support for Cachet?',
+    'admin_account'     => 'Administrator Account',
+    'complete_setup'    => 'Complete Setup',
+    'completed'         => 'Cachet has been configured successfully!',
+    'finish_setup'      => 'Go to dashboard',
+    'allow_tracking'    => 'Allow anonymous usage tracking?',
+];
