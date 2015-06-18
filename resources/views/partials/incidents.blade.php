027b0b63636f692e674f99053afd95710bb3e0fa
Zach Auclair <zach101@gmail.com>
2015-06-18 11:22:10 -0400

    feat(views/index): add quick-update buttons for incidents
    These buttons only appear if the current user is logged in
    and are similar to `resources/views/dashboard/incidents/index.blade.php`.

--- resources/views/partials/incidents.blade.php
+++ resources/views/partials/incidents.blade.php
@@ -12,6 +12,12 @@
                 <div class="col-xs-10 col-xs-offset-2 col-sm-11 col-sm-offset-0">
                     <div class="panel panel-message">
                         <div class="panel-heading">
+                            @if($loggedUser)
+                            <div class="pull-right btn-group">
+                                <a href="/dashboard/incidents/{{ $incident->id }}/edit" class="btn btn-default">{{ trans('forms.edit') }}</a>
+                                <a href="/dashboard/incidents/{{ $incident->id }}/delete" class="btn btn-danger confirm-action" data-method='DELETE'>{{ trans('forms.delete') }}</a>
+                            </div>
+                            @endif
                             <strong>{{ $incident->name }}</strong>{{ $incident->isScheduled ? trans("cachet.incidents.scheduled_at", ["timestamp" => $incident->scheduled_at_diff]) : null }}
                             <br>
                             <small class="date">
