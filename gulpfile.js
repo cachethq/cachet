2579b8a48efd90886032544f4b9e5ab8dd20431c
James Brooks <james@bluebaytravel.co.uk>
2015-06-19 11:47:38 +0100

    Move bower_components into vendor. Closes #747

--- gulpfile.js
+++ gulpfile.js
@@ -9,26 +9,26 @@ elixir(function (mix) {
         .sass('app.scss')
         .jshint('resources/assets/js/*.js')
         .styles([
-            'bower_components/ionicons/css/ionicons.css',
-            'bower_components/jquery-minicolors/jquery.minicolors.css',
+            'vendor/bower_components/ionicons/css/ionicons.css',
+            'vendor/bower_components/jquery-minicolors/jquery.minicolors.css',
             'public/dist/css/app.css'
         ], 'public/dist/css/all.css', './')
         .scripts([
-            'bower_components/jquery/dist/jquery.js',
-            'bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
-            'bower_components/moment/min/moment-with-locales.js',
-            'bower_components/eonasdan-bootstrap-datetimepicker/src/js/bootstrap-datetimepicker.js',
-            'bower_components/lodash/dist/lodash.js',
-            'bower_components/autosize/dest/autosize.js',
-            'bower_components/messenger/build/js/messenger.js',
-            'bower_components/Sortable/Sortable.js',
-            'bower_components/livestampjs/livestamp.js',
-            'bower_components/jquery-minicolors/jquery.minicolors.js',
-            'bower_components/jquery-serialize-object/jquery.serialize-object.js',
-            'bower_components/chartjs/Chart.js',
+            'vendor/bower_components/jquery/dist/jquery.js',
+            'vendor/bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
+            'vendor/bower_components/moment/min/moment-with-locales.js',
+            'vendor/bower_components/eonasdan-bootstrap-datetimepicker/src/js/bootstrap-datetimepicker.js',
+            'vendor/bower_components/lodash/dist/lodash.js',
+            'vendor/bower_components/autosize/dest/autosize.js',
+            'vendor/bower_components/messenger/build/js/messenger.js',
+            'vendor/bower_components/Sortable/Sortable.js',
+            'vendor/bower_components/livestampjs/livestamp.js',
+            'vendor/bower_components/jquery-minicolors/jquery.minicolors.js',
+            'vendor/bower_components/jquery-serialize-object/jquery.serialize-object.js',
+            'vendor/bower_components/chartjs/Chart.js',
             'resources/assets/js/app.js',
             'resources/assets/js/**/*.js'
         ], 'public/dist/js/all.js', './')
         .version(['public/dist/css/all.css', 'public/dist/js/all.js'])
-        .copy('bower_components/ionicons/fonts/', 'public/fonts/');
+        .copy('vendor/bower_components/ionicons/fonts/', 'public/fonts/');
 });
