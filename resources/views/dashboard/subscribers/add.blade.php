dbbf4a093e5e8280297a07bb979bfe13fb957d12
James Brooks <james@bluebaytravel.co.uk>
2015-07-11 16:15:48 +0100

    Fix formatting of Blade

--- resources/views/dashboard/subscribers/add.blade.php
+++ resources/views/dashboard/subscribers/add.blade.php
@@ -2,31 +2,31 @@
 
 @section('content')
     <div class="header">
-    <div class="sidebar-toggler visible-xs">
-        <i class="icon ion-navicon"></i>
-    </div>
-    <span class="uppercase">
-        <i class="icon ion-person"></i> {{ trans('dashboard.subscribers.subscribers') }}
-    </span>
+        <div class="sidebar-toggler visible-xs">
+            <i class="icon ion-navicon"></i>
+        </div>
+        <span class="uppercase">
+            <i class="icon ion-person"></i> {{ trans('dashboard.subscribers.subscribers') }}
+        </span>
     </div>
     <div class="content-wrapper">
-    <div class="row">
-        <div class="col-sm-12">
-        @include('partials.dashboard.errors')
-        <form name="SubscriberForm" class="form-vertical" role="form" action="/dashboard/subscribers/add" method="POST">
-            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-            <fieldset>
-            <div class="form-group">
-                <label>{{ trans('forms.user.email') }}</label>
-                <input type="email" class="form-control" name="email" value="{{ Input::old('email') }}" required>
-            </div>
-            </fieldset>
+        <div class="row">
+            <div class="col-sm-12">
+            @include('partials.dashboard.errors')
+            <form name="SubscriberForm" class="form-vertical" role="form" action="/dashboard/subscribers/add" method="POST">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <fieldset>
+                <div class="form-group">
+                    <label>{{ trans('forms.user.email') }}</label>
+                    <input type="email" class="form-control" name="email" value="{{ Input::old('email') }}" required>
+                </div>
+                </fieldset>
 
-            <div class="form-group">
-            <button type="submit" class="btn btn-success">{{ trans('forms.add') }}</button>
+                <div class="form-group">
+                <button type="submit" class="btn btn-success">{{ trans('forms.add') }}</button>
+                </div>
+            </form>
             </div>
-        </form>
         </div>
-    </div>
     </div>
 @stop
