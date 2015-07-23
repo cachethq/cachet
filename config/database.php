a0d354c3e9f0904d38e12a254419996587b21216
James Brooks <jbrooksuk@me.com>
2015-07-23 21:47:49 +0100

    Configure Redis through environment variables. Closes #805

--- config/database.php
+++ config/database.php
@@ -124,9 +124,9 @@ return [
         'cluster' => false,
 
         'default' => [
-            'host'     => '127.0.0.1',
-            'port'     => 6379,
-            'database' => 0,
+            'host'     => env('REDIS_HOST', '127.0.0.1'),
+            'port'     => env('REDIS_PORT', 6379),
+            'database' => env('REDIS_DATABASE', 0),
         ],
 
     ],
