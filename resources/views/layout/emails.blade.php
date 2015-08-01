2425682381eaf1482b01e74dcaf508f2aa445a5b
Graham Campbell <graham@alt-three.com>
2015-08-01 16:45:58 +0100

    Removed other areas where the word status is appended

--- resources/views/layout/emails.blade.php
+++ resources/views/layout/emails.blade.php
@@ -5,7 +5,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width" />
 <meta name="format-detection" content="address=no;=no;telephone=no" />
-<title>{{ Setting::get('app_name') }} Status</title>
+<title>{{ Setting::get('app_name') }}</title>
 @include('partials.email-css')
 </head>
 <body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0">
@@ -32,7 +32,7 @@
                         </td>
                     </tr>
                     <tr>
-                        <td class="footer">{{ Setting::get('app_name') }} Status</td>
+                        <td class="footer">{{ Setting::get('app_name') }}</td>
                     </tr>
                 </table>
             </td>
