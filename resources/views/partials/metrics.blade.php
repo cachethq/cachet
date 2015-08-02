d07c863564ffd841991825fd73d72987afe39e94
James Brooks <james@bluebaytravel.co.uk>
2015-08-02 15:43:20 +0100

    Start working on making metrics use AJAX and group by hour, day, month

--- resources/views/partials/metrics.blade.php
+++ resources/views/partials/metrics.blade.php
@@ -1,14 +1,7 @@
 @if($metrics->count() > 0)
 <ul class="list-group metrics">
     @foreach($metrics as $metric)
-    <?php
-        $hourPoints = [];
-        foreach (range(1, 11) as $hour) {
-            $hourPoints[$hour] = $metric->getValuesByHour($hour);
-        }
-        $hourPoints = array_reverse($hourPoints);
-    ?>
-    <li class="list-group-item metric">
+    <li class="list-group-item metric" data-metric-id="{{ $metric->id }}">
         <div class="row">
             <div class="col-xs-10">
                 <h4>
@@ -19,55 +12,96 @@
                 </h4>
             </div>
             <div class="col-xs-2 text-right">
-                <small>{{ trans('cachet.metrics.filter.hourly') }}</small>
+                <div class="dropdown">
+                    <a href="javascript: void(0);" class="btn btn-default dropdown-toggle" data-toggle="dropdown">{{ trans('cachet.metrics.filter.hourly') }} <span class="caret"></span></a>
+                    <ul class="dropdown-menu dropdown-menu-right">
+                        <li><a href="#" data-filter-type="hourly">{{ trans('cachet.metrics.filter.hourly') }}</a></li>
+                        <li><a href="#" data-filter-type="daily">{{ trans('cachet.metrics.filter.daily') }}</a></li>
+                        <li><a href="#" data-filter-type="weekly">{{ trans('cachet.metrics.filter.weekly') }}</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
         <hr>
         <div class="row">
             <div class="col-md-12">
                 <div>
-                    <canvas id="metric-{{ $metric->id }}" height="125" width="600"></canvas>
+                    <canvas id="metric-{{ $metric->id }}" data-metric-id="{{ $metric->id }}" data-metric-group="hourly" height="125" width="600"></canvas>
                 </div>
             </div>
         </div>
-        <script>
-            (function () {
-                Chart.defaults.global.pointHitDetectionRadius = 1;
-
-                var hourList = [], date = new Date();
-
-                for (var i = 10; i >= 1; i--) {
-                    hourList.push(moment(date).subtract(i, 'hours').seconds(0).format('HH:ss'));
-                }
-
-                hourList.push(moment(date).seconds(0).format('HH:ss'));
-
-                var hourPoints = [{{ implode(',', $hourPoints) }}];
-
-                var data = {
-                    showTooltips: false,
-                    labels: hourList,
-                    datasets: [{
-                        fillColor: "rgba(220,220,220,0.1)",
-                        strokeColor: "rgba(52,152,219,0.6)",
-                        pointColor: "rgba(220,220,220,1)",
-                        pointStrokeColor: "#fff",
-                        pointHighlightFill: "#fff",
-                        pointHighlightStroke: "rgba(220,220,220,1)",
-                        data: hourPoints
-                    }],
-                };
-
-                var ctx = document.getElementById("metric-{{ $metric->id }}").getContext("2d");
-                new Chart(ctx).Line(data, {
-                    tooltipTemplate: "{!! $metric->name !!}: <%= value %> {!! $metric->suffix !!}",
-                    scaleShowVerticalLines: true,
-                    responsive: true,
-                    maintainAspectRatio: false
-                });
-            }());
-        </script>
     </li>
     @endforeach
 </ul>
+<script>
+    (function () {
+        Chart.defaults.global.pointHitDetectionRadius = 1;
+
+        var metricLists = {
+            hourly: {
+                points: 10,
+                label: 'HH:ss'
+            },
+            daily: {
+                points: 6,
+                label: 'D'
+            },
+            weekly: {
+                points: 51,
+                label: 'W'
+            }
+        };
+
+        var date = new Date();
+
+        var defaultData = {
+            showTooltips: false,
+            labels: [],
+            datasets: [{
+                fillColor: "rgba(220,220,220,0.1)",
+                strokeColor: "rgba(52,152,219,0.6)",
+                pointColor: "rgba(220,220,220,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(220,220,220,1)",
+                data: []
+            }],
+        };
+
+        $('canvas[data-metric-id]').each(function() {
+            var $this = $(this);
+            var metricId = $this.data('metric-id');
+            var metricGroup = $this.data('metric-group');
+        fetchPoints(metricId).then(function (points) {
+            var chartConfig = defaultData,
+                labels = [];
+
+            for (var i = metricLists[metricGroup].points; i >= 1; i--) {
+                labels.push(moment(date).subtract(i, 'hours').seconds(0).format(metricLists[metricGroup].label));
+            }
+            labels.push(moment(date).seconds(0).format(metricLists[metricGroup].label));
+
+            chartConfig.labels = labels;
+
+            var ctx = document.getElementById("metric-"+metricId).getContext("2d");
+
+            new Chart(ctx).Line(chartConfig, {
+                tooltipTemplate: "{!! $metric->name !!}: <%= value %>{!! $metric->suffix !!}",
+                scaleShowVerticalLines: true,
+                scaleShowLabels: false,
+                responsive: true,
+                maintainAspectRatio: false
+            });
+        });
+    });
+
+    function fetchPoints(metricId) {
+        return $.ajax({
+            async: true,
+            dataType: 'json',
+            url: '/api/v1/metrics/'+metricId+'/points',
+        });
+    }
+}());
+</script>
 @endif
