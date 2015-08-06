3c07529693409e913b126855b75381e4b3b97295
James Brooks <jbrooksuk@me.com>
2015-08-06 14:27:44 +0100

    Pulled out the routes for RSS and Atom feeds into their own file

--- app/Http/Routes/FeedRoutes.php
+++ app/Http/Routes/FeedRoutes.php
@@ -13,7 +13,7 @@ namespace CachetHQ\Cachet\Http\Routes;
 
 use Illuminate\Contracts\Routing\Registrar;
 
-class StatusPageRoutes
+class FeedRoutes
 {
     /**
      * Define the status page routes.
@@ -24,10 +24,8 @@ class StatusPageRoutes
     {
         // Prevent access until the app is setup.
         $router->group(['middleware' => 'app.hasSetting', 'setting' => 'app_name'], function ($router) {
-            $router->get('/', [
-                'as'   => 'status-page',
-                'uses' => 'HomeController@showIndex',
-            ]);
+            $router->get('/atom/{component_group?}', 'AtomController@feedAction');
+            $router->get('/rss/{component_group?}', 'RssController@feedAction');
         });
     }
 }
