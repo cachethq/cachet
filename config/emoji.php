e3c38c7461df56e5a41d311039905ccafad9cc24
Graham Campbell <graham@alt-three.com>
2015-08-06 20:00:22 +0100

    Updated emoji package

---
+++ config/emoji.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | GitHub Token
+    |--------------------------------------------------------------------------
+    |
+    | Here you may get us to use your personal github access token to increase
+    | your rate limit while contacting GitHub's API.
+    |
+    */
+
+    'token' => env('GITHUB_TOKEN', null),
+
+];
