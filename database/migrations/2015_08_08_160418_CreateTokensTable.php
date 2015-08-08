8e5a60759dcc4f3ff8187b24d4ab9d1814082c76
James Brooks <jbrooksuk@me.com>
2015-08-08 17:10:00 +0100

    Started working on individual API tokens

---
+++ database/migrations/2015_08_08_160418_CreateTokensTable.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTokensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tokens', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('token')->index();
+            $table->boolean('active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tokens');
+    }
+}
