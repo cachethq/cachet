b4f1a8af612f66dd3f7aa0761d3f5861b9450524
James Brooks <jbrooksuk@me.com>
2015-08-15 20:51:04 +0100

    Re-use the timeline view

--- resources/views/incident.blade.php
+++ resources/views/incident.blade.php
@@ -29,5 +29,20 @@
 
     <h4>{{ formatted_date($incident->created_at) }}</h4>
 
-    @include('partials.incident', ['incident' => $incident, 'with_link' => false])
+    <div class="timeline">
+        <div class="content-wrapper">
+            <div class="moment first">
+                <div class="row event clearfix">
+                    <div class="col-sm-1">
+                        <div class="status-icon status-{{ $incident->status }}" data-toggle="tooltip" title="{{ $incident->humanStatus }}" data-placement="left">
+                            <i class="{{ $incident->icon }}"></i>
+                        </div>
+                    </div>
+                    <div class="col-xs-10 col-xs-offset-2 col-sm-11 col-sm-offset-0">
+                        @include('partials.incident', ['incident' => $incident, 'with_link' => false])
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
