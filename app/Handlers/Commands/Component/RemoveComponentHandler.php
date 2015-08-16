30eec9da247de8d6e872e75128833c7339c9e237
James Brooks <jbrooksuk@me.com>
2015-08-16 13:50:11 +0100

    Added Component events and handlers

---
+++ app/Handlers/Commands/Component/RemoveComponentHandler.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CachetHQ\Cachet\Handlers\Commands\Component;
+
+use CachetHQ\Cachet\Commands\Component\RemoveComponentCommand;
+use CachetHQ\Cachet\Events\Component\ComponentWasAddedEvent;
+use CachetHQ\Cachet\Models\Component;
+
+class RemoveComponentHandler
+{
+    /**
+     * Handle the remove component command.
+     *
+     * @param \CachetHQ\Cachet\Commands\Component\RemoveComponentCommand $command
+     *
+     * @return void
+     */
+    public function handle(RemoveComponentCommand $command)
+    {
+        $component = $command->component;
+
+        event(new ComponentWasRemovedEvent($component));
+
+        $component->delete();
+    }
+}
