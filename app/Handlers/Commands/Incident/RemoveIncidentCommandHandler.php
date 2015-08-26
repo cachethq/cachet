b80b53191b31c51b3103baca347af19a265b97e9
James Brooks <jbrooksuk@me.com>
2015-08-26 13:14:38 +0100

    Commands and events for removing incidents

---
+++ app/Handlers/Commands/Incident/RemoveIncidentCommandHandler.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CachetHQ\Cachet\Handlers\Commands\Incident;
+
+use CachetHQ\Cachet\Commands\Incident\RemoveIncidentCommand;
+use CachetHQ\Cachet\Events\Incident\IncidentWasRemovedEvent;
+use CachetHQ\Cachet\Models\Incident;
+
+class RemoveIncidentCommandHandler
+{
+    /**
+     * Handle the remove incident command.
+     *
+     * @param \CachetHQ\Cachet\Commands\Incident\RemoveIncidentCommand $command
+     *
+     * @return void
+     */
+    public function handle(RemoveIncidentCommand $command)
+    {
+        $incident = $command->incident;
+
+        event(new IncidentWasRemovedEvent($incident));
+
+        $incident->delete();
+    }
+}
