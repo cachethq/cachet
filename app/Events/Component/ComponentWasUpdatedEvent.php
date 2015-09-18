72da18fe765a7256f853a46f151ba0aed4d5e6b4
James Brooks <jbrooksuk@me.com>
2015-09-18 15:12:31 +0100

    UpdateComponentCommand is now done

---
+++ app/Events/Component/ComponentWasUpdatedEvent.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CachetHQ\Cachet\Events\Component;
+
+use CachetHQ\Cachet\Models\Component;
+
+class ComponentWasUpdatedEvent
+{
+    /**
+     * The component that was updated.
+     *
+     * @var \CachetHQ\Cachet\Models\Component
+     */
+    public $component;
+
+    /**
+     * Create a new component was updated event instance.
+     *
+     * @return void
+     */
+    public function __construct(Component $component)
+    {
+        $this->component = $component;
+    }
+}
