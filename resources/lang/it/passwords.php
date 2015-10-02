bd6b3f09d0926b4e89dec299021ef29bef733acc
Davide Bellini <bellini.davide@gmail.com>
2015-10-02 15:43:38 +0200

    Updated Italian translations
    Aligned to the latest version of Laravel

--- resources/lang/it/passwords.php
+++ resources/lang/it/passwords.php
@@ -23,9 +23,9 @@ return [
     */
 
     'password' => 'Le password devono essere di almeno 6 caratteri e devono coincidere.',
-    'user'     => 'Non esiste un utente associato a questo indirizzo e-mail.',
-    'token'    => 'Questo token per la reimpostazione della password non è valido.',
-    'sent'     => 'Promemoria della password inviato!',
     'reset'    => 'La password è stata reimpostata!',
+    'sent'     => 'Promemoria della password inviato!',
+    'token'    => 'Questo token per la reimpostazione della password non è valido.',
+    'user'     => 'Non esiste un utente associato a questo indirizzo e-mail.',
 
 ];
