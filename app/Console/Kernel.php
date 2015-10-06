814fcd844c30aa2bfc1f3bb96812a3f678a49450
Graham Campbell <graham@alt-three.com>
2015-10-06 16:24:51 +0100

    Tweaked command kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -11,6 +11,7 @@
 
 namespace CachetHQ\Cachet\Console;
 
+use CachetHQ\Cachet\Console\Commands\DemoSeederCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -22,7 +23,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        'CachetHQ\Cachet\Console\Commands\DemoSeederCommand',
+        DemoSeederCommand::class,
     ];
 
     /**
