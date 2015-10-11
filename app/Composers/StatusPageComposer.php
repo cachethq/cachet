3988cfb6c0fd2d184937036df2d8bdc088cb9094
James Brooks <jbrooksuk@me.com>
2015-10-11 11:35:00 +0100

    Use warning rather than error

--- app/Composers/StatusPageComposer.php
+++ app/Composers/StatusPageComposer.php
@@ -30,7 +30,7 @@ class StatusPageComposer
     {
         // Default data
         $withData = [
-            'systemStatus'  => 'danger',
+            'systemStatus'  => 'warning',
             'systemMessage' => trans('cachet.service.bad'),
             'favicon'       => 'favicon-high-alert',
         ];
