24e125857c8658d927c6dd976dde2e00c9cb559d
James Brooks <jbrooksuk@me.com>
2015-10-20 22:06:13 +0100

    Organise the footer buttons better. Closes #1047

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -7,19 +7,21 @@
                 @endif
             </div>
             <div class="col-sm-6">
-                <div class="icons">
+                <div class="btn-group">
                     @if($current_user || Setting::get('dashboard_login_link'))
-                    <a href="/dashboard" class="icon-link"><i class="ion-levels"></i> {{ trans('dashboard.dashboard') }}</a>
+                    <a href="/dashboard" class="btn btn-info"><i class="ion-levels"></i> {{ trans('dashboard.dashboard') }}</a>
                     @endif
                     @if($current_user)
-                    <a href="/auth/logout" class="icon-link"><i class="ion-android-exit"></i> {{ trans('dashboard.logout') }}</a>
-                    @endif
-                    <a href="{{ route('feed.rss') }}" class="icon-link rss"><i class="ion-social-rss"></i> {{ trans('cachet.rss-feed') }}</a>
-                    <a href="{{ route('feed.atom') }}" class="icon-link rss"><i class="ion-social-rss"></i> {{ trans('cachet.atom-feed') }}</a>
-                    @if($subscribers_enabled)
-                    <a class="icon-link" href="{{ route('subscribe.subscribe') }}">{{ trans('cachet.subscriber.button') }}</a>
+                    <a href="/auth/logout" class="btn btn-info"><i class="ion-android-exit"></i> {{ trans('dashboard.logout') }}</a>
                     @endif
                 </div>
+                <div class="btn-group">
+                    <a href="{{ route('feed.rss') }}" class="btn btn-warning rss"><i class="ion-social-rss"></i> {{ trans('cachet.rss-feed') }}</a>
+                    <a href="{{ route('feed.atom') }}" class="btn btn-warning rss"><i class="ion-social-rss"></i> {{ trans('cachet.atom-feed') }}</a>
+                </div>
+                @if($subscribers_enabled)
+                <a class="btn btn-success" href="{{ route('subscribe.subscribe') }}"><i class="ion-email"></i> {{ trans('cachet.subscriber.button') }}</a>
+                @endif
             </div>
         </div>
     </div>
