09ed3b3e7abbfff396e4c630e9f35af4c9638534
ApfelUser <silas.rech@icloud.com>
2015-10-28 10:17:17 +0100

    Add correct German translation for auth.php

---
+++ resources/lang/de/auth.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'   => 'Diese Zugangsdaten stimmen nicht mit unseren überein.',
+    'throttle' => 'Zu viele Login-Versuche. Bitte versuchen Sie es in :seconds Sekunden erneut.',
+
+];
