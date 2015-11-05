ca2b6a7ba4c0983f5c080f3341de839d09fbb231
Graham Campbell <graham@alt-three.com>
2015-11-05 16:01:21 +0000

    Updated markdown config and enabled safe mode

--- config/markdown.php
+++ config/markdown.php
@@ -27,4 +27,90 @@ return [
 
     'views' => false,
 
+    /*
+    |--------------------------------------------------------------------------
+    | Renderer Configuration
+    |--------------------------------------------------------------------------
+    |
+    | This option specifies an array of options for rendering HTML.
+    |
+    | Default: [
+    |              'block_separator' => "\n",
+    |              'inner_separator' => "\n",
+    |              'soft_break'      => "\n",
+    |          ]
+    |
+    */
+
+    'renderer' => [
+        'block_separator' => "\n",
+        'inner_separator' => "\n",
+        'soft_break'      => "\n",
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Enable Em Tag Parsing
+    |--------------------------------------------------------------------------
+    |
+    | This option specifies if `<em>` parsing is enabled.
+    |
+    | Default: true
+    |
+    */
+
+    'enable_em' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Enable Strong Tag Parsing
+    |--------------------------------------------------------------------------
+    |
+    | This option specifies if `<strong>` parsing is enabled.
+    |
+    | Default: true
+    |
+    */
+
+    'enable_strong' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Enable Asterisk Parsing
+    |--------------------------------------------------------------------------
+    |
+    | This option specifies if `*` should be parsed for emphasis.
+    |
+    | Default: true
+    |
+    */
+
+    'use_asterisk' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Enable Underscore Parsing
+    |--------------------------------------------------------------------------
+    |
+    | This option specifies if `_` should be parsed for emphasis.
+    |
+    | Default: true
+    |
+    */
+
+    'use_underscore' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Safe Mode
+    |--------------------------------------------------------------------------
+    |
+    | This option specifies if raw HTML should be allowed in the document.
+    |
+    | Default: false
+    |
+    */
+
+    'safe' => true,
+
 ];
