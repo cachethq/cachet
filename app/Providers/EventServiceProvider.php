a88140ed537a670fb40aefbaa53c76c1d97eb066
Joseph Cohen <joseph.cohen@dinkbit.com>
2015-11-08 13:59:46 -0600

    Update event listeners order

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -30,11 +30,11 @@ class EventServiceProvider extends ServiceProvider
         'CachetHQ\Cachet\Events\Subscriber\SubscriberHasSubscribedEvent' => [
             'CachetHQ\Cachet\Handlers\Events\Subscriber\SendSubscriberVerificationEmailHandler',
         ],
-        'CachetHQ\Cachet\Events\User\UserWasInvitedEvent' => [
-            'CachetHQ\Cachet\Handlers\Events\User\SendInviteUserEmailHandler',
-        ],
         'CachetHQ\Cachet\Events\User\UserWasAddedEvent' => [
             //
+        ],
+        'CachetHQ\Cachet\Events\User\UserWasInvitedEvent' => [
+            'CachetHQ\Cachet\Handlers\Events\User\SendInviteUserEmailHandler',
         ],
     ];
 }
