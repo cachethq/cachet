970d903b48985b803ff04d39d42bcccd04163edd
James Brooks <jbrooksuk@me.com>
2015-11-21 23:07:23 +0000

    style_fullwidth_header setting is now in AppComposer

--- resources/views/partials/banner.blade.php
+++ resources/views/partials/banner.blade.php
@@ -1,7 +1,7 @@
-@if($app_banner = $app_banner)
-<div @if(Setting::get('style_fullwidth_header'))class="app-banner"@endif>
+@if($app_banner)
+<div @if($app_banner_style_full_width)class="app-banner"@endif>
     <div class="container">
-        <div class="row app-banner-padding  @if(!Setting::get('style_fullwidth_header')) app-banner @endif">
+        <div class="row app-banner-padding  @if(!$app_banner_style_full_width) app-banner @endif">
             <div class="col-md-12 text-center">
                 @if($app_domain)
                 <a href="{{ $app_domain }}" class="links"><img src="data:{{ $app_banner_type }};base64, {{ $app_banner }}" class="banner-image img-responsive"></a>
