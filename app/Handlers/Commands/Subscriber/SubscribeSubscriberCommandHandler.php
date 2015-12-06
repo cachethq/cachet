e92cb7fb019d1a76a98101ad45eddffb235afaa3
Graham Campbell <graham@alt-three.com>
2015-12-06 18:01:33 +0000

    Added AlreadySubscribedException
    Closes #1201

--- app/Handlers/Commands/Subscriber/SubscribeSubscriberCommandHandler.php
+++ app/Handlers/Commands/Subscriber/SubscribeSubscriberCommandHandler.php
@@ -14,6 +14,7 @@ namespace CachetHQ\Cachet\Handlers\Commands\Subscriber;
 use CachetHQ\Cachet\Commands\Subscriber\SubscribeSubscriberCommand;
 use CachetHQ\Cachet\Commands\Subscriber\VerifySubscriberCommand;
 use CachetHQ\Cachet\Events\Subscriber\SubscriberHasSubscribedEvent;
+use CachetHQ\Cachet\Exceptions\AlreadySubscribedException;
 use CachetHQ\Cachet\Models\Subscriber;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 
@@ -26,10 +27,16 @@ class SubscribeSubscriberCommandHandler
      *
      * @param \CachetHQ\Cachet\Commands\Subscriber\SubscribeSubscriberCommand $command
      *
+     * @throws \CachetHQ\Cachet\Exceptions\AlreadySubscribedException
+     *
      * @return \CachetHQ\Cachet\Models\Subscriber
      */
     public function handle(SubscribeSubscriberCommand $command)
     {
+        if (Subscriber::where('email', $command->email)->first()) {
+            throw new AlreadySubscribedException("Cannot subscribe {$command->email} because they're already subscribed.");
+        }
+
         $subscriber = Subscriber::create(['email' => $command->email]);
 
         if ($command->verified) {
