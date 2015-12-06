6efe100064e0a1e6638b2ccfcf6e9796d1814d8a
Graham Campbell <graham@alt-three.com>
2015-12-06 17:59:26 +0000

    Added new exception interface and updated transformers

---
+++ app/Exceptions/ExceptionInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CachetHQ\Cachet\Exceptions;
+
+/**
+ * This is the exception interface.
+ *
+ * @author Graham Campbell <graham@alt-three.com>
+ */
+interface ExceptionInterface
+{
+    //
+}
