53889efa8d737ea572c8e87300b14f758e798ca3
Graham Campbell <graham@alt-three.com>
2015-12-11 11:44:34 +0000

    Updated exceptions package

--- app/Foundation/Exceptions/Filters/ApiFilter.php
+++ app/Foundation/Exceptions/Filters/ApiFilter.php
@@ -17,37 +17,19 @@ use Illuminate\Http\Request;
 class ApiFilter
 {
     /**
-     * The request instance.
-     *
-     * @var \Illuminate\Http\Request
-     */
-    protected $request;
-
-    /**
-     * Create a new api filter instance.
-     *
-     * @param \Illuminate\Http\Request $request
-     *
-     * @return void
-     */
-    public function __construct(Request $request)
-    {
-        $this->request = $request;
-    }
-
-    /**
      * Filter and return the displayers.
      *
      * @param \GrahamCampbell\Exceptions\Displayers\DisplayerInterface[] $displayers
+     * @param \Illuminate\Http\Request                                   $request
      * @param \Exception                                                 $original
      * @param \Exception                                                 $transformed
      * @param int                                                        $code
      *
      * @return \GrahamCampbell\Exceptions\Displayers\DisplayerInterface[]
      */
-    public function filter(array $displayers, Exception $original, Exception $transformed, $code)
+    public function filter(array $displayers, Request $request, Exception $original, Exception $transformed, $code)
     {
-        if ($this->request->is('api*')) {
+        if ($request->is('api*')) {
             foreach ($displayers as $index => $displayer) {
                 if (!str_contains($displayer->contentType(), 'application/')) {
                     unset($displayers[$index]);
