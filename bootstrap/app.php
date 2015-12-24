9cfb1e1ebebcef4a141dbda33eabb26ed444c5b7
Graham Campbell <graham@alt-three.com>
2015-12-24 17:50:20 +0000

    CS fixes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,20 +24,11 @@ $app = new Illuminate\Foundation\Application(
 |
 */
 
-$app->singleton(
-    'Illuminate\Contracts\Http\Kernel',
-    'CachetHQ\Cachet\Http\Kernel'
-);
+$app->singleton('Illuminate\Contracts\Http\Kernel', 'CachetHQ\Cachet\Http\Kernel');
 
-$app->singleton(
-    'Illuminate\Contracts\Console\Kernel',
-    'CachetHQ\Cachet\Console\Kernel'
-);
+$app->singleton('Illuminate\Contracts\Console\Kernel', 'CachetHQ\Cachet\Console\Kernel');
 
-$app->singleton(
-    'Illuminate\Contracts\Debug\ExceptionHandler',
-    'GrahamCampbell\Exceptions\ExceptionHandler'
-);
+$app->singleton('Illuminate\Contracts\Debug\ExceptionHandler', 'GrahamCampbell\Exceptions\ExceptionHandler');
 
 /*
 |--------------------------------------------------------------------------
