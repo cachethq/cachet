eaa0d35acf8ca0fb108598c884ffcd0e7be6b83e
James Brooks <jbrooksuk@me.com>
2016-01-10 16:51:54 +0000

    Started work on modal subscription stuff

---
+++ resources/views/partials/modals/subscribe.blade.php
@@ -0,0 +1,24 @@
+<div class="modal fade" tabindex="-1" role="dialog" id="subscribeModal">
+    <div class="modal-dialog">
+        <form action="{{ route("subscribe.subscribe", [], false) }}" method="post" class="form">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title">{{ trans("cachet.modal.subscribe.title") }}</h4>
+                </div>
+                <div class="modal-body">
+                        <p>{{ trans("cachet.modal.subscribe.body") }}</p>
+                        <div class="form-group">
+                            <input class="form-control" type="email" name="email">
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">{{ trans("cachet.modal.close") }}</button>
+                    <button type="button" class="btn btn-success">{{ trans("cachet.modal.subscribe.button") }}</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
