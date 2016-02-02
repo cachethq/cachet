226e5b5d5545c73aa487437aff1ef34b70e3dc44
James Brooks <james@alt-three.com>
2016-02-02 06:09:08 -0500

    Applied fixes from StyleCI

--- app/Http/Controllers/StatusPageController.php
+++ app/Http/Controllers/StatusPageController.php
@@ -25,6 +25,7 @@ use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\View;
 use Jenssegers\Date\Date;
 use McCool\LaravelAutoPresenter\Facades\AutoPresenter;
+
 // use PUGX\Poser\Poser;
 // use PUGX\Poser\Render\SvgRender;
 
