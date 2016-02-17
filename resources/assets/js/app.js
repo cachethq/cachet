b8f89e97cd8240e1312db68ef1660ccf7c3d0917
Joseph Cohen <joseph.cohen@dinkbit.com>
2016-02-17 13:32:56 -0600

    Fix [object object] notification error with js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -78,6 +78,9 @@ $(function() {
 
     window.Cachet.Notifier = function () {
         this.notify = function (message, type, options) {
+            if (_.isPlainObject(message)) {
+                message = message.detail;
+            }
             type = (typeof type === 'undefined' || type === 'error') ? 'error' : type;
 
             var defaultOptions = {
