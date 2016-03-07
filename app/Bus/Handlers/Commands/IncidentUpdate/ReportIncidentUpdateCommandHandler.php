0d468fce49687bcaf5a2329fa63050a2370e9067
James Brooks <james@bluebaytravel.co.uk>
2016-03-07 11:58:10 +0000

    Started work on incident updates

---
+++ app/Bus/Handlers/Commands/IncidentUpdate/ReportIncidentUpdateCommandHandler.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CachetHQ\Cachet\Bus\Handlers\Commands\IncidentUpdate;
+
+use CachetHQ\Cachet\Bus\Commands\IncidentUpdate\ReportIncidentUpdateCommand;
+use CachetHQ\Cachet\Models\IncidentUpdate;
+
+/**
+ * This is the report incident update command handler.
+ *
+ * @author James Brooks <james@alt-three.com>
+ */
+class ReportIncidentUpdateCommandHandler
+{
+    /**
+     * Handle the report incident command.
+     *
+     * @param \CachetHQ\Cachet\Bus\Commands\IncidentUpdate\ReportIncidentUpdateCommand $command
+     *
+     * @return \CachetHQ\Cachet\Models\IncidentUpdate
+     */
+    public function handle(ReportIncidentUpdateCommand $command)
+    {
+        $data = [
+            'incident_id' => $command->incident->id,
+            'status'      => $command->status,
+            'message'     => $command->message,
+            'user_id'     => $command->user->id,
+        ];
+
+        // Create the incident update.
+        $update = IncidentUpdate::create($data);
+
+        return $update;
+    }
+}
