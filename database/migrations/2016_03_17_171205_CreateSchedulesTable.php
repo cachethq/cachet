7f667b43256e49cb3918b007d6d05ade60a0c448
James Brooks <james@bluebaytravel.co.uk>
2016-03-17 17:24:43 +0000

    Create new migrations (do not run these, WIP)

---
+++ database/migrations/2016_03_17_171205_CreateSchedulesTable.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->longText('message')->nullable()->default(null);
+            $table->tinyInteger('status')->unsigned()->default(0);
+            $table->timestamp('scheduled_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('schedules');
+    }
+}
