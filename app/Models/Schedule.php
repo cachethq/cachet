cabc9ef49561550a48086b6f1683da515c285560
James Brooks <james@bluebaytravel.co.uk>
2016-03-17 17:24:52 +0000

    New models and schedule presenter

---
+++ app/Models/Schedule.php
@@ -0,0 +1,106 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CachetHQ\Cachet\Models;
+
+use AltThree\Validator\ValidatingTrait;
+use CachetHQ\Cachet\Presenters\SchedulePresenter;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use McCool\LaravelAutoPresenter\HasPresenter;
+
+class Schedule extends Model implements HasPresenter
+{
+    use ValidatingTrait;
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var string[]
+     */
+    protected $casts = [
+        'name'         => 'string',
+        'message'      => 'string',
+        'status'       => 'int',
+        'scheduled_at' => 'date',
+    ];
+
+    /**
+     * The fillable properties.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'message',
+        'status',
+        'scheduled_at',
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * The validation rules.
+     *
+     * @var string[]
+     */
+    public $rules = [
+        'component_id' => 'int',
+        'name'         => 'required|string',
+        'message'      => 'string',
+        'status'       => 'required|int|between:0,2',
+        'scheduled_at' => 'required',
+    ];
+
+    /**
+     * Scopes schedules to those in the future.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeFutureSchedules($query)
+    {
+        return $query->where('status', 0)->where('scheduled_at', '>=', Carbon::now());
+    }
+
+    /**
+     * Scopes schedules to those in the past.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopePastSchedules($query)
+    {
+        return $query->where('scheduled_at', '<=', Carbon::now());
+    }
+
+    /**
+     * Get the components relation.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function components()
+    {
+        return $this->hasMany(ScheduleComponent::class);
+    }
+
+    /**
+     * Get the presenter class.
+     *
+     * @return string
+     */
+    public function getPresenterClass()
+    {
+        return SchedulePresenter::class;
+    }
+}
---
+++ app/Presenters/SchedulePresenter.php
@@ -0,0 +1,183 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CachetHQ\Cachet\Presenters;
+
+use CachetHQ\Cachet\Dates\DateFactory;
+use CachetHQ\Cachet\Presenters\Traits\TimestampsTrait;
+use GrahamCampbell\Markdown\Facades\Markdown;
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Support\Facades\Config;
+use McCool\LaravelAutoPresenter\BasePresenter;
+
+class SchedulePresenter extends BasePresenter implements Arrayable
+{
+    use TimestampsTrait;
+
+    /**
+     * Renders the message from Markdown into HTML.
+     *
+     * @return string
+     */
+    public function formattedMessage()
+    {
+        return Markdown::convertToHtml($this->wrappedObject->message);
+    }
+
+    /**
+     * Present diff for humans date time.
+     *
+     * @return string
+     */
+    public function created_at_diff()
+    {
+        return app(DateFactory::class)->make($this->wrappedObject->created_at)->diffForHumans();
+    }
+
+    /**
+     * Present formatted date time.
+     *
+     * @return string
+     */
+    public function created_at_formatted()
+    {
+        return ucfirst(app(DateFactory::class)->make($this->wrappedObject->created_at)->format(Config::get('setting.incident_date_format', 'l jS F Y H:i:s')));
+    }
+
+    /**
+     * Formats the created_at time ready to be used by bootstrap-datetimepicker.
+     *
+     * @return string
+     */
+    public function created_at_datetimepicker()
+    {
+        return app(DateFactory::class)->make($this->wrappedObject->created_at)->format('d/m/Y H:i');
+    }
+
+    /**
+     * Present formatted date time.
+     *
+     * @return string
+     */
+    public function created_at_iso()
+    {
+        return app(DateFactory::class)->make($this->wrappedObject->created_at)->toISO8601String();
+    }
+
+    /**
+     * Present formatted date time.
+     *
+     * @return string
+     */
+    public function scheduled_at()
+    {
+        return app(DateFactory::class)->make($this->wrappedObject->scheduled_at)->toDateTimeString();
+    }
+
+    /**
+     * Present diff for humans date time.
+     *
+     * @return string
+     */
+    public function scheduled_at_diff()
+    {
+        return app(DateFactory::class)->make($this->wrappedObject->scheduled_at)->diffForHumans();
+    }
+
+    /**
+     * Present formatted date time.
+     *
+     * @return string
+     */
+    public function scheduled_at_formatted()
+    {
+        return ucfirst(app(DateFactory::class)->make($this->wrappedObject->scheduled_at)->format(Config::get('setting.incident_date_format', 'l jS F Y H:i:s')));
+    }
+
+    /**
+     * Present formatted date time.
+     *
+     * @return string
+     */
+    public function scheduled_at_iso()
+    {
+        return app(DateFactory::class)->make($this->wrappedObject->scheduled_at)->toISO8601String();
+    }
+
+    /**
+     * Formats the scheduled_at time ready to be used by bootstrap-datetimepicker.
+     *
+     * @return string
+     */
+    public function scheduled_at_datetimepicker()
+    {
+        return app(DateFactory::class)->make($this->wrappedObject->scheduled_at)->format('d/m/Y H:i');
+    }
+
+    /**
+     * Returns a formatted timestamp for use within the timeline.
+     *
+     * @return string
+     */
+    public function timestamp_formatted()
+    {
+        if ($this->wrappedObject->is_scheduled) {
+            return $this->scheduled_at_formatted;
+        }
+
+        return $this->created_at_formatted;
+    }
+
+    /**
+     * Return the iso timestamp for use within the timeline.
+     *
+     * @return string
+     */
+    public function timestamp_iso()
+    {
+        if ($this->wrappedObject->is_scheduled) {
+            return $this->scheduled_at_iso;
+        }
+
+        return $this->created_at_iso;
+    }
+
+    /**
+     * Returns a human readable version of the status.
+     *
+     * @return string
+     */
+    public function human_status()
+    {
+        // return trans('cachet.incidents.status.'.$this->wrappedObject->status);
+        // TODO: Refactor into translations.
+        switch ($this->wrappedObject->status) {
+            case 0: return 'Upcoming';
+            case 1: return 'In Progress';
+            case 2: return 'Complete';
+        }
+    }
+
+    /**
+     * Convert the presenter instance to an array.
+     *
+     * @return string[]
+     */
+    public function toArray()
+    {
+        return array_merge($this->wrappedObject->toArray(), [
+            'human_status' => $this->human_status(),
+            'scheduled_at' => $this->scheduled_at(),
+            'created_at'   => $this->created_at(),
+            'updated_at'   => $this->updated_at(),
+        ]);
+    }
+}
