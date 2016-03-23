889dd2b1a9d9edaf291dcfb48153d3bf5389c3e4
James Brooks <james@bluebaytravel.co.uk>
2016-03-23 11:22:20 +0000

    Working on the single-incident changes

--- resources/views/single-incident.blade.php
+++ resources/views/single-incident.blade.php
@@ -7,22 +7,28 @@
 @stop
 
 @section('content')
-<h1>{{ formatted_date($incident->created_at) }}</h1>
+<h1><i class="{{ $incident->icon }}"></i> {{ $incident->name }} <small>{{ formatted_date($incident->created_at) }}</small></h1>
 
+{!! $incident->formattedMessage !!}
+
+@if($incident->updates)
 <div class="timeline">
     <div class="content-wrapper">
-        <div class="moment first">
+        @foreach ($incident->updates as $index => $update)
+        <div class="moment {{ $index === 0 ? 'first' : null }}">
             <div class="row event clearfix">
                 <div class="col-sm-1">
-                    <div class="status-icon status-{{ $incident->status }}" data-toggle="tooltip" title="{{ $incident->human_status }}" data-placement="left">
-                        <i class="{{ $incident->icon }}"></i>
+                    <div class="status-icon status-{{ $update->status }}" data-toggle="tooltip" title="{{ $update->human_status }}" data-placement="left">
+                        <i class="{{ $update->icon }}"></i>
                     </div>
                 </div>
                 <div class="col-xs-10 col-xs-offset-2 col-sm-11 col-sm-offset-0">
-                    @include('partials.incident', ['incident' => $incident, 'with_link' => false])
+                    {!! $update->formattedMessage !!}
                 </div>
             </div>
         </div>
+        @endforeach
     </div>
 </div>
+@endif
 @stop
