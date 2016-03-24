b5bac7690ea3f3e4260dab5d05da6654b620f18a
Graham Campbell <graham@alt-three.com>
2016-03-24 15:25:48 +0000

    Synced up db stuff

--- config/database.php
+++ config/database.php
@@ -57,7 +57,7 @@ return [
 
         'sqlite' => [
             'driver'   => 'sqlite',
-            'database' => env('DB_HOST', storage_path().'/database.sqlite'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix'   => env('DB_PREFIX', null),
         ],
 
