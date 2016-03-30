fd90725ff7b972e7622cd683fa5f6c731ede57b5
Joe Cohen <joseph.cohen@dinkbit.com>
2016-03-30 04:13:48 -0400

    Applied fixes from StyleCI

--- tests/Bus/Commands/Metric/AddMetricCommandTest.php
+++ tests/Bus/Commands/Metric/AddMetricCommandTest.php
@@ -29,14 +29,14 @@ class AddMetricCommandTest extends AbstractTestCase
     protected function getObjectAndParams()
     {
         $params = [
-            'name'          => 'Coffee',
-            'suffix'        => 'cups',
-            'description'   => 'Cups of coffee consumed',
-            'default_value' => 0,
-            'calc_type'     => 0,
-            'display_chart' => 1,
-            'places'        => 0,
-            'default_view'  => 0,
+            'name'           => 'Coffee',
+            'suffix'         => 'cups',
+            'description'    => 'Cups of coffee consumed',
+            'default_value'  => 0,
+            'calc_type'      => 0,
+            'display_chart'  => 1,
+            'places'         => 0,
+            'default_view'   => 0,
             'threshold'      => 0,
         ];
 
