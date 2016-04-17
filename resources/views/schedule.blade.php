37815380bcafc945907b2ca99e2e4dced1828aee
James Brooks <jbrooksuk@me.com>
2016-04-17 16:58:03 +0100

    Changing single schedule view

--- resources/views/schedule.blade.php
+++ resources/views/schedule.blade.php
@@ -1,6 +1,14 @@
 @extends('layout.master')
 
+@section('bodyClass', 'no-padding')
+
+@section('outer-content')
+@include('partials.nav')
+@stop
+
 @section('content')
+<h1>{{ $schedule->name }}</h1>
+
 <div class="timeline">
     <div class="content-wrapper">
         <div class="moment first">
@@ -13,7 +21,7 @@
                 <div class="col-xs-10 col-xs-offset-2 col-sm-11 col-sm-offset-0">
                     <div class="panel panel-message incident">
                         <div class="panel-heading">
-                            <strong>{{ $schedule->name }}</strong>, scheduled at {{ formatted_date($schedule->scheduled_at) }}
+                            <strong>{{ $schedule->name }}</strong>{{ trans("cachet.incidents.scheduled_at", ["timestamp" => $schedule->scheduled_at_diff]) }}
                         </div>
                         <div class="panel-body">
                             {!! $schedule->formattedMessage !!}
