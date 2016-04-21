f85e9abc96383a71015198541a6610bcc5e96dcd
James Brooks <james@alt-three.com>
2016-04-21 11:43:14 -0400

    Applied fixes from StyleCI

--- database/migrations/2016_04_21_150021_RenameComponentTagTable.php
+++ database/migrations/2016_04_21_150021_RenameComponentTagTable.php
@@ -10,7 +10,6 @@
  */
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 class RenameComponentTagTable extends Migration
