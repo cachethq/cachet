0200ac848f4420a942d9cfe4548581c67a306690
James Brooks <james@alt-three.com>
2016-05-26 11:20:07 +0100

    Reduce indentation

--- resources/views/emails/subscribers/verify-html.blade.php
+++ resources/views/emails/subscribers/verify-html.blade.php
@@ -5,9 +5,9 @@
 @stop
 
 @section('content')
-    {!! trans('cachet.subscriber.email.verify.html', ['app_name' => $app_name, 'link' => $link]) !!}
+{!! trans('cachet.subscriber.email.verify.html', ['app_name' => $app_name, 'link' => $link]) !!}
 
-    @if($show_support)
-    <p>{!! trans('cachet.powered_by', ['app' => $app_name]) !!}</p>
-    @endif
+@if($show_support)
+<p>{!! trans('cachet.powered_by', ['app' => $app_name]) !!}</p>
+@endif
 @stop
