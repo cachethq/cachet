696a1126c7e7e3bf6ae1c7c7af0a37e56941cde3
Graham Campbell <graham@alt-three.com>
2016-05-29 18:47:33 +0100

    Fixed lang file

--- resources/lang/en/dashboard.php
+++ resources/lang/en/dashboard.php
@@ -220,7 +220,7 @@ return [
             'failure' => 'Settings could not be saved.',
         ],
         'credits' => [
-            'credits'       => 'Credits',
+            'contributors'  => 'Contributors',
             'license'       => 'Cachet is a BSD-3-licensed open source project, released by <a href="https://alt-three.com/?utm_source=cachet&utm_medium=credits&utm_campaign=Cachet%20Credit%20Dashboard" target="_blank">Alt Three Services Limited</a>.',
             'backers-title' => 'Backers & Sponsors',
             'backers'       => 'If you\'d like to support future development, check out the <a href="https://patreon.com/jbrooksuk" target="_blank">Cachet Patreon</a> campaign.',
