091f59c241c75d894a489514daedf1127ca106fe
James Brooks <james@alt-three.com>
2016-05-31 14:27:10 -0400

    Applied fixes from StyleCI
    [ci skip]

--- config/css-inliner.php
+++ config/css-inliner.php
@@ -1,32 +1,41 @@
 <?php
 
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 return [
 
-	/*
-	|--------------------------------------------------------------------------
-	| Strip styles
-	|--------------------------------------------------------------------------
-	|
-	| Settings this to false prevents the inliner from removing the style
-	| definitions that have been inlined.
-	|
-	| Notice that media query styles are not inlined, and hence never
-	| stripped.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Strip styles
+    |--------------------------------------------------------------------------
+    |
+    | Settings this to false prevents the inliner from removing the style
+    | definitions that have been inlined.
+    |
+    | Notice that media query styles are not inlined, and hence never
+    | stripped.
+    |
+    */
 
-	'strip-styles' => true,
+    'strip-styles' => true,
 
-	/*
-	|--------------------------------------------------------------------------
-	| Remove classes
-	|--------------------------------------------------------------------------
-	|
-	| Settings this to false disables the removal of class attributes from
-	| your html elements (do not enable this if you use media queries)
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Remove classes
+    |--------------------------------------------------------------------------
+    |
+    | Settings this to false disables the removal of class attributes from
+    | your html elements (do not enable this if you use media queries)
+    |
+    */
 
-	'strip-classes' => true,
+    'strip-classes' => true,
 
 ];
