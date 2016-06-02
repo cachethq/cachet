1527ec8ddc9bedc86c93a5d457f3961adc8a2e67
James Brooks <jbrooksuk@me.com>
2016-06-02 18:40:51 +0100

    Test all app: commands

--- tests/Functional/CommandTest.php
+++ tests/Functional/CommandTest.php
@@ -13,16 +13,40 @@ namespace CachetHQ\Tests\Cachet\Functional;
 
 use CachetHQ\Tests\Cachet\AbstractTestCase;
 use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 /**
  * This is the command test class.
  *
  * @author Graham Campbell <graham@alt-three.com>
+ * @author James Brooks <james@alt-three.com>
  */
 class CommandTest extends AbstractTestCase
 {
+    use DatabaseMigrations;
+
     public function testMigrations()
     {
         $this->assertSame(0, $this->app->make(Kernel::class)->call('migrate', ['--force' => true]));
+    }
+
+    public function testInstall()
+    {
+        $this->assertSame(0, $this->app->make(Kernel::class)->call('app:install'));
+    }
+
+    public function testUpdate()
+    {
+        $this->assertSame(0, $this->app->make(Kernel::class)->call('app:update'));
+    }
+
+    public function testReset()
+    {
+        $this->assertSame(0, $this->app->make(Kernel::class)->call('app:reset'));
+    }
+
+    public function testSeed()
+    {
+        $this->assertSame(0, $this->app->make(Kernel::class)->call('cachet:seed'));
     }
 }
