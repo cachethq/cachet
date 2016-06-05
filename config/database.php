812160839f72a376ab07637bc0791829502e5dde
James Brooks <jbrooksuk@me.com>
2016-06-05 16:52:18 +0100

    Support a Redis passwords

--- config/database.php
+++ config/database.php
@@ -131,6 +131,7 @@ return [
             'host'     => env('REDIS_HOST', '127.0.0.1'),
             'port'     => env('REDIS_PORT', 6379),
             'database' => env('REDIS_DATABASE', 0),
+            'password' => env('REDIS_PASSWORD', null),
         ],
 
     ],
