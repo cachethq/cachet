7a03bb2140595c61dda03ca04bcf62465feb94e1
James Brooks <james@alt-three.com>
2016-07-12 13:28:25 -0400

    Applied fixes from StyleCI
    [ci skip] [skip ci]

--- database/migrations/2016_07_12_172643_CreatedTimedActionsResponsesTable.php
+++ database/migrations/2016_07_12_172643_CreatedTimedActionsResponsesTable.php
@@ -13,7 +13,6 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-
 class CreatedTimedActionsResponsesTable extends Migration
 {
     /**
