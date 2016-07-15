0d84b68e3b5f099d5db00e686615ecbb5bae17aa
James Brooks <jbrooksuk@me.com>
2016-07-15 13:16:18 +0100

    Implement delete timed action and test it

---
+++ app/Bus/Handlers/Commands/TimedAction/CreateTimedActionCommandHandler.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CachetHQ\Cachet\Bus\Handlers\Commands\TimedAction;
+
+use CachetHQ\Cachet\Bus\Commands\TimedAction\CreateTimedActionCommand;
+use CachetHQ\Cachet\Models\TimedAction;
+
+/**
+ * This is the create timed action command class.
+ *
+ * @author James Brooks <james@alt-three.com>
+ */
+class CreateTimedActionCommandHandler
+{
+    /**
+     * Create a new create timed action command instance.
+     *
+     * @param \CachetHQ\Cachet\Bus\Commands\TimedAction\CreateTimedActionCommand $command
+     *
+     * @return void
+     */
+    public function handle(CreateTimedActionCommand $command)
+    {
+        //
+    }
+}
