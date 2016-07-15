23442613b27760bd94f91d74b54e0fb6283b04e6
James Brooks <jbrooksuk@me.com>
2016-07-15 13:03:44 +0100

    Update the timed action model

--- app/Models/TimedAction.php
+++ app/Models/TimedAction.php
@@ -12,6 +12,9 @@
 namespace CachetHQ\Cachet\Models;
 
 use AltThree\Validator\ValidatingTrait;
+use CachetHQ\Cachet\Models\Traits\SearchableTrait;
+use CachetHQ\Cachet\Models\Traits\SortableTrait;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -21,7 +24,7 @@ use Illuminate\Database\Eloquent\Model;
  */
 class TimedAction extends Model
 {
-    use ValidatingTrait;
+    use SearchableTrait, SortableTrait, ValidatingTrait;
 
     /**
      * The attributes that should be casted to native types.
@@ -68,6 +71,34 @@ class TimedAction extends Model
     ];
 
     /**
+     * The searchable fields.
+     *
+     * @var string[]
+     */
+    protected $searchable = [
+        'id',
+        'name',
+        'active',
+        'timezone',
+        'schedule_frequency',
+        'completion_latency',
+    ];
+
+    /**
+     * The sortable fields.
+     *
+     * @var string[]
+     */
+    protected $sortable = [
+        'id',
+        'name',
+        'active',
+        'timezone',
+        'schedule_frequency',
+        'completion_latency',
+    ];
+
+    /**
      * Get the responses relation.
      *
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
@@ -75,5 +106,17 @@ class TimedAction extends Model
     public function responses()
     {
         return $this->hasMany(TimedActionResponse::class);
+    }
+
+    /**
+     * Scope to enabled timed actions.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeEnabled(Builder $query)
+    {
+        return $query->where('active', true);
     }
 }
