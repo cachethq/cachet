1285d19cd4d0206ea4b97935ed421671c0cac905
James Brooks <james@alt-three.com>
2016-08-04 09:02:42 +0100

    Added missing event tests

---
+++ tests/Bus/Events/System/SystemWasUpdatedEventTest.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CachetHQ\Tests\Cachet\Bus\Events\System;
+
+use CachetHQ\Cachet\Bus\Events\System\SystemWasUpdatedEvent;
+
+/**
+ * This is the system was updated event test class.
+ *
+ * @author James Brooks <james@alt-three.com>
+ */
+class SystemWasUpdatedEventTest extends AbstractSystemEventTestCase
+{
+    protected function objectHasHandlers()
+    {
+        return false;
+    }
+
+    protected function getObjectAndParams()
+    {
+        $params = [];
+        $object = new SystemWasUpdatedEvent();
+
+        return compact('params', 'object');
+    }
+}
