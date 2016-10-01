1407871e1c3a9d4a1c1c14875ffbb70fd66fa79d
Abhimanyu Sharma <abhimanyusharma003@gmail.com>
2016-10-01 21:35:06 +0530

    Fixed closing span tag

--- resources/views/dashboard/subscribers/add.blade.php
+++ resources/views/dashboard/subscribers/add.blade.php
@@ -19,7 +19,7 @@
             <div class="form-group">
                 <label>{{ trans('forms.user.email') }}</label>
                 <textarea rows="5" class="form-control" name="email" required>{{ Binput::old('email') }}</textarea>
-                <span class="help-block">{{ trans('dashboard.subscribers.add.help') }}</p>
+                <span class="help-block">{{ trans('dashboard.subscribers.add.help') }}</span>
             </div>
             </fieldset>
 
