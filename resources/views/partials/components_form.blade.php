c432702740d0413593f32cb6b5373ee4ea0614c7
James Brooks <jbrooksuk@me.com>
2016-10-08 18:54:18 +0100

    Don't indent

--- resources/views/partials/components_form.blade.php
+++ resources/views/partials/components_form.blade.php
@@ -1,23 +1,23 @@
 @if($component_groups->count() > 0)
 @foreach($component_groups as $componentGroup)
-    <ul class="list-group components">
-        @if($componentGroup->enabled_components->count() > 0)
-        <li class="list-group-item group-name">
-            <i class="{{ $componentGroup->collapse_class_with_subscriptions($subscriptions) }} group-toggle"></i>
-            <strong>{{ $componentGroup->name }}</strong>
-            <div class="pull-right text-muted small">
-                <a href="#" class="select-group" id="select-all-{{$componentGroup->id}}">Select All</a>
-                &nbsp;|&nbsp;
-                <a href="#" class="deselect-group" id="deselect-all-{{$componentGroup->id}}">Deselect All</a>
-            </div>
-        </li>
-        <div class="form-group group-items {{ $componentGroup->has_subscriber($subscriptions) ? null : "hide" }}">
-            @foreach($componentGroup->enabled_components()->orderBy('order')->get() as $component)
-            @include('partials.component_input', compact($component))
-            @endforeach
+<ul class="list-group components">
+    @if($componentGroup->enabled_components->count() > 0)
+    <li class="list-group-item group-name">
+        <i class="{{ $componentGroup->collapse_class_with_subscriptions($subscriptions) }} group-toggle"></i>
+        <strong>{{ $componentGroup->name }}</strong>
+        <div class="pull-right text-muted small">
+            <a href="#" class="select-group" id="select-all-{{$componentGroup->id}}">Select All</a>
+            &nbsp;|&nbsp;
+            <a href="#" class="deselect-group" id="deselect-all-{{$componentGroup->id}}">Deselect All</a>
         </div>
-        @endif
-    </ul>
+    </li>
+    <div class="form-group group-items {{ $componentGroup->has_subscriber($subscriptions) ? null : "hide" }}">
+        @foreach($componentGroup->enabled_components()->orderBy('order')->get() as $component)
+        @include('partials.component_input', compact($component))
+        @endforeach
+    </div>
+    @endif
+</ul>
 @endforeach
 @endif
 
