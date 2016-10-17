8dc35a346c4580c1eb65507c0a5062c306c70258
Nicolas Fagotti <nicolasfagotti@gmail.com>
2016-10-17 17:17:48 -0300

    Remove extra html attribute

--- resources/views/partials/components.blade.php
+++ resources/views/partials/components.blade.php
@@ -11,7 +11,7 @@
             <span class="caret"></span>
         </a>
         <ul class="dropdown-menu dropdown-menu-right">
-            <li><a href="{{ cachet_route('status-page') }}" data-filter-component="0">All</a></li>
+            <li><a href="{{ cachet_route('status-page') }}">All</a></li>
             @foreach($all_component_groups as $componentGroup)
             <li><a href="{{ cachet_route('component-status-page', [$componentGroup->id]) }}">{{ $componentGroup->name }}</a></li>
             @endforeach
