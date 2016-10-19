25bba1c1784001cfc17a903d60c607df41c9e66f
Connor S. Parks <connor@connorvg.tv>
2016-10-19 16:28:25 +0100

    re-adds the  middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -33,6 +33,7 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'admin'       => 'CachetHQ\Cachet\Http\Middleware\Admin',
         'can'         => 'Illuminate\Auth\Middleware\Authorize',
+        'auth'        => 'Illuminate\Auth\Middleware\Authenticate',
         'auth.api'    => 'CachetHQ\Cachet\Http\Middleware\ApiAuthentication',
         'guest'       => 'CachetHQ\Cachet\Http\Middleware\RedirectIfAuthenticated',
         'localize'    => 'CachetHQ\Cachet\Http\Middleware\Localize',
