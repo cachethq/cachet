7f3727f37b3df9a7a76bc1666fb1fb940917f6e8
od3n <mr.od3n@gmail.com>
2016-10-20 20:01:55 +0800

    Add Malay translation

---
+++ resources/lang/ms/setup.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'setup'             => 'Setup',
+    'title'             => 'Pasang Cachet',
+    'service_details'   => 'Butiran Perkhidmatan',
+    'env_setup'         => 'Setup Persekitaran',
+    'status_page_setup' => 'Setup Laman Status',
+    'show_support'      => 'Tunjukkan sokongan kepada Cachet?',
+    'admin_account'     => 'Akaun Pentadbir',
+    'complete_setup'    => 'Sempurnakan Setup',
+    'completed'         => 'Cachet telah berjaya dikongfigurasi!',
+    'finish_setup'      => 'Pergi ke dashboard',
+];
