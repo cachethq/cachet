9ec8850f5291c4046b672e434fb144e02d592f6f
James Brooks <jbrooksuk@me.com>
2016-11-07 21:53:45 +0000

    Center footer stuff

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,7 +4,7 @@
 <footer class="footer">
     <div class="container">
         <div class="row">
-            <div class="col-sm-4">
+            <div class="col-sm-8 col-sm-offset-2">
                 @if($show_support)
                 <p>
                     {!! trans('cachet.powered_by') !!}
@@ -13,31 +13,6 @@
                     @endif
                 </p>
                 @endif
-            </div>
-            <div class="col-sm-8">
-                <ul class="list-inline">
-                    @if($current_user || $dashboard_link)
-                    <li>
-                        <a class="btn btn-link" href="{{ cachet_route('dashboard') }}">{{ trans('dashboard.dashboard') }}</a>
-                    </li>
-                    @endif
-                    @if($current_user)
-                    <li>
-                        <a class="btn btn-link" href="{{ cachet_route('auth.logout') }}">{{ trans('dashboard.logout') }}</a>
-                    </li>
-                    @endif
-                    <li>
-                        <a class="btn btn-link" href="{{ cachet_route('feed.rss') }}">{{ trans('cachet.rss-feed') }}</a>
-                    </li>
-                    <li>
-                        <a class="btn btn-link" href="{{ cachet_route('feed.atom') }}">{{ trans('cachet.atom-feed') }}</a>
-                    </li>
-                    @if($enable_subscribers)
-                    <li>
-                        <a class="btn btn-success btn-outline" href="{{ cachet_route('subscribe') }}">{{ trans('cachet.subscriber.button') }}</a>
-                    </li>
-                    @endif
-                </ul>
             </div>
         </div>
     </div>
