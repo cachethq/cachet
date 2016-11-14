d30c14eb64389d51e830db05c266324cad404299
James Brooks <jbrooksuk@me.com>
2016-11-14 21:02:06 +0000

    Fix the field placeholder text

--- resources/views/dashboard/schedule/add.blade.php
+++ resources/views/dashboard/schedule/add.blade.php
@@ -53,7 +53,7 @@
                     </div>
                     <div class="form-group">
                         <label>{{ trans('forms.schedules.scheduled_at') }}</label>
-                        <input type="text" name="completed_at" class="form-control" rel="datepicker-custom" data-date-format="YYYY-MM-DD HH:mm" placeholder="{{ trans('forms.schedules.scheduled_at') }}">
+                        <input type="text" name="completed_at" class="form-control" rel="datepicker-custom" data-date-format="YYYY-MM-DD HH:mm" placeholder="{{ trans('forms.schedules.completed_at') }}">
                     </div>
                 </fieldset>
 
