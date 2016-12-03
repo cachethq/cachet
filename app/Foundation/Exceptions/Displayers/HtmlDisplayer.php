8a5659edefa87681dd848c5967e9d032b2bde4a0
James Brooks <james@alt-three.com>
2016-12-03 22:04:37 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Foundation/Exceptions/Displayers/HtmlDisplayer.php
+++ app/Foundation/Exceptions/Displayers/HtmlDisplayer.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- * This file is part of Laravel Exceptions.
+ * This file is part of Cachet.
  *
- * (c) Graham Campbell <graham@alt-three.com>
+ * (c) Alt Three Services Limited
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
