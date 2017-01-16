f275a79a10e031736cae7ab04ec36007d43c7ea0
Joseph Cohen <joseph.cohen@dinkbit.com>
2017-01-15 23:47:47 -0600

    Update single schedule view

--- resources/views/single-schedule.blade.php
+++ resources/views/single-schedule.blade.php
@@ -3,29 +3,12 @@
 @section('bodyClass', 'no-padding')
 
 @section('content')
-<h1>{{ $schedule->name }}</h1>
+<h1>
+    <i class="{{ $schedule->latest_icon }}"></i>
+    {{ $schedule->name }} <small>{{ formatted_date($schedule->created_at) }}</small>
+</h1>
 
-<div class="timeline">
-    <div class="content-wrapper">
-        <div class="moment first">
-            <div class="row event clearfix">
-                <div class="col-sm-1">
-                    <div class="status-icon status-{{ $schedule->status }}" data-toggle="tooltip" title="{{ $schedule->human_status }}" data-placement="left">
-                        <i class="icon ion-android-calendar"></i>
-                    </div>
-                </div>
-                <div class="col-xs-10 col-xs-offset-2 col-sm-11 col-sm-offset-0">
-                    <div class="panel panel-message incident">
-                        <div class="panel-heading">
-                            <strong>{{ $schedule->name }}</strong>{{ trans("cachet.incidents.scheduled_at", ["timestamp" => $schedule->scheduled_at_diff]) }}
-                        </div>
-                        <div class="panel-body">
-                            {!! $schedule->formatted_message !!}
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="markdown-body">
+    {!! $schedule->formatted_message !!}
 </div>
 @stop
