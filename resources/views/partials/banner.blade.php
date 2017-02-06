5a2aa42b668bc5da6c25a9c0f2a14243e70de17e
James Brooks <jbrooksuk@me.com>
2017-02-06 21:03:52 +0000

    Nav changes

--- resources/views/partials/banner.blade.php
+++ resources/views/partials/banner.blade.php
@@ -1,7 +1,6 @@
-@if($app_header)
+@if ($app_header)
 {!! $app_header !!}
-@else
-@if($app_banner)
+@elseif ($app_banner)
 <div @if($app_banner_style_full_width)class="app-banner"@endif>
     <div class="container">
         <div class="row app-banner-padding  @if(!$app_banner_style_full_width) app-banner @endif">
@@ -15,5 +14,28 @@
         </div>
     </div>
 </div>
-@endif
+@else
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="{{ cachet_route('status-page') }}">{{ $app_name }}</a>
+        </div>
+
+        <div class="navbar-right">
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="navbar-item"><a href="{{ cachet_route('feed.atom') }}">RSS</a></li>
+                    <li class="navbar-item"><a href="{{ cachet_route('feed.rss') }}">Atom</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
+<br>
 @endif
