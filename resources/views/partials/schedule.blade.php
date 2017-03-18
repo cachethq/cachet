f7435748ecf98d86d33565a8fedb20e7d6aac9c2
James Brooks <jbrooksuk@me.com>
2017-03-18 10:15:34 +0000

    Fixes #2413

--- resources/views/partials/schedule.blade.php
+++ resources/views/partials/schedule.blade.php
@@ -5,7 +5,7 @@
         </div>
         <div class="list-group">
             @foreach($scheduled_maintenance as $schedule)
-            <a href="{{ cachet_route('schedule', $schedule) }}" class="list-group-item">
+            <div class="list-group-item">
                 <strong>{{ $schedule->name }}</strong> <small class="date"><abbr class="timeago" data-toggle="tooltip" data-placement="right" title="{{ $schedule->scheduled_at_formatted }}" data-timeago="{{ $schedule->scheduled_at_iso }}"></abbr></small>
                 {!! $schedule->formatted_message !!}
                 @if($schedule->components->count() > 0)
@@ -14,7 +14,7 @@
                 <span class="label label-primary">{{ $affected_component->component->name }}</span>
                 @endforeach
                 @endif
-            </a>
+            </div>
             @endforeach
         </div>
     </div>
