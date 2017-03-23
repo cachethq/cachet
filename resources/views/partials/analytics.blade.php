3d2523886a606f9f9d2a06a2a053187941af1c95
James Brooks <jbrooksuk@me.com>
2017-03-23 22:51:43 +0000

    Use auto in ga call. Closes #2454

--- resources/views/partials/analytics.blade.php
+++ resources/views/partials/analytics.blade.php
@@ -5,7 +5,7 @@
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-    ga('create', '{{ $app_analytics }}', '{{ $_SERVER['SERVER_NAME'] or '' }}');
+    ga('create', '{{ $app_analytics }}', 'auto');
     ga('send', 'pageview');
 </script>
 @endif
