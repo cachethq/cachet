e659504d3a0b9b51270778bb2cec29af071848d7
James Brooks <james@alt-three.com>
2017-05-06 10:04:51 +0100

    New translations setup.php (Indonesian)

---
+++ resources/lang/id-ID/setup.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'setup'             => 'Instalasi',
+    'title'             => 'Instalasi Cachet',
+    'service_details'   => 'Detail Layanan',
+    'env_setup'         => 'Setup lingkungan',
+    'status_page_setup' => 'Penyiapan Halaman Status',
+    'show_support'      => 'Tunjukkan dukungan untuk Cachet? <small>Tampilkan catatan di bawah halaman yang bertaut ke situs Cachet.</small>',
+    'admin_account'     => 'Akun Administrator',
+    'complete_setup'    => 'Selesaikan Instalasi',
+    'completed'         => 'Cachet berhasil dikonfigurasi!',
+    'finish_setup'      => 'Ke dashboard',
+];
