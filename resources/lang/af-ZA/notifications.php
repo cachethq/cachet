1e640d26acbd64a1e858167a61bd5e872be38044
James Brooks <james@alt-three.com>
2017-05-06 10:07:57 +0100

    New translations notifications.php (Afrikaans)

---
+++ resources/lang/af-ZA/notifications.php
@@ -0,0 +1,108 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'component' => [
+        'status_update' => [
+            'mail' => [
+                'subject'  => 'Component Status Updated',
+                'greeting' => 'A component\'s status was updated!',
+                'content'  => ':name status changed from :old_status to :new_status.',
+                'action'   => 'View',
+            ],
+            'slack' => [
+                'title'   => 'Component Status Updated',
+                'content' => ':name status changed from :old_status to :new_status.',
+            ],
+            'sms' => [
+                'content' => ':name status changed from :old_status to :new_status.',
+            ],
+        ],
+    ],
+    'incident' => [
+        'new' => [
+            'mail' => [
+                'subject'  => 'New Incident Reported',
+                'greeting' => 'A new incident was reported at :app_name.',
+                'content'  => 'Incident :name was reported',
+                'action'   => 'View',
+            ],
+            'slack' => [
+                'title'   => 'Incident :name Reported',
+                'content' => 'A new incident was reported at :app_name',
+            ],
+            'sms' => [
+                'content' => 'A new incident was reported at :app_name.',
+            ],
+        ],
+        'update' => [
+            'mail' => [
+                'subject' => 'Incident Updated',
+                'content' => ':name was updated',
+                'title'   => ':name was updated to :new_status',
+                'action'  => 'View',
+            ],
+            'slack' => [
+                'title'   => ':name Updated',
+                'content' => ':name was updated to :new_status',
+            ],
+            'sms' => [
+                'content' => 'Incident :name was updated',
+            ],
+        ],
+    ],
+    'schedule' => [
+        'new' => [
+            'mail' => [
+                'subject' => 'New Schedule Created',
+                'content' => ':name was scheduled for :date',
+                'title'   => 'A new scheduled maintenance was created.',
+                'action'  => 'View',
+            ],
+            'slack' => [
+                'title'   => 'New Schedule Created!',
+                'content' => ':name was scheduled for :date',
+            ],
+            'sms' => [
+                'content' => ':name was scheduled for :date',
+            ],
+        ],
+    ],
+    'subscriber' => [
+        'verify' => [
+            'mail' => [
+                'subject' => 'Verify Your Subscription',
+                'content' => 'Click to verify your subscription to :app_name status page.',
+                'title'   => 'Verify your subscription to :app_name status page.',
+                'action'  => 'Verify',
+            ],
+        ],
+    ],
+    'system' => [
+        'test' => [
+            'mail' => [
+                'subject' => 'Ping from Cachet!',
+                'content' => 'This is a test notification from Cachet!',
+                'title'   => '🔔',
+            ],
+        ],
+    ],
+    'user' => [
+        'invite' => [
+            'mail' => [
+                'subject' => 'Your invitation is inside...',
+                'content' => 'You have been invited to join :app_name status page.',
+                'title'   => 'You\'re invited to join :app_name status page.',
+                'action'  => 'Accept',
+            ],
+        ],
+    ],
+];
