b0daab01e9411a6d87ce6cbd7b03e083c8e5813e
James Brooks <james@alt-three.com>
2017-05-06 10:06:51 +0100

    New translations validation.php (Russian)

---
+++ resources/lang/ru-RU/validation.php
@@ -0,0 +1,122 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+
+    'accepted'   => ':attribute должен быть принят.',
+    'active_url' => ':attribute не является допустимым URL.',
+    'after'      => ':attribute должно быть датой после :date.',
+    'alpha'      => ':attribute может содержать только буквы.',
+    'alpha_dash' => ':attribute может содержать только латинские буквы, цифры и дефис.',
+    'alpha_num'  => ':attribute может содержать только буквы и цифры.',
+    'array'      => ': attribute должно быть массивом.',
+    'before'     => ':attribute должно быть датой до :date.',
+    'between'    => [
+        'numeric' => 'Значение :attribute должно быть между :min и :max.',
+        'file'    => 'Значение :attribute должно быть между :min и :max килобайт.',
+        'string'  => 'Значение :attribute должно быть между :min и :max символов.',
+        'array'   => ':attribute должно содержать от :min до :max элементов.',
+    ],
+    'boolean'        => 'Значение :attribute должно быть true или false.',
+    'confirmed'      => 'The :attribute confirmation does not match.',
+    'date'           => 'Значение :attribute не является корректной датой.',
+    'date_format'    => 'Значение :attribute не соответствует формату :format.',
+    'different'      => 'Значения :attribute и :other должны быть разными.',
+    'digits'         => 'The :attribute must be :digits digits.',
+    'digits_between' => 'The :attribute must be between :min and :max digits.',
+    'email'          => 'The :attribute must be a valid email address.',
+    'exists'         => 'The selected :attribute is invalid.',
+    'distinct'       => 'Поле :attribute содержит дублирующееся значение.',
+    'filled'         => 'The :attribute field is required.',
+    'image'          => ':attribute должно быть изображением.',
+    'in'             => 'The selected :attribute is invalid.',
+    'in_array'       => 'Поле :attribute не существует в :other.',
+    'integer'        => 'The :attribute must be an integer.',
+    'ip'             => 'The :attribute must be a valid IP address.',
+    'json'           => ':attribute должен быть в JSON формате.',
+    'max'            => [
+        'numeric' => 'The :attribute may not be greater than :max.',
+        'file'    => 'The :attribute may not be greater than :max kilobytes.',
+        'string'  => 'The :attribute may not be greater than :max characters.',
+        'array'   => ':attribute не может содержать больше чем :max элементов.',
+    ],
+    'mimes' => 'The :attribute must be a file of type: :values.',
+    'min'   => [
+        'numeric' => 'The :attribute must be at least :min.',
+        'file'    => ':attribute должно быть не меньше :min килобайт.',
+        'string'  => 'The :attribute must be at least :min characters.',
+        'array'   => 'The :attribute must have at least :min items.',
+    ],
+    'not_in'               => 'The selected :attribute is invalid.',
+    'numeric'              => 'Значение :attribute должно быть числом.',
+    'present'              => 'Поле :attribute должно быть заполнено.',
+    'regex'                => 'Неправильный формат :attribute.',
+    'required'             => 'The :attribute field is required.',
+    'required_if'          => 'The :attribute field is required when :other is :value.',
+    'required_unless'      => 'Поле :attribute обязательно, если :other не из :values.',
+    'required_with'        => 'Поле :attribute является обязательным, если все :values заполнены.',
+    'required_with_all'    => 'Поле :attribute является обязательным, если все :values заполнены.',
+    'required_without'     => 'The :attribute field is required when :values is not present.',
+    'required_without_all' => 'The :attribute field is required when none of :values are present.',
+    'same'                 => 'The :attribute and :other must match.',
+    'size'                 => [
+        'numeric' => 'The :attribute must be :size.',
+        'file'    => ':attribute должно быть объемом :size килобайт.',
+        'string'  => 'Поле :attribute должно содержать :size символов.',
+        'array'   => 'The :attribute must contain :size items.',
+    ],
+    'string'   => 'The :attribute must be a string.',
+    'timezone' => ':attribute должно быть корректным часовым поясом.',
+    'unique'   => ':attribute уже занято.',
+    'url'      => 'Неправильный формат :attribute.',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify custom validation messages for attributes using the
+    | convention "attribute.rule" to name the lines. This makes it quick to
+    | specify a specific custom language line for a given attribute rule.
+    |
+    */
+
+    'custom' => [
+        'attribute-name' => [
+            'rule-name' => 'настраиваемое сообщение',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap attribute place-holders
+    | with something more reader friendly such as E-Mail Address instead
+    | of "email". This simply helps us make messages a little cleaner.
+    |
+    */
+
+    'attributes' => [],
+
+];
