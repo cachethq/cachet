00a299c94c109b2502251021e95fcbe36f79de7f
James Brooks <james@alt-three.com>
2017-05-06 10:08:33 +0100

    New translations setup.php (French)

---
+++ resources/lang/fr-FR/setup.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'setup'             => 'Installation',
+    'title'             => 'Configuration de Cachet',
+    'service_details'   => 'Détails du service',
+    'env_setup'         => 'Configuration de l\'environnement',
+    'status_page_setup' => 'Configuration de la page de statut',
+    'show_support'      => 'Afficher votre soutien à Cachet ?',
+    'admin_account'     => 'Compte administrateur',
+    'complete_setup'    => 'Terminer la configuration',
+    'completed'         => 'Cachet a été configuré avec succès !',
+    'finish_setup'      => 'Aller au tableau de bord',
+];
