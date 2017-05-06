314feaa2a9a94f5e5b737dd7b0d6960e0e344877
James Brooks <james@alt-three.com>
2017-05-06 10:08:50 +0100

    New translations setup.php (Greek)

---
+++ resources/lang/el-GR/setup.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'setup'             => 'Εγκατάσταση',
+    'title'             => 'Εγκατάσταση του Cachet',
+    'service_details'   => 'Service Details',
+    'env_setup'         => 'Environment Setup',
+    'status_page_setup' => 'Status Page Setup',
+    'show_support'      => 'Show support for Cachet?',
+    'admin_account'     => 'Administrator Account',
+    'complete_setup'    => 'Ολοκλήρωση εγκατάστασης',
+    'completed'         => 'Cachet has been configured successfully!',
+    'finish_setup'      => 'Go to dashboard',
+];
