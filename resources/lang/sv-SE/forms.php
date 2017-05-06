a78fe308fa318626f3c9e4541c5861fdf269680b
James Brooks <james@alt-three.com>
2017-05-06 10:06:45 +0100

    New translations forms.php (Swedish)

--- resources/lang/sv-SE/forms.php
+++ resources/lang/sv-SE/forms.php
@@ -22,7 +22,13 @@ return [
         'site_locale'      => 'Välj ditt språk',
         'enable_google2fa' => 'Aktivera Google tvåfaktorsautentisering',
         'cache_driver'     => 'Cachedrivrutin',
+        'queue_driver'     => 'Queue Driver',
         'session_driver'   => 'Sessionsdrivrutin',
+        'mail_driver'      => 'Mail Driver',
+        'mail_host'        => 'Mail Host',
+        'mail_address'     => 'Mail From Address',
+        'mail_username'    => 'Mail Username',
+        'mail_password'    => 'Mail Password',
     ],
 
     // Login form fields
@@ -35,6 +41,7 @@ return [
         'invalid-token' => 'Ogiltig nyckel',
         'cookies'       => 'Du måste aktivera cookies för att kunna logga in.',
         'rate-limit'    => 'Rate limit exceeded.',
+        'remember_me'   => 'Kom ihåg mig',
     ],
 
     // Incidents form fields
@@ -44,13 +51,29 @@ return [
         'component'          => 'Komponent',
         'message'            => 'Meddelande',
         'message-help'       => 'Du kan även använda Markdown.',
-        'scheduled_at'       => 'När vill du schemalägga underhållet?',
-        'incident_time'      => 'När inträffade händelsen?',
+        'occurred_at'        => 'When did this incident occur?',
         'notify_subscribers' => 'Meddela prenumeranter?',
         'visibility'         => 'Incident Visibility',
+        'stick_status'       => 'Stick Incident',
+        'stickied'           => 'Stickied',
+        'not_stickied'       => 'Not Stickied',
         'public'             => 'Kan ses av allmänheten',
         'logged_in_only'     => 'Endast synlig för inloggade användare',
         'templates'          => [
+            'name'     => 'Namn',
+            'template' => 'Mall',
+            'twig'     => 'Händelsmallar kan använda <a href="http://twig.sensiolabs.org/" target="_blank">Twig</a>-mallspråk.',
+        ],
+    ],
+
+    'schedules' => [
+        'name'         => 'Namn',
+        'status'       => 'Status',
+        'message'      => 'Meddelande',
+        'message-help' => 'Du kan även använda Markdown.',
+        'scheduled_at' => 'When is this maintenance scheduled for?',
+        'completed_at' => 'When did this maintenance complete?',
+        'templates'    => [
             'name'     => 'Namn',
             'template' => 'Mall',
             'twig'     => 'Händelsmallar kan använda <a href="http://twig.sensiolabs.org/" target="_blank">Twig</a>-mallspråk.',
@@ -69,28 +92,50 @@ return [
         'enabled'     => 'Komponent aktiverad?',
 
         'groups' => [
-            'name'               => 'Namn',
-            'collapsing'         => 'Choose visibility of the group',
-            'visible'            => 'Always expanded',
-            'collapsed'          => 'Collapse the group by default',
-            'collapsed_incident' => 'Collapse the group, but expand if there are issues',
+            'name'                     => 'Namn',
+            'collapsing'               => 'Expand/Collapse options',
+            'visible'                  => 'Alltid expanderad',
+            'collapsed'                => 'Collapse the group by default',
+            'collapsed_incident'       => 'Collapse the group, but expand if there are issues',
+            'visibility'               => 'Synlighet',
+            'visibility_public'        => 'Publikt synlig',
+            'visibility_authenticated' => 'Synlig endast för inloggade användare',
+        ],
+    ],
+
+    // Action form fields
+    'actions' => [
+        'name'               => 'Namn',
+        'description'        => 'Beskrivning',
+        'start_at'           => 'Schedule start time',
+        'timezone'           => 'Tidszon',
+        'schedule_frequency' => 'Schedule frequency (in seconds)',
+        'completion_latency' => 'Completion latency (in seconds)',
+        'group'              => 'Grupp',
+        'active'             => 'Aktiv?',
+        'groups'             => [
+            'name' => 'Gruppnamn',
         ],
     ],
 
     // Metric form fields
     'metrics' => [
-        'name'             => 'Namn',
-        'suffix'           => 'Suffix',
-        'description'      => 'Beskrivning',
-        'description-help' => 'Du kan även använda Markdown.',
-        'display-chart'    => 'Visa diagram på statussidan?',
-        'default-value'    => 'Standardvärde',
-        'calc_type'        => 'Beräkning av mätetal',
-        'type_sum'         => 'Summa',
-        'type_avg'         => 'Medelvärde',
-        'places'           => 'Decimalplatser',
-        'default_view'     => 'Standardvy',
-        'threshold'        => 'How many minutes of threshold between metric points?',
+        'name'                     => 'Namn',
+        'suffix'                   => 'Suffix',
+        'description'              => 'Beskrivning',
+        'description-help'         => 'Du kan även använda Markdown.',
+        'display-chart'            => 'Visa diagram på statussidan?',
+        'default-value'            => 'Standardvärde',
+        'calc_type'                => 'Beräkning av mätetal',
+        'type_sum'                 => 'Summa',
+        'type_avg'                 => 'Medelvärde',
+        'places'                   => 'Decimalplatser',
+        'default_view'             => 'Standardvy',
+        'threshold'                => 'How many minutes of threshold between metric points?',
+        'visibility'               => 'Synlighet',
+        'visibility_authenticated' => 'Synlig för autentiserade användare',
+        'visibility_public'        => 'Synlig för alla',
+        'visibility_hidden'        => 'Alltid dold',
 
         'points' => [
             'value' => 'Värde',
@@ -99,17 +144,21 @@ return [
 
     // Settings
     'settings' => [
-        /// Application setup
+        // Application setup
         'app-setup' => [
-            'site-name'              => 'Webbplatsens namn',
-            'site-url'               => 'Webbplatsens URL',
-            'display-graphs'         => 'Visa grafer på statussidan?',
-            'about-this-page'        => 'Om den här sidan',
-            'days-of-incidents'      => 'Hur många dagar av händelser ska visas?',
-            'banner'                 => 'Banner Image',
-            'banner-help'            => 'Vi rekommenderar att du inte laddar upp bilder som är bredare än 930 px.',
-            'subscribers'            => 'Tillåt att registrera sig för notifikationer via e-post?',
-            'automatic_localization' => 'Automatically localise your status page to your visitor\'s language?',
+            'site-name'                    => 'Webbplatsens namn',
+            'site-url'                     => 'Webbplatsens URL',
+            'display-graphs'               => 'Visa grafer på statussidan?',
+            'about-this-page'              => 'Om den här sidan',
+            'days-of-incidents'            => 'Hur många dagar av händelser ska visas?',
+            'banner'                       => 'Bannerbild',
+            'banner-help'                  => "Vi rekommenderar att du inte laddar upp bilder som är bredare än 930 px.",
+            'subscribers'                  => 'Tillåt att registrera sig för notifikationer via e-post?',
+            'skip_subscriber_verification' => 'Skip verifying of users? (Be warned, you could be spammed)',
+            'automatic_localization'       => 'Automatically localise your status page to your visitor\'s language?',
+            'enable_external_dependencies' => 'Enable Third Party Dependencies (Google Fonts, Trackers, etc...)',
+            'show_timezone'                => 'Show the timezone the status page is running in.',
+            'only_disrupted_days'          => 'Only show days containing incidents in the timeline?',
         ],
         'analytics' => [
             'analytics_google'       => 'Google Analytics-kod',
@@ -128,15 +177,15 @@ return [
             'allowed-domains-help' => 'Kommaseparerad. Domänerna ovan tillåts automatiskt som standard.',
         ],
         'stylesheet' => [
-            'custom-css' => 'Custom Stylesheet',
+            'custom-css' => 'Anpassad stilmall',
         ],
         'theme' => [
-            'background-color'        => 'Background Color',
+            'background-color'        => 'Bakgrundsfärg',
             'background-fills'        => 'Bakgrundsfärg (komponenter, händelser, sidfot)',
             'banner-background-color' => 'Bakgrundsfärg för banner',
             'banner-padding'          => 'Bannerutfyllnad',
             'fullwidth-banner'        => 'Aktivera fullbreddsbanner?',
-            'text-color'              => 'Text Color',
+            'text-color'              => 'Textfärg',
             'dashboard-login'         => 'Visa länk till översiktspanelen i sidfoten?',
             'reds'                    => 'Röd (används för fel)',
             'blues'                   => 'Blå (används för information)',
@@ -167,6 +216,10 @@ return [
             'description' => 'Bjud in dina teammedlemmar genom att fylla i deras epostadresser här.',
             'email'       => 'Epostadress #:id',
         ],
+    ],
+
+    'general' => [
+        'timezone' => 'Välj tidszon',
     ],
 
     // Buttons
