4e20294a4d28ddba1cdf84e9f2d61e40d1efdf1b
James Brooks <james@alt-three.com>
2017-05-06 10:05:22 +0100

    New translations setup.php (Japanese)

---
+++ resources/lang/ja-JP/setup.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'setup'             => 'セットアップ',
+    'title'             => 'Cachetのセットアップ',
+    'service_details'   => 'サービスの詳細情報',
+    'env_setup'         => '環境設定',
+    'status_page_setup' => 'ステータスページのセットアップ',
+    'show_support'      => 'Show support for Cachet?',
+    'admin_account'     => '管理者アカウント',
+    'complete_setup'    => 'セットアップの完了',
+    'completed'         => 'Cachetの設定が正常に終わりました！',
+    'finish_setup'      => 'ダッシュボード画面に移動',
+];
