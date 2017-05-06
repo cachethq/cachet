ff79cc69ef4524d992062c7016f7d6fabbaa252d
James Brooks <james@alt-three.com>
2017-05-06 11:20:18 +0100

    New translations notifications.php (French)

--- resources/lang/fr-FR/notifications.php
+++ resources/lang/fr-FR/notifications.php
@@ -31,66 +31,66 @@ return [
         'new' => [
             'mail' => [
                 'subject'  => 'Nouvel incident signalé',
-                'greeting' => 'A new incident was reported at :app_name.',
-                'content'  => 'Incident :name was reported',
+                'greeting' => 'Un nouvel incident a été signalé pour :app_name.',
+                'content'  => 'Incident :name a été signalé',
                 'action'   => 'View',
             ],
             'slack' => [
-                'title'   => 'Incident :name Reported',
-                'content' => 'A new incident was reported at :app_name',
+                'title'   => 'Incident :name signalé',
+                'content' => 'Un nouvel incident a été signalé pour :app_name',
             ],
             'sms' => [
-                'content' => 'A new incident was reported at :app_name.',
+                'content' => 'Un nouvel incident a été signalé pour :app_name.',
             ],
         ],
         'update' => [
             'mail' => [
-                'subject' => 'Incident Updated',
-                'content' => ':name was updated',
-                'title'   => ':name was updated to :new_status',
+                'subject' => 'Incident mis à jour',
+                'content' => ':name a été mis à jour',
+                'title'   => ':name est passé à :new_status',
                 'action'  => 'View',
             ],
             'slack' => [
-                'title'   => ':name Updated',
-                'content' => ':name was updated to :new_status',
+                'title'   => ':name mis à jour',
+                'content' => ':name est passé à :new_status',
             ],
             'sms' => [
-                'content' => 'Incident :name was updated',
+                'content' => 'Incident :name a été mis à jour',
             ],
         ],
     ],
     'schedule' => [
         'new' => [
             'mail' => [
-                'subject' => 'New Schedule Created',
-                'content' => ':name was scheduled for :date',
-                'title'   => 'A new scheduled maintenance was created.',
+                'subject' => 'Nouvelle planification créée',
+                'content' => ':name a été planifié pour :date',
+                'title'   => 'Une nouvelle maintenance planifiée a été créée.',
                 'action'  => 'View',
             ],
             'slack' => [
-                'title'   => 'New Schedule Created!',
-                'content' => ':name was scheduled for :date',
+                'title'   => 'Nouvelle planification créée !',
+                'content' => ':name a été planifié pour :date',
             ],
             'sms' => [
-                'content' => ':name was scheduled for :date',
+                'content' => ':name a été planifié pour :date',
             ],
         ],
     ],
     'subscriber' => [
         'verify' => [
             'mail' => [
-                'subject' => 'Verify Your Subscription',
-                'content' => 'Click to verify your subscription to :app_name status page.',
-                'title'   => 'Verify your subscription to :app_name status page.',
-                'action'  => 'Verify',
+                'subject' => 'Veuillez vérifier votre abonnement',
+                'content' => 'Cliquez ici pour vérifier votre abonnement à la page de statut de :app_name.',
+                'title'   => 'Vérifiez votre abonnement à la page de statut de :app_name.',
+                'action'  => 'Vérifier',
             ],
         ],
     ],
     'system' => [
         'test' => [
             'mail' => [
-                'subject' => 'Ping from Cachet!',
-                'content' => 'This is a test notification from Cachet!',
+                'subject' => 'Ping depuis Cachet!',
+                'content' => 'Ceci est une notification de test depuis Cachet !',
                 'title'   => '🔔',
             ],
         ],
@@ -98,10 +98,10 @@ return [
     'user' => [
         'invite' => [
             'mail' => [
-                'subject' => 'Your invitation is inside...',
-                'content' => 'You have been invited to join :app_name status page.',
-                'title'   => 'You\'re invited to join :app_name status page.',
-                'action'  => 'Accept',
+                'subject' => 'Votre invitation est à l\'intérieur...',
+                'content' => 'Vous avez été invité à rejoindre la page de statut de :app_name.',
+                'title'   => 'Vous êtes invité à rejoindre la page de statut de :app_name.',
+                'action'  => 'Accepter',
             ],
         ],
     ],
