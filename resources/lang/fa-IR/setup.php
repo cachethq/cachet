fc78e966f18dea0d34dd36bd9659775a23b8d32d
James Brooks <james@alt-three.com>
2017-05-06 10:04:15 +0100

    New translations setup.php (Persian)

---
+++ resources/lang/fa-IR/setup.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'setup'             => 'نصب',
+    'title'             => 'نصب Cachet',
+    'service_details'   => 'جزئیات سرویس',
+    'env_setup'         => 'نصب محیط برنامه',
+    'status_page_setup' => 'نصب صفحه وضعیت',
+    'show_support'      => 'پشتیبانی از Cachet نمایش داده شود؟',
+    'admin_account'     => 'حساب مدیر سیستم',
+    'complete_setup'    => 'تکمیل عملیات نصب',
+    'completed'         => 'Cachet با موفقیت تنظیم و راه‌اندازی شد!',
+    'finish_setup'      => 'رفتن به داشبورد',
+];
