77ed9383f980b0fc9f5619e65451b05891f1eeaf
James Brooks <james@alt-three.com>
2017-05-06 10:04:18 +0100

    New translations setup.php (Norwegian)

---
+++ resources/lang/no-NO/setup.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'setup'             => 'Konfigurering',
+    'title'             => 'Installer Cachet',
+    'service_details'   => 'Tjenestedetaljer',
+    'env_setup'         => 'Miljøoppsett',
+    'status_page_setup' => 'Statusside-oppsett',
+    'show_support'      => 'Vis at du støtter Cachet?',
+    'admin_account'     => 'Administratorkonto',
+    'complete_setup'    => 'Fullfør installasjon',
+    'completed'         => 'Konfigurering av Cachet var vellykket!',
+    'finish_setup'      => 'Gå til dashbord',
+];
