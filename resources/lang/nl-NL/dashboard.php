7939f06fd1c85fcd8b148145d0a8ee90e3e8cff0
James Brooks <james@alt-three.com>
2017-05-07 10:10:12 +0100

    New translations dashboard.php (Dutch)

--- resources/lang/nl-NL/dashboard.php
+++ resources/lang/nl-NL/dashboard.php
@@ -39,7 +39,7 @@ return [
         'update' => [
             'title'    => 'Maak een nieuwe incident update',
             'subtitle' => 'Voeg een update toe aan <strong>:incident</strong>',
-            'success'  => 'Update added.',
+            'success'  => 'Update toegevoegd.',
         ],
 
         // Incident templates
