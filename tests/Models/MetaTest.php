1faee910f34cb6a177447bc7f8c346f8d100a743
James Brooks <jbrooksuk@me.com>
2017-06-13 19:25:16 +0100

    Add test for meta model

---
+++ tests/Models/MetaTest.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CachetHQ\Tests\Cachet\Models;
+
+use AltThree\TestBench\ValidationTrait;
+use CachetHQ\Cachet\Models\Meta;
+use CachetHQ\Tests\Cachet\AbstractTestCase;
+
+/**
+ * This is the meta model test class.
+ *
+ * @author James Brooks <james@alt-three.com>
+ */
+class MetaTest extends AbstractTestCase
+{
+    use ValidationTrait;
+
+    public function testValidation()
+    {
+        $this->checkRules(new Meta());
+    }
+}
