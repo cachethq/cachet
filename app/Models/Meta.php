4b9e0141e8ec9ed2915d7e7012acda6bb8431111
James Brooks <jbrooksuk@me.com>
2017-06-13 19:23:53 +0100

    Create meta table and model

---
+++ app/Models/Meta.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CachetHQ\Cachet\Models;
+
+use AltThree\Validator\ValidatingTrait;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * This is the meta model class.
+ *
+ * @author James Brooks <james@alt-three.com>
+ */
+class Meta extends Model
+{
+    use ValidatingTrait;
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var string[]
+     */
+    protected $casts = [
+        'id'        => 'int',
+        'key'       => 'string',
+        'value'     => 'json',
+        'meta_id'   => 'int',
+        'meta_type' => 'string',
+    ];
+
+    /**
+     * The validation rules.
+     *
+     * @var string[]
+     */
+    public $rules = [
+        'id'        => 'nullable|int|min:1',
+        'key'       => 'required|string',
+        'value'     => 'nullable',
+        'meta_id'   => 'required|int',
+        'meta_type' => 'required|string',
+    ];
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'meta';
+
+    /**
+    * Get all of the owning meta models.
+    *
+    * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+    */
+   public function meta()
+   {
+       return $this->morphTo();
+   }
+}
---
+++ database/migrations/2017_06_13_181049_CreateMetaTable.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMetaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meta', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('key')->index();
+            $table->string('value');
+            $table->integer('meta_id')->unsigned();
+            $table->string('meta_type');
+            $table->timestamps();
+
+            $table->index(['meta_id', 'meta_type']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meta');
+    }
+}
