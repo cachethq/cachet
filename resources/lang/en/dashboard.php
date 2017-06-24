261be1dfef7d18da1e49f915705245e9923434a4
James Brooks <jbrooksuk@me.com>
2017-06-24 18:33:28 +0100

    Fixes #2579

--- resources/lang/en/dashboard.php
+++ resources/lang/en/dashboard.php
@@ -18,10 +18,10 @@ return [
     'incidents' => [
         'title'                    => 'Incidents & Schedule',
         'incidents'                => 'Incidents',
-        'logged'                   => '{0} There are no incidents, good work.|You have logged one incident.|You have reported <strong>:count</strong> incidents.',
+        'logged'                   => '{0} There are no incidents, good work.|[1] You have logged one incident.|[2, Inf] You have reported <strong>:count</strong> incidents.',
         'incident-create-template' => 'Create Template',
         'incident-templates'       => 'Incident Templates',
-        'updates'                  => '{0} Zero Updates|One Update|:count Updates',
+        'updates'                  => '{0} Zero Updates|[1] One Update|[2] Two Updates|[3,Inf] Several Updates',
         'add'                      => [
             'title'   => 'Report an incident',
             'success' => 'Incident added.',
