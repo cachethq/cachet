5fb8557af58f15a94d565cc908d44e5cbb744aed
Brendan Benke <brendan.benke@menlosecurity.com>
2017-07-05 21:59:38 -0400

    Make system last updated message responsive

--- resources/views/partials/modules/status.blade.php
+++ resources/views/partials/modules/status.blade.php
@@ -1,7 +1,7 @@
 <div class="section-status">
     <div class="alert alert-{{ $system_status }}">{{ $system_message }}
     @if ($show_system_updated)
-        <span class="pull-right"><small>{{ trans('cachet.service.updated') }} {{ $system_updated }}</small></span>
+        <span class="pull-right hidden-xs"><small>{{ trans('cachet.service.updated') }} {{ $system_updated }}</small></span>
     @endif
     </div>
 </div>
