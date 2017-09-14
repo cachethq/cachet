216befb1c4836f2241b1275ffd6db7fa32a7a6ce
Luke Cousins <luke@viovet.co.uk>
2017-09-14 17:07:58 +0100

    Fixing issue with setting maintenance mode

--- deploy.dog/InstallOrUpdate.sh
+++ deploy.dog/InstallOrUpdate.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+CWD=$(pwd)
+
 # Determine if our key file exists as this determine if we have a key already (and so run an upgrade) or need to do a new install
 if [ ! -f ../../../shared/deploy.dog.cachet-key ]; then
     # File not found, new install
@@ -27,8 +29,15 @@ else
     # Add key into .env file
     KEY=$(head -n 1 ../../../shared/deploy.dog.cachet-key)
     sed -i "s/DDREPLACECACHETKEY/$(echo $KEY | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/" .env
+    
+    # Take down the current version (which is in a different directory to this one)
+    cd ../../../current/code
+    php artisan down
+    cd "$CWD"
+    
+    # Take down this version (it shouldn't be live yet, but if we take it down now we can ensure we control when the new one comes up)
+    php artisan down
 
     # Upgrade Cachet
-    php artisan down
     php artisan app:update
 fi
