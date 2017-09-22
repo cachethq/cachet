4d8ccc14128923bfff38433c5f7860104b191014
Ferreira Venancio Diogo, INI-INO-BIX-IO <Diogo.FerreiraVenancio@swisscom.com>
2017-09-22 13:13:24 +0200

    removed timezones for calculations

--- app/Repositories/Uptime/UpTimeRepository.php
+++ app/Repositories/Uptime/UpTimeRepository.php
@@ -4,6 +4,7 @@ namespace CachetHQ\Cachet\Repositories\Uptime;
 use DateTime;
 use DateTimeZone;
 use Illuminate\Support\Facades\DB;
+use Jenssegers\Date\Date;
 
 
 /**
@@ -32,15 +33,6 @@ class UpTimeRepository
     }
 
 
-
-    private function getStartDates($format){
-        $fromDate = new DateTime();
-        $fromDate->setTimezone(new DateTimeZone($this->userTimeZone));
-        $toDate = DateTime::createFromFormat($format, $fromDate->format($format));
-        return compact("fromDate", "toDate");
-    }
-
-
     /**
      * @param $component
      * @param $days
@@ -49,27 +41,20 @@ class UpTimeRepository
      */
     public function ComponentUpTimeForLastDays($component, $days){
 
-        $dates = $this->getStartDates("Y-m-d H-m");
-        $fromDate = $dates["fromDate"];
-        $toDate = $dates["toDate"];
+        $fromDate = new DateTime("now");
+        $toDate = new DateTime("now");
+        $toDate->setTime(0,0,0);
+        $fromDate->setTime(0,0,0);
+        $fromDate->modify("+1 day");
 
         // 1st iteration : substract hours and minutes ex: Wed: 14:37 -> compute uptime for last 14:37 hours
         // 2nd iteration: uptime for Tuesday ( 24H ), Monday ( 24H ) etc...
 
         $upTimes = [];
 
+        $periodInHours = 24.0;
+
         for($i = 1; $i <= $days; $i ++){
-
-            $diff = $fromDate->diff($toDate);
-
-            $periodInHours = $diff->h > 0 ? $diff->h : 24.0;
-
-            if($periodInHours == 24.0){
-                $fromDate->setTime(0,0,0);
-                $toDate->setTime(0,0,0);
-            }
-
-
             $downTime = $this
                 ->repository
                 ->getComponentUpTimeSinceHours(
@@ -95,9 +80,11 @@ class UpTimeRepository
      */
     public function ComponentUpTimesForLastHours($component, $hours){
 
-        $dates = $this->getStartDates("H");
-        $fromDate = $dates["fromDate"];
-        $toDate = $dates["toDate"];
+        $fromDate = new DateTime("now");
+        $toDate = new DateTime("now");
+        $fromDate->modify("+1 hour");
+        $fromDate->setTime($fromDate->format("H"),0,0);
+        $toDate->setTime($toDate->format("H"),0,0);
 
         // 1st iteration : substract minutes ex: 14:37 -> compute uptime for 37 minutes
         // 2nd iteration: uptime for 13:00 to 14:00, 3th 12:00 to 13:00 etc...
@@ -105,6 +92,7 @@ class UpTimeRepository
         $upTimes = [];
 
         for($i = 1; $i <= $hours; $i ++){
+
             $downTime = $this
                 ->repository
                 ->getComponentUpTimeSinceHours(
@@ -112,11 +100,23 @@ class UpTimeRepository
                     $toDate->getTimestamp(),
                     $fromDate->getTimestamp()
                 );
-            $upTimes[$toDate->format('Y-m-d H:i')] = (1.0 - $downTime) * 100.0;
+
+            if($downTime > 1.0 ) $downTime = 1.0;
+            $upTimes[$this->getDateLabel($toDate, 'Y-m-d H:00:00')] = (1.0 - $downTime) * 100.0;
             $fromDate = clone $toDate;
             $toDate->modify("-1 hour");
         }
-
         return $upTimes;
+    }
+
+    /**
+     * @param $date
+     * @param $format
+     * @return mixed
+     */
+    private function getDateLabel($date,$format){
+        $dateToShow = clone $date;
+        $dateToShow->setTimeZone(new DateTimeZone($this->userTimeZone));
+        return $dateToShow->format($format);
     }
 }
