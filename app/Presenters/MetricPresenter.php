e13d6799d5dfabe78d0842240a7d67846943bf49
James Brooks <james@alt-three.com>
2017-09-30 11:29:42 +0100

    Apply fixes from StyleCI (#2743)
    [ci skip] [skip ci]

--- app/Presenters/MetricPresenter.php
+++ app/Presenters/MetricPresenter.php
@@ -77,7 +77,8 @@ class MetricPresenter extends BasePresenter implements Arrayable, Jsonable
     /**
      * Convert the object to its JSON representation.
      *
-     * @param  int  $options
+     * @param int $options
+     *
      * @return string
      */
     public function toJson($options = 0)
