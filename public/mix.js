a7e11e12d1d2665f8b1c9e967d15f79fe376b74d
Diogo-Ferreira <d.venancio.ferreira@gmail.com>
2017-10-20 10:33:28 +0200

    reverted mix.js

--- public/mix.js
+++ public/mix.js
@@ -1,129 +1,34 @@
-<<<<<<< HEAD
 webpackJsonp([1],{
-
-/***/ 13:
-=======
-/******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId])
-/******/ 			return installedModules[moduleId].exports;
-/******/
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// identity function for calling harmony imports with the correct context
-/******/ 	__webpack_require__.i = function(value) { return value; };
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, {
-/******/ 				configurable: false,
-/******/ 				enumerable: true,
-/******/ 				get: getter
-/******/ 			});
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 48);
-/******/ })
-/************************************************************************/
-/******/ ({
-
-/***/ 11:
->>>>>>> 923c237a... date range integration, some code refactoring
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ }),
-
-<<<<<<< HEAD
-/***/ 14:
-=======
-/***/ 12:
->>>>>>> 923c237a... date range integration, some code refactoring
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ }),
-
-<<<<<<< HEAD
-/***/ 15:
-=======
-/***/ 13:
->>>>>>> 923c237a... date range integration, some code refactoring
-/***/ (function(module, exports) {
-
-
-
-/***/ }),
-
-<<<<<<< HEAD
-/***/ 51:
-/***/ (function(module, exports, __webpack_require__) {
-
-__webpack_require__(15);
-__webpack_require__(13);
-module.exports = __webpack_require__(14);
-=======
-/***/ 48:
-/***/ (function(module, exports, __webpack_require__) {
-
-__webpack_require__(13);
-__webpack_require__(11);
-module.exports = __webpack_require__(12);
->>>>>>> 923c237a... date range integration, some code refactoring
-
-
-/***/ })
-
-<<<<<<< HEAD
-},[51]);
-=======
-/******/ });
->>>>>>> 923c237a... date range integration, some code refactoring
+    
+    /***/ 13:
+    /***/ (function(module, exports) {
+    
+    // removed by extract-text-webpack-plugin
+    
+    /***/ }),
+    
+    /***/ 14:
+    /***/ (function(module, exports) {
+    
+    // removed by extract-text-webpack-plugin
+    
+    /***/ }),
+    
+    /***/ 15:
+    /***/ (function(module, exports) {
+    
+    
+    
+    /***/ }),
+    
+    /***/ 51:
+    /***/ (function(module, exports, __webpack_require__) {
+    
+    __webpack_require__(15);
+    __webpack_require__(13);
+    module.exports = __webpack_require__(14);
+    
+    
+    /***/ })
+    
+    },[51]);
