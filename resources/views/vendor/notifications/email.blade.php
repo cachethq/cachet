81956a422440217dc28e2097e15e9bb4e66a680f
James Brooks <jbrooksuk@me.com>
2017-12-03 10:25:27 +0000

    Use settings.app_name and app.name for fallback

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -71,7 +71,7 @@ $style = [
                     <tr>
                         <td style="{{ $style['email-masthead'] }}">
                             <a style="{{ $fontFamily }} {{ $style['email-masthead_name'] }}" href="{{ url('/') }}" target="_blank">
-                                {{ setting('app_name') }}
+                                {{ {{ setting('app_name', config('app.name')) }} }}
                             </a>
                         </td>
                     </tr>
@@ -140,7 +140,7 @@ $style = [
 
                                         <!-- Salutation -->
                                         <p style="{{ $style['paragraph'] }}">
-                                            Regards,<br>{{ setting('app_name') }}
+                                            Regards,<br>{{ {{ setting('app_name', config('app.name')) }} }}
                                         </p>
 
                                         <!-- Sub Copy -->
@@ -176,7 +176,7 @@ $style = [
                                     <td style="{{ $fontFamily }} {{ $style['email-footer_cell'] }}">
                                         <p style="{{ $style['paragraph-sub'] }}">
                                             &copy; {{ date('Y') }}
-                                            <a style="{{ $style['anchor'] }}" href="{{ url('/') }}" target="_blank">{{ setting('app_name') }}</a>.
+                                            <a style="{{ $style['anchor'] }}" href="{{ url('/') }}" target="_blank">{{ {{ setting('app_name', config('app.name')) }} }}</a>.
                                             All rights reserved.
                                         </p>
                                     </td>
