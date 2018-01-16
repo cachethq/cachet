1ca473ed757fb0a22ea2b3b03f2e003d31d0b5bc
James Brooks <james@alt-three.com>
2018-01-16 14:06:40 +0000

    New translations setup.php (Thai)

---
+++ resources/lang/th-TH/setup.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'setup'             => 'ตั้งค่า',
+    'title'             => 'ติดตั้ง Cachet',
+    'service_details'   => 'รายละเอียดของ Services',
+    'env_setup'         => 'ตั้งค่าสภาพแวดล้อม',
+    'status_page_setup' => 'ตั้งค่าหน้าแสดงสถานะ',
+    'show_support'      => 'แสดงการสนับสนุน Cachet',
+    'admin_account'     => 'บัญชีผู้ดูแลระบบ',
+    'complete_setup'    => 'ติดตั้งเสร็จสมบูรณ์',
+    'completed'         => 'การปรับแต่ง Cachet สำเร็จ!',
+    'finish_setup'      => 'ไปที่หน้า Dashboard',
+];
