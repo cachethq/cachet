2d4d823959b21e880769b83657157daca8ceed42
A <anthony@uxen.fr>
2018-01-26 11:13:24 +0100

    Adds a line in the notification email to manage its subscriptions.
    Untill now a subscriber wasn't able to manage its subscription because
    there was no link anywhere, so it was forced to unsubscribe and then
    subscribe again.
    
    Now the link is in the notification email, after 'Unsubscribe at'.
    
    Related to #2887

--- app/Notifications/Component/ComponentStatusChangedNotification.php
+++ app/Notifications/Component/ComponentStatusChangedNotification.php
@@ -88,7 +88,9 @@ class ComponentStatusChangedNotification extends Notification
                     ->greeting(trans('notifications.component.status_update.mail.subject'))
                     ->line($content)
                     ->action(trans('notifications.component.status_update.mail.action'), cachet_route('status-page'))
-                    ->line(trans('cachet.subscriber.unsubscribe', ['link' => cachet_route('subscribe.unsubscribe', $notifiable->verify_code)]));
+                    ->line(trans('cachet.subscriber.unsubscribe', ['link' => cachet_route('subscribe.unsubscribe', $notifiable->verify_code)]))
+                    //TODO: Translate the text below
+                    ->line('Manage your subscriptions at ' . cachet_route('subscribe.manage', $notifiable->verify_code));
     }
 
     /**
