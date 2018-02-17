d1efb7fbf7738ddf2b7601374e855a72d2834dc7
James Brooks <james@alt-three.com>
2018-02-17 13:21:49 +0000

    New translations forms.php (Chinese Simplified)

--- resources/lang/zh-CN/forms.php
+++ resources/lang/zh-CN/forms.php
@@ -155,7 +155,7 @@ return [
             'days-of-incidents'                     => '显示多少天的故障？',
             'time_before_refresh'                   => '状态页刷新速率（秒）',
             'banner'                                => '横幅图像',
-            'banner-help'                           => '建议上传文件宽度不大于 930 像素。',
+            'banner-help'                           => "建议上传文件宽度不大于 930 像素。",
             'subscribers'                           => '允许用户订阅邮件通知',
             'suppress_notifications_in_maintenance' => '当计划维护发生时静默处理事件通知？',
             'skip_subscriber_verification'          => '是否跳过用户邮件验证？(小心，这可能会被滥用)',
@@ -218,7 +218,7 @@ return [
         ],
         'team' => [
             'description' => '请输入您要邀请的团队成员的邮件地址：',
-            'email'       => '电子邮箱 #:id',
+            'email'       => '团队成员的电子邮箱地址',
         ],
     ],
 
@@ -238,7 +238,7 @@ return [
     'remove'         => '删除',
     'invite'         => '邀请',
     'signup'         => '注册',
-    'manage_updates' => 'Manage Updates',
+    'manage_updates' => '管理更新',
 
     // Other
     'optional' => '* 可选',
