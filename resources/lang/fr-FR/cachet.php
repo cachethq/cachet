b16f1fd2799b53af80b0aaeda45d5815240d5a55
James Brooks <james@alt-three.com>
2018-02-17 13:50:13 +0000

    New translations cachet.php (French)

--- resources/lang/fr-FR/cachet.php
+++ resources/lang/fr-FR/cachet.php
@@ -120,9 +120,9 @@ return [
     // Meta descriptions
     'meta' => [
         'description' => [
-            'incident'  => 'Details and updates about the :name incident that occurred on :date',
-            'schedule'  => 'Details about the scheduled maintenance period :name starting :startDate',
-            'subscribe' => 'Subscribe to :app in order to receive updates of incidents and scheduled maintenance periods',
+            'incident'  => 'Détails et mises à jour sur l\'incident :name qui a eu lieu le :date',
+            'schedule'  => 'Détails sur la période de maintenance planifiée :name débutant le :startDate',
+            'subscribe' => 'S’abonner à :app afin de recevoir les mises à jour des incidents et des périodes de maintenance planifiée',
             'overview'  => 'Restez à jour avec les dernières mises à jour de :app.',
         ],
     ],
