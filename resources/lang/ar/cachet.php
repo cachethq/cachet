995cd83c7d2dfb13cb8da25ce54be83f5292d792
ButterflyOfFire <ButterflyOfFire@protonmail.com>
2018-03-07 19:22:29 +0100

    Updating cachet.php (ar)
    Updating cachet.php (ar)

--- resources/lang/ar/cachet.php
+++ resources/lang/ar/cachet.php
@@ -12,110 +12,95 @@
 return [
     // Components
     'components' => [
-        'last_updated' => 'Last updated :timestamp',
+        'last_updated' => 'آخر تحديث :timestamp',
         'status'       => [
+            0 => 'مجهول',
             1 => 'Operational',
-            2 => 'Performance Issues',
-            3 => 'Partial Outage',
-            4 => 'Major Outage',
+            2 => 'مشاكل أداء',
+            3 => 'إنقطاع جزئي',
+            4 => 'إنقطاع أساسي',
         ],
         'group' => [
-            'other' => 'Other Components',
+            'other' => 'مكونات أخرى',
         ],
     ],
 
     // Incidents
     'incidents' => [
-        'none'          => 'No incidents reported',
-        'past'          => 'Past Incidents',
-        'previous_week' => 'Previous',
-        'next_week'     => 'Next',
-        'scheduled'     => 'Scheduled Maintenance',
-        'scheduled_at'  => ', scheduled :timestamp',
-        'status'        => [
-            0 => 'Scheduled', // TODO: Hopefully remove this.
-            1 => 'Investigating',
-            2 => 'Identified',
-            3 => 'Watching',
-            4 => 'Fixed',
+        'none'         => 'لا توجد حالات تم الإبلاغ عنها',
+        'past'         => 'حالات الحوادث السابقة',
+        'stickied'     => 'حالات الحوادث المثبتة',
+        'scheduled'    => 'صيانة مجدولة',
+        'scheduled_at' => ', مجدولة :timestamp',
+        'posted'       => 'تم الإرسال :timestamp',
+        'posted_at'    => 'تم الإرسال :timestamp',
+        'status'       => [
+            1 => 'تحقيق',
+            2 => 'تم التعرف عليه',
+            3 => 'مراقبة',
+            4 => 'تمّ اصلاحها',
+        ],
+    ],
+
+    // Schedule
+    'schedules' => [
+        'status' => [
+            0 => 'القادم',
+            1 => 'جار المعالجة',
+            2 => 'مكتملة',
         ],
     ],
 
     // Service Status
     'service' => [
-        'good'  => '[0,1] System operational|[2,Inf] All systems are operational',
-        'bad'   => '[0,1] The system is currently experiencing issues|[2,Inf] Some systems are experiencing issues',
-        'major' => '[0,1] The service is experiencing a major outage|[2,Inf] Some systems are experiencing a major outage',
+        'good'  => '[0,1]System operational|[2,*] All systems are operational',
+        'bad'   => '[0,1]The system is experiencing issues|[2,*]Some systems are experiencing issues',
+        'major' => '[0,1]The system is experiencing major issues|[2,*]Some systems are experiencing major issues',
     ],
 
     'api' => [
-        'regenerate' => 'Regenerate API Key',
-        'revoke'     => 'Revoke API Key',
+        'regenerate' => 'إعادة إنشاء مفتاح الواجهة البرمجية',
+        'revoke'     => 'إزالة مفتاح الواجهة البرمجية',
     ],
 
     // Metrics
     'metrics' => [
         'filter' => [
-            'last_hour' => 'Last Hour',
-            'hourly'    => 'Last 12 Hours',
-            'weekly'    => 'Week',
-            'monthly'   => 'Month',
+            'last_hour' => 'الساعة الأخيرة',
+            'hourly'    => 'آخر 12 ساعة',
+            'weekly'    => 'أسبوع',
+            'monthly'   => 'شهر',
         ],
     ],
 
     // Subscriber
     'subscriber' => [
-        'subscribe' => 'Subscribe to get the most recent updates',
-        'button'    => 'Subscribe',
-        'manage'    => [
-            'no_subscriptions' => 'You\'re currently subscribed to all updates.',
-            'my_subscriptions' => 'You\'re currently subscribed to the following updates.',
+        'subscribe'   => 'إشترك للحصول على التحديثات',
+        'unsubscribe' => 'إلغاء الإشتراك في :link',
+        'button'      => 'اشتراك',
+        'manage'      => [
+            'no_subscriptions' => 'أنت الآن مشترك للحصول على جميع التحديثات.',
+            'my_subscriptions' => 'أنت كنت مشترك حاليا بالتحديثات التالية.',
         ],
         'email' => [
-            'subscribe'          => 'Subscribe to email updates.',
-            'subscribed'         => 'You\'ve been subscribed to email notifications, please check your email to confirm your subscription.',
-            'verified'           => 'Your email subscription has been confirmed. Thank you!',
-            'manage'             => 'Manage your subscription',
-            'unsubscribe'        => 'Unsubscribe from email updates.',
-            'unsubscribed'       => 'Your email subscription has been cancelled.',
-            'failure'            => 'Something went wrong with the subscription.',
-            'already-subscribed' => 'Cannot subscribe :email because they\'re already subscribed.',
-            'verify'             => [
-                'text'   => "Please confirm your email subscription to :app_name status updates.\n:link",
-                'html'   => '<p>Please confirm your email subscription to :app_name status updates.</p>',
-                'button' => 'Confirm Subscription',
-            ],
-            'maintenance' => [
-                'subject' => '[Maintenance Scheduled] :name',
-            ],
-            'incident' => [
-                'subject' => '[New Incident] :status: :name',
-            ],
-            'component' => [
-                'subject'       => 'Component Status Update',
-                'text'          => 'The component :component_name has seen a status change. The component is now at :component_human_status.\nThank you, :app_name',
-                'html'          => '<p>The component :component_name has seen a status change. The component is now at :component_human_status.</p><p>Thank you, :app_name</p>',
-                'tooltip-title' => 'Subscribe to notifications for :component_name.',
-            ],
-        ],
-    ],
-
-    'users' => [
-        'email' => [
-            'invite' => [
-                'text' => "You have been invited to the team :app_name status page, to sign up follow the next link.\n:link\nThank you, :app_name",
-                'html' => '<p>You have been invited to the team :app_name status page, to sign up follow the next link.</p><p><a href=":link">:link</a></p><p>Thank you, :app_name</p>',
-            ],
+            'subscribe'          => 'إشترك في تحديثات البريد الإلكتروني.',
+            'subscribed'         => 'تم تسجيلك في تنبيهات البريد الإلكتروني، الرجاء تفقد بريدك الإلكتروني لتأكيد الإشتراك.',
+            'verified'           => 'تم تأكيد بريدك الإلكتروني. شكراً لك !',
+            'manage'             => 'تحكم في الاشتراكات الخاص بك',
+            'unsubscribe'        => 'إلغاء الإشتراك من تحديثات البريد الإلكتروني.',
+            'unsubscribed'       => 'تم إلغاء إشتراك بريدك الإلكتروني.',
+            'failure'            => 'حدث خلل أثناء الإشتراك.',
+            'already-subscribed' => 'لا يمكن الاشتراك: البريد الإلكتروني نظراً لأنها كنت الاشتراك مسبقاً.',
         ],
     ],
 
     'signup' => [
-        'title'    => 'Sign Up',
-        'username' => 'Username',
-        'email'    => 'Email',
-        'password' => 'Password',
-        'success'  => 'Your account has been created.',
-        'failure'  => 'Something went wrong with the signup.',
+        'title'    => 'تسجيل حساب',
+        'username' => 'إسم المستخدم',
+        'email'    => 'عنوان البريد الإلكتروني',
+        'password' => 'كلمة السر',
+        'success'  => 'لقد تم إنشاء حسابك.',
+        'failure'  => 'لقد حدث خطأ أثناء إنشاء الحساب.',
     ],
 
     'system' => [
@@ -124,19 +109,29 @@ return [
 
     // Modal
     'modal' => [
-        'close'     => 'Close',
+        'close'     => 'إغلاق',
         'subscribe' => [
-            'title'  => 'Subscribe to component updates',
+            'title'  => 'الإشتراك في تحديثات المكونات',
             'body'   => 'Enter your email address to subscribe to updates for this component. If you\'re already subscribed, you\'ll already receive emails for this component.',
-            'button' => 'Subscribe',
+            'button' => 'إشتراك',
+        ],
+    ],
+
+    // Meta descriptions
+    'meta' => [
+        'description' => [
+            'incident'  => 'Details and updates about the :name incident that occurred on :date',
+            'schedule'  => 'Details about the scheduled maintenance period :name starting :startDate',
+            'subscribe' => 'Subscribe to :app in order to receive updates of incidents and scheduled maintenance periods',
+            'overview'  => 'Stay up to date with the latest service updates from :app.',
         ],
     ],
 
     // Other
-    'home'            => 'Home',
-    'description'     => 'Stay up to date with the latest service updates from :app.',
-    'powered_by'      => 'Powered by <a href="https://cachethq.io" class="links">Cachet</a>.',
-    'about_this_site' => 'About This Site',
+    'home'            => 'الصفحة الرئيسية',
+    'powered_by'      => 'مدعوم بواسطة <a href="https://cachethq.io" class="links">Cachet</a>.',
+    'timezone'        => 'Times are shown in :timezone.',
+    'about_this_site' => 'عن هذا الموقع',
     'rss-feed'        => 'RSS',
     'atom-feed'       => 'Atom',
     'feed'            => 'Status Feed',
