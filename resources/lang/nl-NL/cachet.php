656aada76f263335f22d8629699c19dfed54a814
James Brooks <james@alt-three.com>
2018-03-13 19:50:08 +0000

    New translations cachet.php (Dutch)

--- resources/lang/nl-NL/cachet.php
+++ resources/lang/nl-NL/cachet.php
@@ -54,8 +54,8 @@ return [
     // Service Status
     'service' => [
         'good'  => '[0,1] Systeem operationeel|[2,Inf] Alle systemen zijn operationeel',
-        'bad'   => '[0,1] Het systeem ondervind momenteel problemen |[2,Inf] Sommige systemen ondervinden momenteel problemen',
-        'major' => '[0,1] De service ondervind momenteel a grote storing|[2,Inf] Sommige systemen ondervinden momenteel een grote storing',
+        'bad'   => '[0,1] Het systeem ondervindt momenteel problemen |[2,Inf] Sommige systemen ondervinden momenteel problemen',
+        'major' => '[0,1] De service ondervindt momenteel een grote storing|[2,Inf] Sommige systemen ondervinden momenteel een grote storing',
     ],
 
     'api' => [
@@ -122,7 +122,7 @@ return [
         'description' => [
             'incident'  => 'Informatie betreffende het incident :name van :date',
             'schedule'  => 'Informatie betreffende het onderhoud :name gepland op :startDate',
-            'subscribe' => 'Subscribe to :app in order to receive updates of incidents and scheduled maintenance periods',
+            'subscribe' => 'Abonneer je op :app om updates te ontvangen van incidenten en gepland onderhoud',
             'overview'  => 'Blijf op de hoogte van de laatste service updates over :app.',
         ],
     ],
