e8e11899a25929fc3793a442441ae521fd037195
James Brooks <james@alt-three.com>
2018-03-27 18:24:11 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Notifications/IncidentUpdate/IncidentUpdatedNotification.php
+++ app/Notifications/IncidentUpdate/IncidentUpdatedNotification.php
@@ -85,7 +85,6 @@ class IncidentUpdatedNotification extends Notification
                     ->markdown($this->update->message)
                     ->line(trans('cachet.subscriber.unsubscribe', ['link' => cachet_route('subscribe.unsubscribe', $notifiable->verify_code)]))
                     ->line(trans('cachet.subscriber.manage.manage_at_link', ['link' => cachet_route('subscribe.manage', $notifiable->verify_code)]));
-
     }
 
     /**
