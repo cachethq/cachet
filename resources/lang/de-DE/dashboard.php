04c3d326d1ac66a795f70fad0ab396f49ea377d7
James Brooks <james@alt-three.com>
2018-03-28 04:20:11 +0100

    New translations dashboard.php (German)

--- resources/lang/de-DE/dashboard.php
+++ resources/lang/de-DE/dashboard.php
@@ -35,7 +35,7 @@ return [
                 'failure' => 'Etwas ist mit dem Aktualisieren des Vorfall Updates schief gelaufen',
             ],
         ],
-        'reported_by'              => 'Reported by :user',
+        'reported_by'              => 'Gemeldet von :user',
         'add'                      => [
             'title'   => 'Ereignis hinzufügen',
             'success' => 'Ereignis hinzugefügt.',
