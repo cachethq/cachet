5c678d3b5014d0b7c9e9e69be6a23ba0680a0299
James Brooks <james@alt-three.com>
2018-03-29 10:50:14 +0100

    New translations cachet.php (Chinese Simplified)

--- resources/lang/zh-CN/cachet.php
+++ resources/lang/zh-CN/cachet.php
@@ -81,7 +81,7 @@ return [
         'manage'      => [
             'no_subscriptions' => '您当前已订阅所有更新。',
             'my_subscriptions' => '您当前已订阅下列更新',
-            'manage_at_link'   => 'Manage your subscriptions at :link',
+            'manage_at_link'   => '在 :link 管理你的订阅',
         ],
         'email' => [
             'subscribe'          => '订阅电子邮件更新。',
@@ -121,7 +121,7 @@ return [
     // Meta descriptions
     'meta' => [
         'description' => [
-            'incident'  => 'Details and updates about the :name incident that occurred on :date',
+            'incident'  => '有关于发生在 :date 的 :name 事件的细节与更新',
             'schedule'  => 'Details about the scheduled maintenance period :name starting :startDate',
             'subscribe' => 'Subscribe to :app in order to receive updates of incidents and scheduled maintenance periods',
             'overview'  => '始终保持对 :app 服务状态的关注。',
