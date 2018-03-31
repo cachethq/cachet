483ee042fff3388c6d29437d2a5e0a6a843e91e8
James Brooks <james@alt-three.com>
2018-03-31 12:01:27 +0100

    New translations dashboard.php (Chinese Traditional)

--- resources/lang/zh-TW/dashboard.php
+++ resources/lang/zh-TW/dashboard.php
@@ -18,12 +18,12 @@ return [
     'incidents' => [
         'title'                    => '事件與排程',
         'incidents'                => '事件',
-        'logged'                   => '{0} 做得好，沒有任何事件。|你記錄了一個事件。|你回報了 <strong>:count</strong> 個事件。',
+        'logged'                   => '{0} There are no incidents, good work.|[1]You have logged one incident.|[2,*]You have reported <strong>:count</strong> incidents.',
         'incident-create-template' => '新增模板',
         'incident-templates'       => '事件模板',
         'updates'                  => [
             'title'   => 'Incident updates for :incident',
-            'count'   => '{0} Zero Updates|[1] One Update|[2] Two Updates|[3,*] Several Updates',
+            'count'   => '{0}Zero Updates|[1]One Update|[2]Two Updates|[3,*]Several Updates',
             'add'     => [
                 'title'   => 'Create new incident update',
                 'success' => 'Your new incident update has been created.',
@@ -75,7 +75,7 @@ return [
     // Incident Maintenance
     'schedule' => [
         'schedule'     => '排程維護',
-        'logged'       => '{0} There are no schedules, good work.|You have logged one schedule.|You have reported <strong>:count</strong> schedules.',
+        'logged'       => '{0}There has been no Maintenance, good work.|[1]You have logged one schedule.|[2,*]You have reported <strong>:count</strong> schedules.',
         'scheduled_at' => '排程於 :timestamp',
         'add'          => [
             'title'   => 'Add Scheduled Maintenance',
