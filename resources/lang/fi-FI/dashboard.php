58667188c881139de229a858e1b15df0c6ad8bc7
James Brooks <james@alt-three.com>
2018-04-02 22:12:26 +0100

    New translations dashboard.php (Finnish)

--- resources/lang/fi-FI/dashboard.php
+++ resources/lang/fi-FI/dashboard.php
@@ -18,7 +18,7 @@ return [
     'incidents' => [
         'title'                    => 'Tapahtumat &amp; ajastaminen',
         'incidents'                => 'Tapahtumat',
-        'logged'                   => '{0} There are no incidents, good work.|[1]You have logged one incident.|[2,*]You have reported <strong>:count</strong> incidents.',
+        'logged'                   => '{0}There are no incidents, good work.|[1]You have logged one incident.|[2,*]You have reported <strong>:count</strong> incidents.',
         'incident-create-template' => 'Luo mallipohja',
         'incident-templates'       => 'Tapahtumamalli',
         'updates'                  => [
