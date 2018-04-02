9a36f692e8d04f216826c52d36dd1fcf9399fc33
James Brooks <james@alt-three.com>
2018-04-02 22:10:44 +0100

    New translations dashboard.php (Japanese)

--- resources/lang/ja-JP/dashboard.php
+++ resources/lang/ja-JP/dashboard.php
@@ -18,7 +18,7 @@ return [
     'incidents' => [
         'title'                    => 'インシデント &amp; スケジュール',
         'incidents'                => 'インシデント',
-        'logged'                   => '{0} There are no incidents, good work.|[1]You have logged one incident.|[2,*]You have reported <strong>:count</strong> incidents.',
+        'logged'                   => '{0}There are no incidents, good work.|[1]You have logged one incident.|[2,*]You have reported <strong>:count</strong> incidents.',
         'incident-create-template' => 'テンプレートの作成',
         'incident-templates'       => 'インシデント・テンプレート',
         'updates'                  => [
