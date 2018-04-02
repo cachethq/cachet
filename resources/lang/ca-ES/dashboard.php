6018835cb44367167cd6b4d298f82cc2e373a016
James Brooks <james@alt-three.com>
2018-04-02 22:11:48 +0100

    New translations dashboard.php (Catalan)

--- resources/lang/ca-ES/dashboard.php
+++ resources/lang/ca-ES/dashboard.php
@@ -18,7 +18,7 @@ return [
     'incidents' => [
         'title'                    => 'Incidències i manteniments',
         'incidents'                => 'Incidències',
-        'logged'                   => '{0} There are no incidents, good work.|[1]You have logged one incident.|[2,*]You have reported <strong>:count</strong> incidents.',
+        'logged'                   => '{0}There are no incidents, good work.|[1]You have logged one incident.|[2,*]You have reported <strong>:count</strong> incidents.',
         'incident-create-template' => 'Crear plantilla',
         'incident-templates'       => 'Incident Templates',
         'updates'                  => [
