a8983f4c804705e96b4727b169d569f081d6aa6c
James Brooks <james@alt-three.com>
2018-04-02 22:12:07 +0100

    New translations dashboard.php (Greek)

--- resources/lang/el-GR/dashboard.php
+++ resources/lang/el-GR/dashboard.php
@@ -18,7 +18,7 @@ return [
     'incidents' => [
         'title'                    => 'Περιστατικά &amp; Πρόγραμμα',
         'incidents'                => 'Περιστατικά',
-        'logged'                   => '{0} There are no incidents, good work.|[1]You have logged one incident.|[2,*]You have reported <strong>:count</strong> incidents.',
+        'logged'                   => '{0}There are no incidents, good work.|[1]You have logged one incident.|[2,*]You have reported <strong>:count</strong> incidents.',
         'incident-create-template' => 'Δημιουργία προτύπου',
         'incident-templates'       => 'Πρότυπα Περιστατικών',
         'updates'                  => [
