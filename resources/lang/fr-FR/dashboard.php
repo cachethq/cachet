36dbcce7a75ea45d070c9dabb232ace4411ec2bd
James Brooks <james@alt-three.com>
2018-04-02 22:12:29 +0100

    New translations dashboard.php (French)

--- resources/lang/fr-FR/dashboard.php
+++ resources/lang/fr-FR/dashboard.php
@@ -18,7 +18,7 @@ return [
     'incidents' => [
         'title'                    => 'Incidents &amp; Planification',
         'incidents'                => 'Incidents',
-        'logged'                   => '{0} There are no incidents, good work.|[1]You have logged one incident.|[2,*]You have reported <strong>:count</strong> incidents.',
+        'logged'                   => '{0}There are no incidents, good work.|[1]You have logged one incident.|[2,*]You have reported <strong>:count</strong> incidents.',
         'incident-create-template' => 'Créer un modèle',
         'incident-templates'       => 'Modèles d\'incident',
         'updates'                  => [
