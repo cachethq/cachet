4475a12d0406b04396d24bc1c3ef2ea23c458e75
James Brooks <james@alt-three.com>
2018-04-03 21:10:22 +0100

    Apply fixes from StyleCI (#2995)
    [ci skip] [skip ci]

--- resources/lang/pl-PL/forms.php
+++ resources/lang/pl-PL/forms.php
@@ -155,7 +155,7 @@ return [
             'days-of-incidents'                     => 'Z ilu ostatnich dni pokazywać incydenty?',
             'time_before_refresh'                   => 'Częstotliwość odświeżania strony statusu (w sekundach)',
             'banner'                                => 'Baner',
-            'banner-help'                           => "Zaleca się, aby przesyłać pliki nie większe niż 930px szerokości",
+            'banner-help'                           => 'Zaleca się, aby przesyłać pliki nie większe niż 930px szerokości',
             'subscribers'                           => 'Czy zezwolić użytkownikom na subskrypcje e-mail w celu otrzymywania powiadomień?',
             'suppress_notifications_in_maintenance' => 'Czy pomijać powiadomienia, kiedy zdarzenie wystąpiło podczas przerwy konserwacyjnej?',
             'skip_subscriber_verification'          => 'Pominąć weryfikację użytkowników? (Ostrzeżenie: możesz otrzymać spam)',
