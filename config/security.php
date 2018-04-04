50bcfb509305386233567e6742be8992f0e583c0
Nico Stapelbroek <nstapelbroek@gmail.com>
2018-04-04 18:37:52 +0200

    Move always_authenticate config to the setting.php file

--- config/security.php
+++ config/security.php
@@ -22,15 +22,4 @@ return [
     */
     'evil' => ['(?<!\w)on\w*', 'style', 'xmlns', 'formaction', 'form', 'xlink:href', 'FSCommand', 'seekSegmentTime'],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Always authenticate
-    |--------------------------------------------------------------------------
-    |
-    | Whether to lock down Cachet and only allow viewing pages
-    | when authenticated.
-    |
-    */
-    'always_authenticate' => false,
-
 ];
