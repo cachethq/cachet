71893cc8cb2a61ef3bc8a40c330e21acc97b5b14
CachetBot <40326150+CachetBot@users.noreply.github.com>
2018-06-17 07:50:08 +0100

    New translations dashboard.php (Zulu)

---
+++ resources/lang/zu-ZA/dashboard.php
@@ -0,0 +1,304 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+    'dashboard'          => 'crwdns950:0crwdne950:0',
+    'writeable_settings' => 'crwdns951:0crwdne951:0',
+
+    // Incidents
+    'incidents' => [
+        'title'                    => 'crwdns1400:0crwdne1400:0',
+        'incidents'                => 'crwdns953:0crwdne953:0',
+        'logged'                   => 'crwdns1441:0{0}crwdnd1441:0[1]crwdne1441:0',
+        'incident-create-template' => 'crwdns955:0crwdne955:0',
+        'incident-templates'       => 'crwdns956:0crwdne956:0',
+        'updates'                  => [
+            'title'   => 'crwdns1402:0crwdne1402:0',
+            'count'   => 'crwdns1439:0{0}crwdnd1439:0[1]crwdnd1439:0[2]crwdne1439:0',
+            'add'     => [
+                'title'   => 'crwdns1404:0crwdne1404:0',
+                'success' => 'crwdns1405:0crwdne1405:0',
+                'failure' => 'crwdns1406:0crwdne1406:0',
+            ],
+            'edit' => [
+                'title'   => 'crwdns1407:0crwdne1407:0',
+                'success' => 'crwdns1408:0crwdne1408:0',
+                'failure' => 'crwdns1409:0crwdne1409:0',
+            ],
+        ],
+        'reported_by'              => 'crwdns1433:0crwdne1433:0',
+        'add'                      => [
+            'title'   => 'crwdns958:0crwdne958:0',
+            'success' => 'crwdns959:0crwdne959:0',
+            'failure' => 'crwdns960:0crwdne960:0',
+        ],
+        'edit' => [
+            'title'   => 'crwdns961:0crwdne961:0',
+            'success' => 'crwdns962:0crwdne962:0',
+            'failure' => 'crwdns963:0crwdne963:0',
+        ],
+        'delete' => [
+            'success' => 'crwdns964:0crwdne964:0',
+            'failure' => 'crwdns965:0crwdne965:0',
+        ],
+
+        // Incident templates
+        'templates' => [
+            'title' => 'crwdns968:0crwdne968:0',
+            'add'   => [
+                'title'   => 'crwdns969:0crwdne969:0',
+                'message' => 'crwdns1443:0crwdne1443:0',
+                'success' => 'crwdns971:0crwdne971:0',
+                'failure' => 'crwdns972:0crwdne972:0',
+            ],
+            'edit' => [
+                'title'   => 'crwdns973:0crwdne973:0',
+                'success' => 'crwdns974:0crwdne974:0',
+                'failure' => 'crwdns975:0crwdne975:0',
+            ],
+            'delete' => [
+                'success' => 'crwdns976:0crwdne976:0',
+                'failure' => 'crwdns977:0crwdne977:0',
+            ],
+        ],
+    ],
+
+    // Incident Maintenance
+    'schedule' => [
+        'schedule'     => 'crwdns1410:0crwdne1410:0',
+        'logged'       => 'crwdns1440:0{0}crwdnd1440:0[1]crwdne1440:0',
+        'scheduled_at' => 'crwdns980:0crwdne980:0',
+        'add'          => [
+            'title'   => 'crwdns1412:0crwdne1412:0',
+            'success' => 'crwdns1413:0crwdne1413:0',
+            'failure' => 'crwdns1414:0crwdne1414:0',
+        ],
+        'edit' => [
+            'title'   => 'crwdns1415:0crwdne1415:0',
+            'success' => 'crwdns1416:0crwdne1416:0',
+            'failure' => 'crwdns1417:0crwdne1417:0',
+        ],
+        'delete' => [
+            'success' => 'crwdns1418:0crwdne1418:0',
+            'failure' => 'crwdns1419:0crwdne1419:0',
+        ],
+    ],
+
+    // Components
+    'components' => [
+        'components'         => 'crwdns989:0crwdne989:0',
+        'component_statuses' => 'crwdns990:0crwdne990:0',
+        'listed_group'       => 'crwdns991:0crwdne991:0',
+        'add'                => [
+            'title'   => 'crwdns992:0crwdne992:0',
+            'message' => 'crwdns993:0crwdne993:0',
+            'success' => 'crwdns994:0crwdne994:0',
+            'failure' => 'crwdns995:0crwdne995:0',
+        ],
+        'edit' => [
+            'title'   => 'crwdns996:0crwdne996:0',
+            'success' => 'crwdns997:0crwdne997:0',
+            'failure' => 'crwdns998:0crwdne998:0',
+        ],
+        'delete' => [
+            'success' => 'crwdns999:0crwdne999:0',
+            'failure' => 'crwdns1000:0crwdne1000:0',
+        ],
+
+        // Component groups
+        'groups' => [
+            'groups'        => 'crwdns1001:0crwdne1001:0',
+            'no_components' => 'crwdns1002:0crwdne1002:0',
+            'add'           => [
+                'title'   => 'crwdns1003:0crwdne1003:0',
+                'success' => 'crwdns1004:0crwdne1004:0',
+                'failure' => 'crwdns1005:0crwdne1005:0',
+            ],
+            'edit' => [
+                'title'   => 'crwdns1006:0crwdne1006:0',
+                'success' => 'crwdns1007:0crwdne1007:0',
+                'failure' => 'crwdns1008:0crwdne1008:0',
+            ],
+            'delete' => [
+                'success' => 'crwdns1009:0crwdne1009:0',
+                'failure' => 'crwdns1010:0crwdne1010:0',
+            ],
+        ],
+    ],
+
+    // Metrics
+    'metrics' => [
+        'metrics' => 'crwdns1011:0crwdne1011:0',
+        'add'     => [
+            'title'   => 'crwdns1012:0crwdne1012:0',
+            'message' => 'crwdns1013:0crwdne1013:0',
+            'success' => 'crwdns1014:0crwdne1014:0',
+            'failure' => 'crwdns1015:0crwdne1015:0',
+        ],
+        'edit' => [
+            'title'   => 'crwdns1016:0crwdne1016:0',
+            'success' => 'crwdns1017:0crwdne1017:0',
+            'failure' => 'crwdns1018:0crwdne1018:0',
+        ],
+        'delete' => [
+            'success' => 'crwdns1019:0crwdne1019:0',
+            'failure' => 'crwdns1020:0crwdne1020:0',
+        ],
+    ],
+    // Subscribers
+    'subscribers' => [
+        'subscribers'          => 'crwdns1021:0crwdne1021:0',
+        'description'          => 'crwdns1022:0crwdne1022:0',
+        'description_disabled' => 'crwdns1420:0crwdne1420:0',
+        'verified'             => 'crwdns1023:0crwdne1023:0',
+        'not_verified'         => 'crwdns1024:0crwdne1024:0',
+        'subscriber'           => 'crwdns1025:0crwdne1025:0',
+        'no_subscriptions'     => 'crwdns1026:0crwdne1026:0',
+        'global'               => 'crwdns1421:0crwdne1421:0',
+        'add'                  => [
+            'title'   => 'crwdns1027:0crwdne1027:0',
+            'success' => 'crwdns1028:0crwdne1028:0',
+            'failure' => 'crwdns1029:0crwdne1029:0',
+            'help'    => 'crwdns1030:0crwdne1030:0',
+        ],
+        'edit' => [
+            'title'   => 'crwdns1031:0crwdne1031:0',
+            'success' => 'crwdns1032:0crwdne1032:0',
+            'failure' => 'crwdns1033:0crwdne1033:0',
+        ],
+    ],
+
+    // Team
+    'team' => [
+        'team'        => 'crwdns1034:0crwdne1034:0',
+        'member'      => 'crwdns1035:0crwdne1035:0',
+        'profile'     => 'crwdns1036:0crwdne1036:0',
+        'description' => 'crwdns1325:0crwdne1325:0',
+        'add'         => [
+            'title'   => 'crwdns1038:0crwdne1038:0',
+            'success' => 'crwdns1039:0crwdne1039:0',
+            'failure' => 'crwdns1040:0crwdne1040:0',
+        ],
+        'edit' => [
+            'title'   => 'crwdns1041:0crwdne1041:0',
+            'success' => 'crwdns1042:0crwdne1042:0',
+            'failure' => 'crwdns1043:0crwdne1043:0',
+        ],
+        'delete' => [
+            'success' => 'crwdns1044:0crwdne1044:0',
+            'failure' => 'crwdns1045:0crwdne1045:0',
+        ],
+        'invite' => [
+            'title'   => 'crwdns1046:0crwdne1046:0',
+            'success' => 'crwdns1047:0crwdne1047:0',
+            'failure' => 'crwdns1048:0crwdne1048:0',
+        ],
+    ],
+
+    // Settings
+    'settings' => [
+        'settings'  => 'crwdns1049:0crwdne1049:0',
+        'app-setup' => [
+            'app-setup'   => 'crwdns1050:0crwdne1050:0',
+            'images-only' => 'crwdns1051:0crwdne1051:0',
+            'too-big'     => 'crwdns1052:0crwdne1052:0',
+        ],
+        'analytics' => [
+            'analytics' => 'crwdns1053:0crwdne1053:0',
+        ],
+        'log' => [
+            'log' => 'crwdns1054:0crwdne1054:0',
+        ],
+        'localization' => [
+            'localization' => 'crwdns1055:0crwdne1055:0',
+        ],
+        'customization' => [
+            'customization' => 'crwdns1056:0crwdne1056:0',
+            'header'        => 'crwdns1057:0crwdne1057:0',
+            'footer'        => 'crwdns1058:0crwdne1058:0',
+        ],
+        'mail' => [
+            'mail'  => 'crwdns1059:0crwdne1059:0',
+            'test'  => 'crwdns1060:0crwdne1060:0',
+            'email' => [
+                'subject' => 'crwdns1061:0crwdne1061:0',
+                'body'    => 'crwdns1062:0crwdne1062:0',
+            ],
+        ],
+        'security' => [
+            'security'   => 'crwdns1063:0crwdne1063:0',
+            'two-factor' => 'crwdns1064:0crwdne1064:0',
+        ],
+        'stylesheet' => [
+            'stylesheet' => 'crwdns1065:0crwdne1065:0',
+        ],
+        'theme' => [
+            'theme' => 'crwdns1066:0crwdne1066:0',
+        ],
+        'edit' => [
+            'success' => 'crwdns1067:0crwdne1067:0',
+            'failure' => 'crwdns1068:0crwdne1068:0',
+        ],
+        'credits' => [
+            'credits'       => 'crwdns1069:0crwdne1069:0',
+            'contributors'  => 'crwdns1070:0crwdne1070:0',
+            'license'       => 'crwdns1071:0%20Ccrwdnd1071:0%20crwdne1071:0',
+            'backers-title' => 'crwdns1072:0crwdne1072:0',
+            'backers'       => 'crwdns1073:0crwdne1073:0',
+            'thank-you'     => 'crwdns1074:0crwdne1074:0',
+        ],
+    ],
+
+    // Login
+    'login' => [
+        'login'      => 'crwdns1075:0crwdne1075:0',
+        'logged_in'  => 'crwdns1076:0crwdne1076:0',
+        'welcome'    => 'crwdns1077:0crwdne1077:0',
+        'two-factor' => 'crwdns1078:0crwdne1078:0',
+    ],
+
+    // Sidebar footer
+    'help'        => 'crwdns1079:0crwdne1079:0',
+    'status_page' => 'crwdns1080:0crwdne1080:0',
+    'logout'      => 'crwdns1081:0crwdne1081:0',
+
+    // Notifications
+    'notifications' => [
+        'notifications' => 'crwdns1082:0crwdne1082:0',
+        'awesome'       => 'crwdns1083:0crwdne1083:0',
+        'whoops'        => 'crwdns1084:0crwdne1084:0',
+    ],
+
+    // Widgets
+    'widgets' => [
+        'support'          => 'crwdns1085:0crwdne1085:0',
+        'support_subtitle' => 'crwdns1086:0crwdne1086:0',
+        'news'             => 'crwdns1087:0crwdne1087:0',
+        'news_subtitle'    => 'crwdns1088:0crwdne1088:0',
+    ],
+
+    // Welcome modal
+    'welcome' => [
+        'welcome' => 'crwdns1089:0crwdne1089:0',
+        'message' => 'crwdns1090:0crwdne1090:0',
+        'close'   => 'crwdns1091:0crwdne1091:0',
+        'steps'   => [
+            'component'  => 'crwdns1092:0crwdne1092:0',
+            'incident'   => 'crwdns1093:0crwdne1093:0',
+            'customize'  => 'crwdns1094:0crwdne1094:0',
+            'team'       => 'crwdns1095:0crwdne1095:0',
+            'api'        => 'crwdns1096:0crwdne1096:0',
+            'two-factor' => 'crwdns1097:0crwdne1097:0',
+        ],
+    ],
+
+];
