b32d7a0e62ae91e2eafaa3f7618803682c6693ab
CachetBot <40326150+CachetBot@users.noreply.github.com>
2018-06-17 07:34:07 +0100

    New translations dashboard.php (Dutch)

--- resources/lang/nl-NL/dashboard.php
+++ resources/lang/nl-NL/dashboard.php
@@ -18,12 +18,12 @@ return [
     'incidents' => [
         'title'                    => 'Incidenten & Onderhoud',
         'incidents'                => 'Incidenten',
-        'logged'                   => '{0}There are no incidents, good work.|[1]You have logged one incident.|[2,*]You have reported <strong>:count</strong> incidents.',
+        'logged'                   => '{0}Top, er zijn geen incidenten.|[1]Je hebt één incident gerapporteerd.|[2,*]Je hebt <strong>:count</strong> incidenten gerapporteerd.',
         'incident-create-template' => 'Maak template',
         'incident-templates'       => 'Incident Sjablonen',
         'updates'                  => [
             'title'   => 'Incident updates voor :incident',
-            'count'   => '{0}Zero Updates|[1]One Update|[2]Two Updates|[3,*]Several Updates',
+            'count'   => '{0}Geen updates|[1]Één update|[2]Twee updates|[3,*]Meerdere updates',
             'add'     => [
                 'title'   => 'Maak een nieuwe incident update',
                 'success' => 'Je nieuwe incident update is aangemaakt.',
@@ -75,7 +75,7 @@ return [
     // Incident Maintenance
     'schedule' => [
         'schedule'     => 'Gepland onderhoud',
-        'logged'       => '{0}There has been no Maintenance, good work.|[1]You have logged one schedule.|[2,*]You have reported <strong>:count</strong> schedules.',
+        'logged'       => '{0}Er zijn geen geplande onderhoudsmomenten.|[1]Er is één onderhoudsmoment gepland.|[2,*]Er zijn <strong>:count</strong> geplande onderhoudsmomenten.',
         'scheduled_at' => 'Gepland op :timestamp',
         'add'          => [
             'title'   => 'Gepland onderhoud toevoegen',
