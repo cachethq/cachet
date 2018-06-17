9f8bc32f79d2f692c11e2176acd10562e0ba4103
CachetBot <40326150+CachetBot@users.noreply.github.com>
2018-06-17 07:50:11 +0100

    New translations setup.php (Zulu)

---
+++ resources/lang/zu-ZA/setup.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    'setup'             => 'crwdns1255:0crwdne1255:0',
+    'title'             => 'crwdns1256:0crwdne1256:0',
+    'service_details'   => 'crwdns1257:0crwdne1257:0',
+    'env_setup'         => 'crwdns1258:0crwdne1258:0',
+    'status_page_setup' => 'crwdns1259:0crwdne1259:0',
+    'show_support'      => 'crwdns1260:0crwdne1260:0',
+    'admin_account'     => 'crwdns1261:0crwdne1261:0',
+    'complete_setup'    => 'crwdns1262:0crwdne1262:0',
+    'completed'         => 'crwdns1263:0crwdne1263:0',
+    'finish_setup'      => 'crwdns1264:0crwdne1264:0',
+];
