55ff39d07b3e1e8d8822b660a33452179be25ab2
CachetBot <40326150+CachetBot@users.noreply.github.com>
2018-06-17 07:50:12 +0100

    New translations validation.php (Zulu)

---
+++ resources/lang/zu-ZA/validation.php
@@ -0,0 +1,122 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+
+    'accepted'   => 'crwdns1265:0crwdne1265:0',
+    'active_url' => 'crwdns1266:0crwdne1266:0',
+    'after'      => 'crwdns1267:0crwdne1267:0',
+    'alpha'      => 'crwdns1268:0crwdne1268:0',
+    'alpha_dash' => 'crwdns1269:0crwdne1269:0',
+    'alpha_num'  => 'crwdns1270:0crwdne1270:0',
+    'array'      => 'crwdns1271:0crwdne1271:0',
+    'before'     => 'crwdns1272:0crwdne1272:0',
+    'between'    => [
+        'numeric' => 'crwdns1273:0crwdne1273:0',
+        'file'    => 'crwdns1274:0crwdne1274:0',
+        'string'  => 'crwdns1275:0crwdne1275:0',
+        'array'   => 'crwdns1276:0crwdne1276:0',
+    ],
+    'boolean'        => 'crwdns1277:0crwdne1277:0',
+    'confirmed'      => 'crwdns1278:0crwdne1278:0',
+    'date'           => 'crwdns1279:0crwdne1279:0',
+    'date_format'    => 'crwdns1280:0crwdne1280:0',
+    'different'      => 'crwdns1281:0crwdne1281:0',
+    'digits'         => 'crwdns1282:0crwdne1282:0',
+    'digits_between' => 'crwdns1283:0crwdne1283:0',
+    'email'          => 'crwdns1284:0crwdne1284:0',
+    'exists'         => 'crwdns1285:0crwdne1285:0',
+    'distinct'       => 'crwdns1286:0crwdne1286:0',
+    'filled'         => 'crwdns1287:0crwdne1287:0',
+    'image'          => 'crwdns1288:0crwdne1288:0',
+    'in'             => 'crwdns1289:0crwdne1289:0',
+    'in_array'       => 'crwdns1290:0crwdne1290:0',
+    'integer'        => 'crwdns1291:0crwdne1291:0',
+    'ip'             => 'crwdns1292:0crwdne1292:0',
+    'json'           => 'crwdns1293:0crwdne1293:0',
+    'max'            => [
+        'numeric' => 'crwdns1294:0crwdne1294:0',
+        'file'    => 'crwdns1295:0crwdne1295:0',
+        'string'  => 'crwdns1296:0crwdne1296:0',
+        'array'   => 'crwdns1297:0crwdne1297:0',
+    ],
+    'mimes' => 'crwdns1298:0crwdne1298:0',
+    'min'   => [
+        'numeric' => 'crwdns1299:0crwdne1299:0',
+        'file'    => 'crwdns1300:0crwdne1300:0',
+        'string'  => 'crwdns1301:0crwdne1301:0',
+        'array'   => 'crwdns1302:0crwdne1302:0',
+    ],
+    'not_in'               => 'crwdns1303:0crwdne1303:0',
+    'numeric'              => 'crwdns1304:0crwdne1304:0',
+    'present'              => 'crwdns1305:0crwdne1305:0',
+    'regex'                => 'crwdns1306:0crwdne1306:0',
+    'required'             => 'crwdns1307:0crwdne1307:0',
+    'required_if'          => 'crwdns1308:0crwdne1308:0',
+    'required_unless'      => 'crwdns1309:0crwdne1309:0',
+    'required_with'        => 'crwdns1310:0crwdne1310:0',
+    'required_with_all'    => 'crwdns1311:0crwdne1311:0',
+    'required_without'     => 'crwdns1312:0crwdne1312:0',
+    'required_without_all' => 'crwdns1313:0crwdne1313:0',
+    'same'                 => 'crwdns1314:0crwdne1314:0',
+    'size'                 => [
+        'numeric' => 'crwdns1315:0crwdne1315:0',
+        'file'    => 'crwdns1316:0crwdne1316:0',
+        'string'  => 'crwdns1317:0crwdne1317:0',
+        'array'   => 'crwdns1318:0crwdne1318:0',
+    ],
+    'string'   => 'crwdns1319:0crwdne1319:0',
+    'timezone' => 'crwdns1320:0crwdne1320:0',
+    'unique'   => 'crwdns1321:0crwdne1321:0',
+    'url'      => 'crwdns1322:0crwdne1322:0',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify custom validation messages for attributes using the
+    | convention "attribute.rule" to name the lines. This makes it quick to
+    | specify a specific custom language line for a given attribute rule.
+    |
+    */
+
+    'custom' => [
+        'attribute-name' => [
+            'rule-name' => 'crwdns1323:0crwdne1323:0',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap attribute place-holders
+    | with something more reader friendly such as E-Mail Address instead
+    | of "email". This simply helps us make messages a little cleaner.
+    |
+    */
+
+    'attributes' => [],
+
+];
