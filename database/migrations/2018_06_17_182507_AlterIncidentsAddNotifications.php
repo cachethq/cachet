7c9d540477665c5d24e5d5b9069ecba8dfa81822
James Brooks <jbrooksuk@me.com>
2018-06-17 19:31:47 +0100

    Add notifications column to incidents (WIP)

---
+++ database/migrations/2018_06_17_182507_AlterIncidentsAddNotifications.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterIncidentsAddNotifications extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('incidents', function (Blueprint $table) {
+            $table->boolean('notifications')->default(false)->after('stickied');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('incidents', function (Blueprint $table) {
+            $table->dropColumn('notifications');
+        });
+    }
+}
