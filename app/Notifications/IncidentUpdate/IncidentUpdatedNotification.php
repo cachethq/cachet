c3bd8ff16fde517542c1c3fbd6b1df7442637196
Erik Anders <erik.anders@securepoint.de>
2019-01-23 14:25:25 +0100

    add privacy/imprint link to emails

--- app/Notifications/IncidentUpdate/IncidentUpdatedNotification.php
+++ app/Notifications/IncidentUpdate/IncidentUpdatedNotification.php
@@ -87,6 +87,10 @@ class IncidentUpdatedNotification extends Notification
                 'unsubscribeUrl'         => cachet_route('subscribe.unsubscribe', $notifiable->verify_code),
                 'manageSubscriptionText' => trans('cachet.subscriber.manage_subscription'),
                 'manageSubscriptionUrl'  => cachet_route('subscribe.manage', $notifiable->verify_code),
+                'privacyText'            => trans('forms.settings.privacy.privacy-statement'),
+                'privacyUrl'             => cachet_route('privacy'),
+                'imprintText'            => trans('forms.settings.privacy.imprint'),
+                'imprintUrl'             => cachet_route('imprint'),
         ]);
     }
 
