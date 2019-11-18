0015ac776ec9b7ceb1076de8b5bcd90392d8e061
Laravel Shift <shift@laravelshift.com>
2019-11-18 17:03:44 +0000

    Shift cleanup

--- config/auth.php
+++ config/auth.php
@@ -68,7 +68,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\User::class,
+            'model' => CachetHQ\Cachet\User::class,
         ],
 
         // 'users' => [
