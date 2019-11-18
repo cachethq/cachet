a9ba43039755ac2d32f4bba7c64c0d89c34cd4a3
James Brooks <james@alt-three.com>
2019-11-18 16:45:11 +0000

    Apply fixes from StyleCI

--- app/Http/Resources/Incident.php
+++ app/Http/Resources/Incident.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Cachet.
+ *
+ * (c) Alt Three Services Limited
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace CachetHQ\Cachet\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -9,7 +18,8 @@ class Incident extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
+     *
      * @return array
      */
     public function toArray($request)
