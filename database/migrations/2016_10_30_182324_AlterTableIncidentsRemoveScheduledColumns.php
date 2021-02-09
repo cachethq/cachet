dede2946936de8d0261d006e2a3aed63ebeb93d9
Seb Dangerfield <1449113+sedan07@users.noreply.github.com>
2021-02-09 16:55:22 +0000

    Close old maintenance schedules on migration
    Otherwise all maintenance schedules will appear on the status page, if
    you have 10s or 100s of these closing them won't be much fun.

--- database/migrations/2016_10_30_182324_AlterTableIncidentsRemoveScheduledColumns.php
+++ database/migrations/2016_10_30_182324_AlterTableIncidentsRemoveScheduledColumns.php
@@ -14,6 +14,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Carbon\Carbon;
+use CachetHQ\Cachet\Models\Schedule;
 
 class AlterTableIncidentsRemoveScheduledColumns extends Migration
 {
@@ -24,6 +26,7 @@ class AlterTableIncidentsRemoveScheduledColumns extends Migration
      */
     public function up()
     {
+        $yesterday = Carbon::now()->subDay();
         // We need a better way of handling data migrations...
         $system = app(System::class);
         $prefix = $system->getTablePrefix();
@@ -34,6 +37,20 @@ class AlterTableIncidentsRemoveScheduledColumns extends Migration
         Schema::table('incidents', function (Blueprint $table) {
             $table->dropColumn('scheduled_at');
         });
+
+        // Close schedules older than a day
+        $old_schedules = DB::table('schedules')
+            ->where('scheduled_at', '<', $yesterday)
+            ->get();
+
+        foreach ($old_schedules as $schedule) {
+            DB::table('schedules')
+                ->where('id', $schedule->id)
+                ->update([
+                    'completed_at' => $schedule->scheduled_at,
+                    'status' => Schedule::COMPLETE
+                ]);
+        }
     }
 
     /**
@@ -46,5 +63,8 @@ class AlterTableIncidentsRemoveScheduledColumns extends Migration
         Schema::table('incidents', function (Blueprint $table) {
             $table->timestamp('scheduled_at')->before('created_at')->nullable()->default(null);
         });
+        $system = app(System::class);
+        $prefix = $system->getTablePrefix();
+        DB::update("INSERT INTO {$prefix}incidents (name, status, message, scheduled_at, created_at, updated_at) SELECT name, 0, message, scheduled_at, created_at, updated_at FROM {$prefix}schedules");
     }
 }
