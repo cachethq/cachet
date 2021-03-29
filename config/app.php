84b0c5a4be9ff699274216612285f6c87ab2db97
James Brooks <james@alt-three.com>
2021-03-29 12:24:45 +0100

    Compatibility fixes

--- config/app.php
+++ config/app.php
@@ -174,11 +174,11 @@ return [
          * Packages Service Providers...
          */
         env('APP_DEBUG') ? Bugsnag\BugsnagLaravel\BugsnagServiceProvider::class : null,
-        AltThree\Badger\BadgerServiceProvider::class,
-        AltThree\Emoji\EmojiServiceProvider::class,
-        AltThree\Twitter\TwitterServiceProvider::class,
         Barryvdh\Cors\ServiceProvider::class,
         env('APP_DEBUG') ? Barryvdh\Debugbar\ServiceProvider::class : null,
+        CachetHQ\Badger\BadgerServiceProvider::class,
+        CachetHQ\Emoji\EmojiServiceProvider::class,
+        CachetHQ\Twitter\TwitterServiceProvider::class,
         GrahamCampbell\Binput\BinputServiceProvider::class,
         GrahamCampbell\Exceptions\ExceptionsServiceProvider::class,
         GrahamCampbell\Markdown\MarkdownServiceProvider::class,
