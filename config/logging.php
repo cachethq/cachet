4652bb7ca855f41c21834b9affb24edfba64adf3
Florian Zwonarz <fzwonarz@hostzero.com>
2023-08-18 16:00:12 +0000

    Changed default logger

--- config/logging.php
+++ config/logging.php
@@ -24,7 +24,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'stack'),
+    'default' => env('LOG_CHANNEL', 'daily'),
 
     /*
     |--------------------------------------------------------------------------
