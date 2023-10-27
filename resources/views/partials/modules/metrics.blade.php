13aa8f40a55f0a1ee4af8667585eba458d28e11e
James Brooks <james@alt-three.com>
2023-10-27 13:22:57 +0100

    Improve Metric component

--- resources/views/partials/modules/metrics.blade.php
+++ resources/views/partials/modules/metrics.blade.php
@@ -4,7 +4,7 @@
     <ul class="list-group">
         @foreach($metrics as $metric)
         <li class="list-group-item metric" data-metric-id="{{ $metric->id }}">
-            <metric-chart :metric="{{ $metric->toJson() }}" :theme-light="{{ json_encode($themeMetrics) }}" :theme="{{ json_encode(color_darken($themeMetrics, -0.1)) }}" :theme-dark="{{ json_encode(color_darken($themeMetrics, -0.2)) }}"></metric-chart>
+            <metric-chart :metric="{{ $metric->toJson() }}" theme-light="{{ $themeMetrics }}" theme="{{ color_darken($themeMetrics, -0.1) }}" theme-dark="{{ color_darken($themeMetrics, -0.2) }}"></metric-chart>
         </li>
         @endforeach
     </ul>
