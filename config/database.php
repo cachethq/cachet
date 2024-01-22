b5fde230756cb18e7d41d4feb942723e3568104a
James Brooks <james@alt-three.com>
2024-01-22 08:36:05 +0000

    wip

--- config/database.php
+++ config/database.php
@@ -65,7 +65,7 @@ return [
             'collation'      => 'utf8mb4_unicode_ci',
             'prefix'         => '',
             'prefix_indexes' => true,
-            'strict'         => true,
+            'strict'         => env('DB_STRICT', true),
             'engine'         => null,
             'options'        => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
