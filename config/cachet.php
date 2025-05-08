5de867b2c54ad8e500be94abf73d337348735feb
James Brooks <james@alt-three.com>
2025-05-08 12:55:10 +0100

    Update cachet config

--- config/cachet.php
+++ config/cachet.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of Cachet.
- *
- * (c) Alt Three Services Limited
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 return [
 
     /*
@@ -30,7 +21,7 @@ return [
      |
      | This is the URI path where Cachet will be accessible from.
      */
-    'path' => env('CACHET_PATH', 'status'),
+    'path' => env('CACHET_PATH', '/'),
 
     'guard' => env('CACHET_GUARD', null),
 
@@ -42,7 +33,9 @@ return [
      | This is the model that will be used to authenticate users. This model
      | must be an instance of Illuminate\Foundation\Auth\User.
      */
-    'user_model' => App\Models\User::class,
+    'user_model' => env('CACHET_USER_MODEL', \App\Models\User::class),
+
+    'user_migrations' => env('CACHET_USER_MIGRATIONS', true),
 
     /*
      |--------------------------------------------------------------------------
@@ -77,11 +70,14 @@ return [
      */
     'middleware' => [
         'web',
+        \Cachet\Http\Middleware\AuthenticateRemoteUser::class,
     ],
 
     'api_middleware' => [
         'api',
     ],
+
+    'trusted_proxies' => env('CACHET_TRUSTED_PROXIES', ''),
 
     /*
      |--------------------------------------------------------------------------
@@ -95,5 +91,91 @@ return [
      */
     'api_rate_limit' => env('CACHET_API_RATE_LIMIT', 300),
 
+    /*
+     |--------------------------------------------------------------------------
+     | Cachet Beacon
+     |--------------------------------------------------------------------------
+     |
+     | Enable Cachet's telemetry. Cachet will only ever send anonymous data
+     | to the cachethq.io domain. This enables us to understand how Cachet
+     | is used.
+     |
+     */
     'beacon' => env('CACHET_BEACON', true),
+
+    /*
+     |--------------------------------------------------------------------------
+     | Cachet Docker
+     |--------------------------------------------------------------------------
+     |
+     | Determines whether Cachet is running from within a Docker instance.
+     |
+     */
+    'docker' => env('CACHET_DOCKER', false),
+
+    /*
+     |--------------------------------------------------------------------------
+     | Cachet Webhooks
+     |--------------------------------------------------------------------------
+     |
+     | Configure how Cachet sends webhooks for events.
+     |
+     */
+    'webhooks' => [
+        'queue_connection' => env('CACHET_WEBHOOK_QUEUE_CONNECTION', 'default'),
+        'queue_name' => env('CACHET_WEBHOOK_QUEUE_NAME', 'webhooks'),
+
+        'logs' => [
+            'prune_logs_after_days' => 30,
+        ],
+    ],
+
+    /*
+     |--------------------------------------------------------------------------
+     | Cachet Supported Locales
+     |--------------------------------------------------------------------------
+     |
+     | Configure which locales are supported by Cachet.
+     |
+     */
+    'supported_locales' => [
+        'de' => 'Deutsch (DE)',
+        'de_AT' => 'Deutsch (AT)',
+        'de_CH' => 'Deutsch (CH)',
+        'en' => 'English',
+        'en_GB' => 'English (UK)',
+        'es_ES' => 'Spanish (ES)',
+        'nl' => 'Nederlands',
+        'pt_BR' => 'Português (BR)',
+        'zh_CN' => '简体中文',
+        'zh_TW' => '繁體中文',
+        'ph' => 'Filipino',
+    ],
+
+    /*
+     |--------------------------------------------------------------------------
+     | Cachet Demo Mode
+     |--------------------------------------------------------------------------
+     |
+     | Whether to run Cachet in demo mode. This will adjust some of the default
+     | settings to allow Cachet to run in a demo environment.
+     |
+     */
+    'demo_mode' => env('CACHET_DEMO_MODE', false),
+
+    /*
+     |--------------------------------------------------------------------------
+     | Cachet Blog Feed
+     |--------------------------------------------------------------------------
+     |
+     | This is the URI to the Cachet blog feed. This is used to display
+     | the latest blog posts on the status page. By default, this is
+     | set to the public Cachet blog feed.
+     |
+     */
+    'feed' => [
+        'uri' => env('CACHET_FEED_URI', 'https://blog.cachethq.io/rss'),
+        'cache' => env('CACHET_FEED_CACHE', 3600),
+    ],
+
 ];
